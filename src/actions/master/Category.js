8e60e531a7d6e2490857bfdbd66cbb1e068c21dc
priyankashrivas <priyanka.shriwas@systematix.com>
2019-10-17 11:37:57 +0530

    M1 [CIR-T207] seperation of all actions of masters within one folder

---
+++ src/actions/master/Category.js
@@ -0,0 +1,107 @@
+import axios from 'axios';
+import {
+    API,
+    API_REQUEST,
+    API_FAILURE,
+    CREATE_CATEGORY_TYPE_SUCCESS,
+    CREATE_CATEGORY_TYPE_FAILURE,
+    CREATE_CATEGORY_FAILURE,
+    CREATE_CATEGORY_SUCCESS,
+    FETCH_CATEGORY_DATA_FAILURE,
+    GET_CATEGORY_DATA_SUCCESS
+} from '../../config/constants';
+import {
+    apiErrors
+} from '../../helper/util';
+import {
+    MESSAGES
+} from '../../config/message';
+import { toastr } from 'react-redux-toastr'
+
+const headers = {
+    'Content-Type': 'application/json',
+    //Authorization:'Bearer 4lEZa54IiLSaAmloKW8YyBFpB5pX6dAqkKw3szUT8O8HaEgKB7G4LgbvYl9eBOu1e3tgvYOligAncfRb_4PUNwSrygdtmTvLdwMoJi5yQu9iIJAOu6J1U5iIKou92e9XLNAq953S1-R985Yc-BvLt9X9HJKYpgo4mu2DelbnHauQUdk-H-Rgv1umz56UhtnGcsPyzlHriGvJKhJjQtdPCA'
+};
+
+/**
+ * @method createCategoryTypeAPI
+ * @description create category type
+ */
+
+export function createCategoryTypeAPI(data, callback) {
+    return (dispatch) => {
+        // dispatch({
+        //     type:  API_REQUEST,
+        // });
+        const request = axios.post(API.createcategoryTypeAPI, data,headers);
+        request.then((response) => {
+            if (response.data.Result) {
+                    dispatch({
+                        type: CREATE_CATEGORY_TYPE_SUCCESS,
+                    });
+                    callback(response);
+            } else {
+                dispatch({ type: CREATE_CATEGORY_TYPE_FAILURE });
+                    if (response.data.Message) {
+                        toastr.error(response.data.Message);
+                    } 
+            }
+        }).catch((error) => {
+            dispatch({
+                type: API_FAILURE
+            });
+            apiErrors(error);
+        });
+    };
+}
+
+/**
+ * @method createCategoryTypeAPI
+ * @description create category type
+ */
+
+export function createCategoryAPI(data, callback) {
+    return (dispatch) => {
+        // dispatch({
+        //     type:  API_REQUEST,
+        // });
+        const request = axios.post(API.createCategoryAPI, data,headers);
+        request.then((response) => {
+            if (response.data.Result) {
+                    dispatch({
+                        type: CREATE_CATEGORY_SUCCESS,
+                    });
+                    callback(response);
+            } else {
+                dispatch({ type: CREATE_CATEGORY_FAILURE });
+                    if (response.data.Message) {
+                        toastr.error(response.data.Message);
+                    } 
+            }
+        }).catch((error) => {
+            dispatch({
+                type: API_FAILURE
+            });
+            apiErrors(error);
+        });
+    };
+}
+
+export function fetchCategoryMasterDataAPI() {
+    return (dispatch) => {
+        const API1 = axios.get(API.fetchCategoryType, headers);
+        Promise.all([API1])
+            .then((response) => {
+                dispatch({
+                    type: GET_CATEGORY_DATA_SUCCESS,
+                    payload: response[0].data.SelectList,
+                });
+                 
+            }).catch((error) => {
+                dispatch({
+                    type: FETCH_CATEGORY_DATA_FAILURE
+                });
+                apiErrors(error);
+            });
+    };
+}
---
+++ src/actions/master/unitOfMeasurment.js
@@ -0,0 +1,157 @@
+import axios from 'axios';
+import {
+    API,
+    API_REQUEST,
+    API_FAILURE,
+    FETCH_MATER_DATA_FAILURE,
+    GET_UOM_DATA_SUCCESS,
+    CREATE_PART_REQUEST,
+    CREATE_PART_FAILURE,
+    CREATE_PART_SUCCESS,
+    GET_UOM_SUCCESS,
+    GET_UOM_DATA_FAILURE
+} from '../../config/constants';
+import {
+    apiErrors
+} from '../../helper/util';
+import {
+    MESSAGES
+} from '../../config/message';
+import { toastr } from 'react-redux-toastr'
+
+const headers = {
+    'Content-Type': 'application/json',
+    //Authorization:'Bearer 4lEZa54IiLSaAmloKW8YyBFpB5pX6dAqkKw3szUT8O8HaEgKB7G4LgbvYl9eBOu1e3tgvYOligAncfRb_4PUNwSrygdtmTvLdwMoJi5yQu9iIJAOu6J1U5iIKou92e9XLNAq953S1-R985Yc-BvLt9X9HJKYpgo4mu2DelbnHauQUdk-H-Rgv1umz56UhtnGcsPyzlHriGvJKhJjQtdPCA'
+};
+
+export function getUnitOfMeasurementAPI(callback) {
+    return (dispatch) => {
+        dispatch({ type: API_REQUEST });
+        axios.get(API.getUOMAPI, {headers})
+            .then((response) => {
+                if (response.data.Result === true) {
+                    dispatch({
+                        type: GET_UOM_DATA_SUCCESS,
+                        payload: response.data.DataList,
+                    });
+                    callback(response);
+                } else {
+                    toastr.error(MESSAGES.SOME_ERROR);
+                }
+            }).catch((error) => {
+                console.log('error',error.response ? error.response : error)
+                dispatch({
+                    type: GET_UOM_DATA_FAILURE
+                });
+                callback(error);
+                apiErrors(error);
+            });
+    };
+}
+
+
+
+/**
+ * @method deleteUserMediaAPI
+ * @description delete user media
+ */
+export function getOneUnitOfMeasurementAPI(uomId,isEditFlag,callback) {
+    return (dispatch) => {
+        dispatch({ type: API_REQUEST });
+        if(isEditFlag){
+            axios.get(`${API.getUOMAPI}/${uomId}`, headers)
+            .then((response) => {
+                if (response.data.Result === true) {
+                    dispatch({
+                        type: GET_UOM_SUCCESS,
+                        payload: response.data.Data,
+                    });
+                    callback(response);
+                } else {
+                    toastr.error(MESSAGES.SOME_ERROR);
+                }
+                    callback(response);
+            }).catch((error) => {
+                apiErrors(error);
+                dispatch({ type: API_FAILURE });
+            });
+        }else{
+            dispatch({
+                type: GET_UOM_SUCCESS,
+                payload: {},
+            });
+            callback({});
+        }
+    };
+}
+
+/**
+ * @method createOpportunityAPI
+ * @description create opportunity 
+ */
+
+export function createUnitOfMeasurementAPI(data, callback) {
+    return (dispatch) => {
+        dispatch({
+            type: CREATE_PART_REQUEST
+        });
+        const request = axios.post(API.createUOMAPI, data, headers);
+        request.then((response) => {
+            if (response.data.Result === true) {
+                    dispatch({
+                        type: CREATE_PART_SUCCESS,
+                    });
+                    callback(response);
+            } else {
+                dispatch({ type: CREATE_PART_FAILURE });
+                    if (response.data.Message) {
+                        toastr.error(response.data.Message);
+                    } 
+            }
+        }).catch((error) => {
+            dispatch({
+                type: CREATE_PART_FAILURE
+            });
+            apiErrors(error);
+        });
+    };
+}
+
+/**
+ * @method deleteUserMediaAPI
+ * @description delete user media
+ */
+export function deleteUnitOfMeasurementAPI(index,Id ,callback) {
+    return (dispatch) => {
+        dispatch({ type: API_REQUEST });
+        axios.delete(`${API.deleteUOMAPI}/${Id}`, headers)
+            .then((response) => {
+                // getUserProfileAPIForUpdatingProps(dispatch, id, () => {
+                    callback(response);
+                   // dispatch({ type: DELETE_USER_MEDIA_SUCCESS });
+                // });
+            }).catch((error) => {
+                apiErrors(error);
+                dispatch({ type: API_FAILURE });
+            });
+    };
+}
+
+/**
+ * @method deleteUserMediaAPI
+ * @description delete user media
+ */
+export function updateUnitOfMeasurementAPI(uomId,requestData, callback) {
+    console.log('requestData', requestData);
+    
+    return (dispatch) => {
+        dispatch({ type: API_REQUEST });
+        axios.put(`${API.updateUOMAPI}`,requestData, headers)
+            .then((response) => {
+                    callback(response);
+            }).catch((error) => {
+                apiErrors(error);
+                dispatch({ type: API_FAILURE });
+            });
+    };
+}
