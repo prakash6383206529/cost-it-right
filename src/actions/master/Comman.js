29d84e08f14ba85310fe6268b5686ae9801affd4
priyankashrivas <priyanka.shriwas@systematix.com>
2019-10-21 18:51:31 +0530

    M1 [CIR-T213] Working on BOM master actions

--- src/actions/master/Comman.js
+++ src/actions/master/Comman.js
@@ -3,6 +3,9 @@ import {
     API,
     API_REQUEST,
     API_FAILURE,
+    GET_UOM_DATA_SUCCESS,
+    GET_MATERIAL_TYPE_SUCCESS,
+    GET_PART_SUCCESS,
     FETCH_MATER_DATA_FAILURE,
     GET_COUNTRY_SUCCESS,
     GET_STATE_SUCCESS,
@@ -24,29 +27,35 @@ const headers = {
     //Authorization:'Bearer 4lEZa54IiLSaAmloKW8YyBFpB5pX6dAqkKw3szUT8O8HaEgKB7G4LgbvYl9eBOu1e3tgvYOligAncfRb_4PUNwSrygdtmTvLdwMoJi5yQu9iIJAOu6J1U5iIKou92e9XLNAq953S1-R985Yc-BvLt9X9HJKYpgo4mu2DelbnHauQUdk-H-Rgv1umz56UhtnGcsPyzlHriGvJKhJjQtdPCA'
 };
 
-// export function fetchMasterDataAPI() {
-//     return (dispatch) => {
-//         const API1 = axios.get(API.getAllMasterUOMAPI, headers);
-//         const API2 = axios.get(API.getMaterialType, headers);
-//         Promise.all([API1, API2])
-//             .then((response) => {
-//                 dispatch({
-//                     type: GET_UOM_DATA_SUCCESS,
-//                     payload: response[0].data.SelectList,
-//                 });
+export function fetchMasterDataAPI() {
+    return (dispatch) => {
+        const API1 = axios.get(API.getAllMasterUOMAPI, headers);
+        const API2 = axios.get(API.getMaterialType, headers);
+        const API3 = axios.get(API.getPart, headers)
+        Promise.all([API1, API2, API3])
+            .then((response) => {
+                dispatch({
+                    type: GET_UOM_DATA_SUCCESS,
+                    payload: response[0].data.SelectList,
+                });
                 
-//                 dispatch({
-//                     type: GET_MATERIAL_TYPE_SUCCESS,
-//                     payload: response[1].data.SelectList,
-//                 });  
-//             }).catch((error) => {
-//                 dispatch({
-//                     type: FETCH_MATER_DATA_FAILURE
-//                 });
-//                 apiErrors(error);
-//             });
-//     };
-// }
+                dispatch({
+                    type: GET_MATERIAL_TYPE_SUCCESS,
+                    payload: response[1].data.SelectList,
+                });
+
+                dispatch({
+                    type: GET_PART_SUCCESS,
+                    payload: response[2].data.SelectList,
+                });  
+            }).catch((error) => {
+                dispatch({
+                    type: FETCH_MATER_DATA_FAILURE
+                });
+                apiErrors(error);
+            });
+    };
+}
 /**
  * @method fetchCountryDataAPI
  * @description Used to fetch country list
