9feed628b9804c21619c709bd2ce1de0f2a2bf07
priyankashrivas <priyanka.shriwas@systematix.com>
2019-11-01 10:06:56 +0530

    M1[CIR-T-207] mix configuration in routing

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -144,6 +144,7 @@ class SideBar extends Component {
                   <a className="dropdown-item" href="/labour-master">Labour Master</a>
                   <a className="dropdown-item" href="/overhead-profit-master">Overhead and Profit</a>
                   <a className="dropdown-item" href="/depreciation-master">Depreciation Master</a>
+                  <a className="dropdown-item" href="/process-master">Process Master</a>
                 </div>
               </li>
               <li className="nav-item dropdown">
