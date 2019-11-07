06f231b8c661f5db09c2fb2178cfd271f757b8c1
priyankashrivas <priyanka.shriwas@systematix.com>
2019-11-07 17:04:28 +0530

    M1[CIR-T-188] creating packaging component, re-initialize fields,perform edit and delete operation

--- src/components/masters/sap-masters/freight-master/index.js
+++ src/components/masters/sap-masters/freight-master/index.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
-    Container, Row, Col, Button, Table } from 'reactstrap';
+    Container, Row, Col, Button, TabContent, TabPane, Nav, NavItem, NavLink} from 'reactstrap';
 import Addfreight from './AddFreight';
-import { getFreightDetailAPI, deleteFreightAPI } from '../../../../actions/master/Freight';
+import FreightDetail from './FreightDetail';
+import { getFreightDetailAPI } from '../../../../actions/master/Freight';
+import PackagingDetail from './PackagingDetail';
 import { Loader } from '../../../common/Loader';
 import { CONSTANT } from '../../../../helper/AllConastant';
-import {
-    convertISOToUtcDate,
-} from '../../../../helper';
-import { toastr } from 'react-redux-toastr';
-import { MESSAGES } from '../../../../config/message';
-import NoContentFound from '../../../common/NoContentFound';
-
+import classnames from 'classnames';
 
 class FreightMaster extends Component {
     constructor(props) {
@@ -20,17 +16,43 @@ class FreightMaster extends Component {
         this.state = {
             isOpen: false,
             isEditFlag: false,
-            freightType: 1,
+            activeTab: '1',
+            freightId: ''
 
         }
     }
 
-    /**
+     /**
     * @method componentDidMount
     * @description called after render the component
     */
     componentDidMount() {
-        this.props.getFreightDetailAPI(res => {});
+        this.props.getFreightDetailAPI(res => { });
+    }
+
+    /**
+    * @method toggle
+    * @description toggling the tabs
+    */
+    toggle = (tab) => {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+        this.props.getFreightDetailAPI(res => { });
+    }
+
+     /**
+     * @method editFuelDetails
+     * @description  used to edit fuel details
+     */
+    editFreighteDetails = (editFlag, isModelOpen, FreightId) => {
+        this.setState({
+            isEditFlag: editFlag,
+            isOpenModel: isModelOpen,
+            FreightId: FreightId,
+        })
     }
 
     /**
@@ -38,64 +60,31 @@ class FreightMaster extends Component {
      * @description  used to open filter form 
      */
     openModel = () => {
-        this.setState({ isOpen: true, isEditFlag: false })
+        this.setState({
+            isOpen: true,
+            isEditFlag: false,
+        })
     }
 
-    /**
+      /**
      * @method onCancel
      * @description  used to cancel filter form
      */
     onCancel = () => {
-        this.setState({ isOpen: false })
-    }
-
-     /**
-    * @method editDetails
-    * @description confirm delete bop
-    */
-    editDetails = (Id) => {
         this.setState({
-            isEditFlag: true,
-            isOpen: true,
-            freightId: Id,
+            isOpen: false,
+        }, () => {
+            this.props.getFreightDetailAPI(res => { });
         })
     }
 
-    /**
-    * @method delete 
-    * @description confirm delete bop
-    */
-    deleteBOP = (Id) => {
-        const toastrConfirmOptions = {
-            onOk: () => {
-                this.confirmDelete(Id)
-            },
-            onCancel: () => console.log('CANCEL: clicked')
-        };
-        return toastr.confirm(`${MESSAGES.CONFIRM_DELETE} freight ?`, toastrConfirmOptions);
-    }
-
-    /**
-    * @method confirmDelete
-    * @description confirm delete bought out part
-    */
-    confirmDelete = (Id) => {
-        this.props.deleteFreightAPI(Id, (res) => {
-            if (res.data.Result) {
-                toastr.success(MESSAGES.DELETE_FREIGHT_SUCCESS);
-                this.props.getFreightDetailAPI(res => { });
-            } else {
-                toastr.error(MESSAGES.SOME_ERROR);
-            }
-        });
-    }
 
     /**
     * @method render
     * @description Renders the component
     */
     render() {
-        const { isOpen, isEditFlag, freightId, freightType } = this.state;
+        const { isOpen, isEditFlag, freightId } = this.state;
         return (
             <Container className="top-margin">
             {/* {this.props.loading && <Loader/>} */}
@@ -108,84 +97,35 @@ class FreightMaster extends Component {
                     </Col>
                 </Row>
                 <hr />
-                <Row>
-                    <Col>
-                        <h5>{`${CONSTANT.FREIGHT} ${CONSTANT.MASTER} ${CONSTANT.DETAILS}`}</h5>
-                    </Col>
-                </Row>
-                <Col>
-                <Table className="table table-striped" bordered>
-                { this.props.freightDetail && this.props.freightDetail.length > 0 &&
-                    <thead>
-                        <tr>
-                        <th>{`${CONSTANT.FREIGHT} ${CONSTANT.TYPE}`}</th>
-                        <th>{`${CONSTANT.PLANT} ${CONSTANT.NAME}`}</th>
-                        {freightType !== 2 &&<th>{`${CONSTANT.SOURCE} ${CONSTANT.CITY} ${CONSTANT.NAME}`}</th>}
-                        {freightType !== 2 &&<th>{`${CONSTANT.DESTINATION} ${CONSTANT.CITY} ${CONSTANT.NAME}`}</th> }
-                        {freightType !== 2 &&<th>{`${CONSTANT.FREIGHT} To From`}</th>}
-                        {freightType !== 2 &&<th>{`Part Truck Load Rate PerKilogram`}</th>}
-                        {freightType !== 2 &&<th>{`Part Truck Load Rate CubicFeet`}</th>}
-                        {freightType !== 2 &&<th>{`FullTruck Load Rate OneTon`}</th>}
-                        {freightType !== 2 &&<th>{`Full Truck Load Rate TwoTon`}</th>}
-                        {freightType !== 2 &&<th>{`Full Truck Load Rate FiveTon`}</th>}
-                        {freightType !== 2 &&<th>{`Full Truck Load Rate NineTon`}</th> }
-                        {freightType !== 2 &&<th>{`Full Truck Load Rate ElevenTon`}</th>}
-                        {freightType !== 2 &&<th>{`Full Truck Load Rate SixteenTon`}</th>}
-                        {freightType !== 2 &&<th>{`Full Truck Load Rate Twenty FiveTon`}</th>}
-                        {freightType !== 2 &&<th>{`Full Truck Load Rate Thirty OneTon`}</th>}
-                        {freightType !== 2 &&<th>{`Full Truck Load Rate Trailer`}</th>}
-                        {freightType === 2 &&<th>{`Supplier Name`}</th>}
-                        {freightType === 2 &&<th>{`Per Trip Cost`}</th> }
-                        {freightType === 2 &&<th>{`Packaging Costing Head `}</th>}
-                        {freightType === 2 &&<th>{`Packaging cost`}</th>}
-                        {freightType === 2 &&<th>{`Per Kilogram`}</th>}
-                        {freightType === 2 &&<th>{`Loding Unloading Costing Heads`}</th>}
-                        {freightType === 2 &&<th>{`LodingUnloading`}</th>}
-                        <th>{`${CONSTANT.DATE}`}</th>
-                        <th>{'Status '}</th>
-                        </tr>
-                    </thead>}
-                    <tbody > 
-                        {this.props.freightDetail && this.props.freightDetail.length > 0 &&
-                            this.props.freightDetail.map((item, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{item.FreightType}</td>
-                                        <td>{item.PlantName}</td> 
-                                        { item.FreightType === 1 &&<td >{item.SourceCityName}</td>}
-                                        { item.FreightType === 1 &&<td>{item.DestinationCityName}</td>}
-                                        { item.FreightType === 1 &&<td>{item.FreightToFrom}</td>}
-                                        { item.FreightType === 1 &&<td>{item.PartTruckLoadRatePerKilogram}</td>}
-                                        { item.FreightType === 1 &&<td>{item.PartTruckLoadRateCubicFeet}</td>} 
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateOneTon}</td>}
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateTwoTon}</td>} 
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateFiveTon}</td>}
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateNineTon}</td> }
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateElevenTon}</td>}
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateSixteenTon}</td> }
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateTwentyFiveTon}</td>}
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateThirtyOneTon}</td> }
-                                        { item.FreightType === 1 &&<td>{item.FullTruckLoadRateTrailer}</td>}
-                                        { item.FreightType === 2 &&<td>{item.SupplierId}</td> }
-                                        { item.FreightType === 2 &&<td>{item.PerTrip}</td>}
-                                        { item.FreightType === 2 &&<td>{item.PackagingCostingHeadsId}</td> }
-                                        { item.FreightType === 2 &&<td>{item.Packaging}</td>}
-                                        { item.FreightType === 2 &&<td>{item.PerKilogram}</td> }
-                                        { item.FreightType === 2 &&<td>{item.LodingUnloadingCostingHeadsId}</td>}
-                                        { item.FreightType === 2 &&<td>{item.LodingUnloading}</td>}
-                                        <td>{convertISOToUtcDate(item.CreatedDate)}</td>  
-                                        <td>{item.IsActive}</td>
-                                        <td>
-                                            <Button className="btn btn-secondary" onClick={() => this.editDetails(item.FreightId)}><i className="fas fa-pencil-alt"></i></Button>
-                                            <Button className="btn btn-danger" onClick={() => this.deleteBOP(item.FreightId)}><i className="far fa-trash-alt"></i></Button>
-                                        </td>  
-                                    </tr>
-                                )
-                            })}
-                            {this.props.freightDetail === undefined && <NoContentFound title={CONSTANT.EMPTY_DATA} />}
-                    </tbody>  
-                </Table> 
-                </Col>
+                <div>
+                    <Nav tabs className="subtabs">
+                        <NavItem>
+                            <NavLink className={classnames({ active: this.state.activeTab === '1' })} onClick={() => { this.toggle('1'); }}>
+                                {`Freight`}
+                            </NavLink>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLink className={classnames({ active: this.state.activeTab === '2' })} onClick={() => { this.toggle('2'); }}>
+                                {`Packaging`}
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                    <TabContent activeTab={this.state.activeTab}>
+                        <TabPane tabId="1">
+                            <FreightDetail 
+                                editFreightDetails={this.editFreighteDetails}
+                                toggle={this.toggle} 
+                            />
+                        </TabPane>
+                        <TabPane tabId="2">
+                            <PackagingDetail 
+                              editPackagingDetails={this.editPackagingDetails}
+                              toggle={this.toggle}
+                            />
+                        </TabPane>
+                    </TabContent>
+                </div>
                 {isOpen && (
                     <Addfreight
                         isOpen={isOpen}
@@ -204,13 +144,12 @@ class FreightMaster extends Component {
 * @description return state to component as props
 * @param {*} state
 */
-function mapStateToProps({ freight }) {
-    const { freightDetail ,loading } = freight;
-    return { freightDetail, loading }
+function mapStateToProps() {
+    return { }
 }
 
 
 export default connect(
-    mapStateToProps, { getFreightDetailAPI,deleteFreightAPI }
+    mapStateToProps, {getFreightDetailAPI}
 )(FreightMaster);
 
