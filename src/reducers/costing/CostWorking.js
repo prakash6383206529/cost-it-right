41f33439720fed1172cf1001b1776fe34218c1db
priyankashrivas <priyanka.shriwas@systematix.com>
2019-11-08 17:42:23 +0530

    M1[CIR-T-251] making action creator for add, get and create weight calculation, Making initial Componet fields

---
+++ src/reducers/costing/CostWorking.js
@@ -0,0 +1,50 @@
+import {
+    API_REQUEST,
+    API_FAILURE,
+    GET_WEIGHT_CALC_INFO_SUCCESS,
+    CREATE_WEIGHT_CALC_COSTING_SUCCESS,
+    UPDATE_WEIGHT_CALC_SUCCESS,
+    GET_WEIGHT_CALC_LAYOUT_SUCCESS
+} from '../../config/constants';
+
+const initialState = {
+
+};
+
+export default function CostWorkingReducer(state = initialState, action) {
+    switch (action.type) {
+        case API_REQUEST:
+            return {
+                ...state,
+                loading: true
+            };
+        case API_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: true
+            };
+        case GET_WEIGHT_CALC_INFO_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: true,
+                weightCostingInfo: action.payload
+            };
+        case CREATE_WEIGHT_CALC_COSTING_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: true,
+            };
+        case GET_WEIGHT_CALC_LAYOUT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: true,
+                weightLayoutType: action.payload
+            };
+        default:
+            return state;
+    }
+}
