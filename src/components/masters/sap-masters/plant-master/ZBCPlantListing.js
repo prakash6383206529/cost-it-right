c2c0904a15a8b88110c9118a4b1502ab1fb94c13
Poonam kungwani <poonam.kungwani@systematixindia.com>
2020-08-11 15:37:18 +0530

    M1-CIR-T100 changes in add icons add 11-08

--- src/components/masters/sap-masters/plant-master/ZBCPlantListing.js
+++ src/components/masters/sap-masters/plant-master/ZBCPlantListing.js
@@ -448,7 +448,7 @@ class ZBCPlantListing extends Component {
                     trClassName={'userlisting-row'}
                     tableHeaderClass='my-custom-header'
                     pagination>
-                    <TableHeaderColumn dataField="Sr. No." width={'70'} dataFormat={this.indexFormatter}>Sr. No.</TableHeaderColumn>
+                    <TableHeaderColumn dataField="Sr. No." width={'70'} dataFormat={this.indexFormatter} dataSort={true}>Sr. No.</TableHeaderColumn>
                     <TableHeaderColumn dataField="IsVendor" hidden={true} dataFormat={this.checkIsVendorFormatter} dataAlign="center" dataSort={true}>Is Vendor</TableHeaderColumn>
                     <TableHeaderColumn dataField="PlantName" dataAlign="center" dataSort={true}>Plant Name</TableHeaderColumn>
                     <TableHeaderColumn dataField="PlantCode" dataAlign="center" dataSort={true}>Plant Code</TableHeaderColumn>
