88e2b2860c1a28a8124c678bfb23f6a766159883
Mahak <mahak.rawat@systematixindia.com>
2021-04-10 12:09:10 +0530

    Simulation history list coming from action,Simulation RM domestic listing

--- src/components/simulation/components/Simulation.js
+++ src/components/simulation/components/Simulation.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SearchableSelectHookForm } from '../../layout/HookFormInputs';
 import RMDomesticListing from '../../masters/material-master/RMDomesticListing';
 import { Row, Col } from 'reactstrap'
+import { Controller, useForm } from 'react-hook-form';
+import { getSelectListOfMasters } from '../actions/Simulation';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Simulation(props) {
+
     let options = {}
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getSelectListOfMasters(() => { }))
+    }, [])
+
+    const { register, handleSubmit, control, setValue, errors, getValues } = useForm({
+        mode: 'onBlur',
+        reValidateMode: 'onChange',
+    })
+    const masterList = useSelector(state => state.simulation.masterSelectList)
+    console.log(masterList, "MASTER LIST");
+    const [master, setMaster] = useState({ label: 'RM Domestic', value: "0" })
+
+    const handleMasterChange = (e) => {
+        setMaster(e.target.value)
+    }
+
+    const renderListing = (label) => {
+        let temp = []
+        if (label === 'masters') {
+            masterList && masterList.map((item) => {
+                if (item.Value === '0') return false
+                temp.push({ label: item.Text, value: item.Value })
+                return null
+            })
+            return temp
+        }
+    }
+
     return (
         <div>
             <Row>
@@ -13,12 +48,29 @@ function Simulation(props) {
                 </Col>
             </Row>
             <Row>
-                <Col>
+                <Col md="1">
                     <div>Masters:</div>
                 </Col>
-                {/* <SearchableSelectHookForm /> */}
+                <Col md="3">
+                    <div className="flex-fill filled-small hide-label">
+                        <SearchableSelectHookForm
+                            label={''}
+                            name={'Masters'}
+                            placeholder={'Masters'}
+                            Controller={Controller}
+                            control={control}
+                            rules={{ required: false }}
+                            register={register}
+                            // defaultValue={plant.length !== 0 ? plant : ''}
+                            options={renderListing('masters')}
+                            mandatory={false}
+                            handleChange={() => { }}
+                            errors={errors.Masters}
+                        />
+                    </div>
+                </Col>
             </Row>
-            {/* <RMDomesticListing /> */}
+            <RMDomesticListing />
         </div>
     );
 }
