79c2c2c6b69fce8fa7883dfa13720ccc14694448
harish.patel <harish.patel@systematixindia.com>
2021-04-29 16:26:17 +0530

    Merged Mahak's Code and simulation upload hide from menu

--- src/config/constants.js
+++ src/config/constants.js
@@ -1459,6 +1459,7 @@ export const DIMENSIONLESS = 'Dimensionless'
 //UOM CATEGORY
 export const STD = 'STD'
 
+
 //SIMULATION MASTER NAME
 export const RMDOMESTIC = 'Raw Material(Domestic)'
 export const RMIMPORT = 'Raw Material(Import)'
