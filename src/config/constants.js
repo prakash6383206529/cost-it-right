76e99e584cf1494a1908855a689399e0347758f4
harish.patel <harish.patel@systematixindia.com>
2021-05-05 17:23:03 +0530

    Other cost API unusual call and merged code

--- src/config/constants.js
+++ src/config/constants.js
@@ -1466,6 +1466,7 @@ export const DIMENSIONLESS = 'Dimensionless'
 //UOM CATEGORY
 export const STD = 'STD'
 
+
 //SIMULATION MASTER NAME
 export const RMDOMESTIC = 'Raw Material(Domestic)'
 export const RMIMPORT = 'Raw Material(Import)'
