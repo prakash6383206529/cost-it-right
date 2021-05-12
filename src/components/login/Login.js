9bcdc6f9560293e4fbe39007e0c86b599d494d90
ankitm.verma <ankitm.verma@systematixindia.com>
2021-05-12 21:08:31 +0530

    logo changes and client master heading changed to customer master

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -91,7 +91,7 @@ class Login extends Component {
               <div className="col-md-5 form-section">
 
                 <div className="text-center">
-                  <a href="javaScript:Void(0);"><img className="logo-first" src={require("../../assests/images/minda-logo.png")} alt="Minda" /></a>
+                  <a href="javaScript:Void(0);"><img className="logo-first" src={require("../../assests/images/minda-logo1x.png")} alt="Minda" /></a>
                 </div>
                 <h3 className="text-center">Welcome Back,<br /> Please login to your account</h3>
                 {/* <p>Welcome Back, Please login to your account</p> */}
