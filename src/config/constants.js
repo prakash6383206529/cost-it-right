5f8db669f4970c4a0bad53e2249e16797c51eaa0
harish.patel <harish.patel@systematixindia.com>
2021-05-18 12:35:14 +0530

    Merged code

--- src/config/constants.js
+++ src/config/constants.js
@@ -1481,6 +1481,7 @@ export const DIMENSIONLESS = 'Dimensionless'
 //UOM CATEGORY
 export const STD = 'STD'
 
+
 //SIMULATION MASTER NAME
 export const RMDOMESTIC = 'Raw Material(Domestic)'
 export const RMIMPORT = 'Raw Material(Import)'
