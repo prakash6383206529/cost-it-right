3d72fc32dc20d419148b9e9777791f9f84949cbb
harish.patel <harish.patel@systematixindia.com>
2021-05-24 11:02:51 +0530

    Quantity issue fixes

--- src/config/constants.js
+++ src/config/constants.js
@@ -33,6 +33,7 @@ export const FILE_URL = 'http://10.10.1.100:10132/';
 //MINDA PRODUCTION FILE URL
 // export const FILE_URL = 'https://apicostmanagement.unominda.com/';
 
+
 /** Export API */
 export const API = {
 
