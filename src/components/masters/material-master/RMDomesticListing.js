feaf0400a271e022aa4b81c2ad02b6c8ad1ddafe
Mahak <mahak.rawat@systematixindia.com>
2021-06-22 01:01:19 +0530

    Conflict resolved for listing

--- src/components/masters/material-master/RMDomesticListing.js
+++ src/components/masters/material-master/RMDomesticListing.js
@@ -13,7 +13,7 @@ import { CONSTANT } from '../../../helper/AllConastant';
 import NoContentFound from '../../common/NoContentFound';
 import { MESSAGES } from '../../../config/message';
 import { toastr } from 'react-redux-toastr';
-import { BootstrapTable, TableHeaderColumn,ExportCSVButton  } from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn, ExportCSVButton } from 'react-bootstrap-table';
 import InputRange from 'react-input-range';
 import 'react-input-range/lib/css/index.css'
 import moment from 'moment';
@@ -23,7 +23,7 @@ import ConfirmComponent from "../../../helper/ConfirmComponent";
 import LoaderCustom from '../../common/LoaderCustom';
 import { costingHeadObjs } from '../../../config/masterData';
 import { getPlantSelectListByType, getTechnologySelectList } from '../../../actions/Common'
-import { ZBC,RmDomestic } from '../../../config/constants'
+import { ZBC, RmDomestic } from '../../../config/constants'
 
 class RMDomesticListing extends Component {
     constructor(props) {
@@ -515,15 +515,15 @@ class RMDomesticListing extends Component {
         let products = []
         products = this.props.rmDataList
         return products; // must return the data which you want to be exported
-      }
+    }
 
     createCustomExportCSVButton = (onClick) => {
         return (
-          <ExportCSVButton btnText='Download' onClick={ () => this.handleExportCSVButtonClick(onClick) }/>
+            <ExportCSVButton btnText='Download' onClick={() => this.handleExportCSVButtonClick(onClick)} />
         );
-      }
+    }
 
-    
+
 
     /**
     * @method render
@@ -736,7 +736,7 @@ class RMDomesticListing extends Component {
                             multiColumnSearch={true}
                             // exportCSV={true}
                             //ignoreSinglePage
-                            exportCSV = {this.props.isSimulation ? false : true}
+                            exportCSV={this.props.isSimulation ? false : true}
                             csvFileName={`${RmDomestic}.csv`}
                             ref={'table'}
                             pagination>
