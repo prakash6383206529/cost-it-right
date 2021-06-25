c494f42b58c502074da81fd41723bf4a3c2fed5c
harish.patel <harish.patel@systematixindia.com>
2021-06-25 19:50:51 +0530

    RM Simulation duplicate entry fixes

--- src/components/simulation/components/SimulationPages/RMSimulation.js
+++ src/components/simulation/components/SimulationPages/RMSimulation.js
@@ -68,7 +68,7 @@ function RMSimulation(props) {
 
         let tempArr = []
         list && list.map(item => {
-            if (item.NewBasicRate || item.NewScrapRate) {
+            if ((item.NewBasicRate !== undefined || item.NewScrapRate !== undefined) && ((item.NewBasicRate !== undefined ? Number(item.NewBasicRate) : Number(item.BasicRate)) !== Number(item.BasicRate) || (item.NewScrapRate !== undefined ? Number(item.NewScrapRate) : Number(item.ScrapRate)) !== Number(item.ScrapRate))) {
                 let tempObj = {}
                 tempObj.CostingHead = item.CostingHead
                 tempObj.RawMaterialName = item.RawMaterial
@@ -87,19 +87,19 @@ function RMSimulation(props) {
                 tempObj.NewNetLandedCost = Number(item.NewBasicRate ? item.NewBasicRate : item.BasicRate) + checkForNull(item.RMShearingCost) + checkForNull(item.RMFreightCost)
                 tempObj.EffectiveDate = item.EffectiveDate
                 tempArr.push(tempObj)
-            } else {
-                return false
             }
+            return null;
         })
         obj.SimulationRawMaterials = tempArr
+        console.log('obj: ', obj);
 
-        dispatch(runVerifySimulation(obj, res => {
+        // dispatch(runVerifySimulation(obj, res => {
 
-            if (res.data.Result) {
-                setToken(res.data.Identity)
-                setShowVerifyPage(true)
-            }
-        }))
+        //     if (res.data.Result) {
+        //         setToken(res.data.Identity)
+        //         setShowVerifyPage(true)
+        //     }
+        // }))
     }
 
     const cancelVerifyPage = () => {
@@ -189,7 +189,7 @@ function RMSimulation(props) {
     const newBasicRateFormatter = (cell, row, enumObject, rowIndex) => {
         return (
             <>
-                <span className={`${!isbulkUpload ? 'form-control' : ''}`} >{cell ? cell : row.BasicRate} </span>
+                <span className={`${!isbulkUpload ? 'form-control' : ''}`} >{cell ? Number(cell) : Number(row.BasicRate)} </span>
             </>
         )
     }
@@ -197,7 +197,7 @@ function RMSimulation(props) {
     const newScrapRateFormatter = (cell, row, enumObject, rowIndex) => {
         return (
             <>
-                <span className={`${!isbulkUpload ? 'form-control' : ''}`} >{cell ? cell : row.ScrapRate}</span>
+                <span className={`${!isbulkUpload ? 'form-control' : ''}`} >{cell ? Number(cell) : Number(row.ScrapRate)}</span>
             </>
         )
     }
