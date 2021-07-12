6d9983c851bc03a747dda673339616ac3f428ac9
harish.patel <harish.patel@systematixindia.com>
2021-07-12 09:57:07 +0530

    Set value changes in costing details

--- src/components/layout/HookFormInputs.js
+++ src/components/layout/HookFormInputs.js
@@ -35,6 +35,7 @@ export const TextFieldHooks = (input) => {
 
 
 export const TextFieldHookForm = (field) => {
+  console.log('first field: ', field);
   const { label, Controller, control, register, name, defaultValue, mandatory, errors, rules, handleChange } = field
   // console.log('register: ', register);
   // console.log('name: ', name);
@@ -58,6 +59,7 @@ export const TextFieldHookForm = (field) => {
           {...register}
           defaultValue={defaultValue}
           render={({ field: { onChange, onBlur, value } }) => {
+            console.log('field: ', field);
 
             return (
               <input
