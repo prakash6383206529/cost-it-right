714c45e0c655d271d3f68b13402385c2aa61556f
ankitm.verma <ankitm.verma@systematixindia.com>
2021-07-14 18:33:57 +0530

    listing top searchbox text

--- src/components/costing/components/Drawers/AddBOP.js
+++ src/components/costing/components/Drawers/AddBOP.js
@@ -380,7 +380,7 @@ function AddBOP(props) {
                 </BootstrapTable> */}
                   <div className="ag-grid-wrapper" style={{ width: '100%', height: '100%' }}>
                     <div className="ag-grid-header">
-                      <input type="text" className="form-control table-search" id="filter-text-box" placeholder="Search" onChange={(e) => onFilterTextBoxChanged(e)} />
+                      <input type="text" className="form-control table-search" id="filter-text-box" placeholder="Search " onChange={(e) => onFilterTextBoxChanged(e)} />
                     </div>
                     <div
                       className="ag-theme-material"
