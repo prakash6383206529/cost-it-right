1b1b17d3cb376680413ba2951940d6c2a9cb29e4
harish.patel <harish.patel@systematixindia.com>
2021-07-20 20:38:39 +0530

    Resolved cherry pick conflict

--- src/components/simulation/components/CostingSimulation.js
+++ src/components/simulation/components/CostingSimulation.js
@@ -645,6 +645,11 @@ function CostingSimulation(props) {
                                         {'Send For Approval'}
                                     </button>
 
+                                    <button className="user-btn mr5 save-btn" onClick={VerifyImpact}>
+                                        <div className={"save-icon"}></div>
+                                        {"Verify Impact"}
+                                    </button>
+
                                     <button
                                         type="button"
                                         className="user-btn mr5 save-btn"
@@ -653,13 +658,6 @@ function CostingSimulation(props) {
                                         <div className={"back-icon"}></div>
                                         {"Go to History"}
                                     </button>
-
-                                    {/* <button className="user-btn mr5 save-btn" onClick={VerifyImpact}>
-                                        <div className={"save-icon"}></div>
-                                        {"Verify Impact"}
-                                    </button> */}
-
-
 
                                 </div>
                             </Row>
