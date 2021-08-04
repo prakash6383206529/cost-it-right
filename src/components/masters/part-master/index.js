4dbaec3f931df9e45c3beb46710398e4d10c563d
Mahak <mahak.rawat@systematixindia.com>
2021-08-04 18:32:26 +0530

    Product master in progress

--- src/components/masters/part-master/index.js
+++ src/components/masters/part-master/index.js
@@ -11,6 +11,8 @@ import { checkPermission } from '../../../helper/util';
 import { reactLocalStorage } from 'reactjs-localstorage';
 import { loggedInUserId } from '../../../helper/auth';
 import { getLeftMenu, } from '../../../actions/auth/AuthActions';
+import IndivisualProductListing from './IndivisualProductListing';
+import AddIndivisualProduct from './AddIndivisualProduct';
 
 class PartMaster extends Component {
     constructor(props) {
@@ -20,6 +22,7 @@ class PartMaster extends Component {
             activeTab: '1',
             isAddBOMForm: false,
             isPartForm: false,
+            isProductForm: false,
             getDetails: {},
 
             ViewAccessibility: false,
@@ -39,7 +42,7 @@ class PartMaster extends Component {
                 let Data = leftMenuData;
                 const accessData = Data && Data.find(el => el.PageName === PART)
                 const permmisionData = accessData && accessData.Actions && checkPermission(accessData.Actions)
-                console.log(permmisionData,'permmisionDatapermmisionData')
+                console.log(permmisionData, 'permmisionDatapermmisionData')
                 if (permmisionData !== undefined) {
                     this.setState({
                         ViewAccessibility: permmisionData && permmisionData.View ? permmisionData.View : false,
@@ -79,7 +82,7 @@ class PartMaster extends Component {
 
     //HIDE BOM & PART INDIVIDUAL FORM
     hideForm = () => {
-        this.setState({ isAddBOMForm: false, isPartForm: false, getDetails: {}, })
+        this.setState({ isAddBOMForm: false, isPartForm: false, isProductForm: false, getDetails: {}, })
     }
 
     //DISPLAY INDIVIDUAL PART FORM
@@ -92,12 +95,22 @@ class PartMaster extends Component {
         this.setState({ getDetails: data, isPartForm: true, isAddBOMForm: false, })
     }
 
+    //DISPLAY INDIVIDUAL PART FORM
+    displayIndividualProductForm = () => {
+        this.setState({ isProductForm: true, getDetails: {} })
+    }
+
+    //GET DETAILS OF INDIVIDUAL PART
+    getIndividualProductDetails = (data) => {
+        this.setState({ getDetails: data, isProductForm: true, isAddBOMForm: false, })
+    }
+
     /**
     * @method render
     * @description Renders the component
     */
     render() {
-        const { isAddBOMForm, isPartForm, } = this.state;
+        const { isAddBOMForm, isPartForm, isProductForm } = this.state;
 
         if (isAddBOMForm === true) {
             return <AddAssemblyPart
@@ -114,54 +127,78 @@ class PartMaster extends Component {
             />
         }
 
+        if (isProductForm === true) {
+            return <AddIndivisualProduct
+                hideForm={this.hideForm}
+                data={this.state.getDetails}
+            />
+        }
+
         return (
             <>
-            <div className="container-fluid">
-                <div className="user-page p-0">
-                    {/* {this.props.loading && <Loader/>} */}
-                    <div>
-                        <h1>Part Master</h1>
-                        <Nav tabs className="subtabs mt-0">
-                            <NavItem>
-                                <NavLink className={classnames({ active: this.state.activeTab === '1' })} onClick={() => { this.toggle('1'); }}>
-                                    Manage Assembly Part
+                <div className="container-fluid">
+                    <div className="user-page p-0">
+                        {/* {this.props.loading && <Loader/>} */}
+                        <div>
+                            <h1>Part Master</h1>
+                            <Nav tabs className="subtabs mt-0">
+                                <NavItem>
+                                    <NavLink className={classnames({ active: this.state.activeTab === '1' })} onClick={() => { this.toggle('1'); }}>
+                                        Manage Assembly Part
                                 </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className={classnames({ active: this.state.activeTab === '2' })} onClick={() => { this.toggle('2'); }}>
-                                    Manage Component/Part
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className={classnames({ active: this.state.activeTab === '2' })} onClick={() => { this.toggle('2'); }}>
+                                        Manage Component/Part
                                 </NavLink>
-                            </NavItem>
-                        </Nav>
-                        <TabContent activeTab={this.state.activeTab}>
-                            {this.state.activeTab === '1' &&
-                                <TabPane tabId="1">
-                                    <AssemblyPartListing
-                                        displayForm={this.displayForm}
-                                        getDetails={this.getDetails}
-                                        AddAccessibility={this.state.AddAccessibility}
-                                        EditAccessibility={this.state.EditAccessibility}
-                                        DeleteAccessibility={this.state.DeleteAccessibility}
-                                        BulkUploadAccessibility={this.state.BulkUploadAccessibility}
-                                        DownloadAccessibility={this.state.DownloadAccessibility}
-                                    />
-                                </TabPane>}
-                            {this.state.activeTab === '2' &&
-                                <TabPane tabId="2">
-                                    <IndivisualPartListing
-                                        formToggle={this.displayIndividualForm}
-                                        getDetails={this.getIndividualPartDetails}
-                                        AddAccessibility={this.state.AddAccessibility}
-                                        EditAccessibility={this.state.EditAccessibility}
-                                        DeleteAccessibility={this.state.DeleteAccessibility}
-                                        BulkUploadAccessibility={this.state.BulkUploadAccessibility}
-                                        DownloadAccessibility={this.state.DownloadAccessibility}
-                                    />
-                                </TabPane>}
-                        </TabContent>
-                    </div>
-                </div >
-            </div>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className={classnames({ active: this.state.activeTab === '3' })} onClick={() => { this.toggle('3'); }}>
+                                        Manage Products
+                                </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <TabContent activeTab={this.state.activeTab}>
+                                {this.state.activeTab === '1' &&
+                                    <TabPane tabId="1">
+                                        <AssemblyPartListing
+                                            displayForm={this.displayForm}
+                                            getDetails={this.getDetails}
+                                            AddAccessibility={this.state.AddAccessibility}
+                                            EditAccessibility={this.state.EditAccessibility}
+                                            DeleteAccessibility={this.state.DeleteAccessibility}
+                                            BulkUploadAccessibility={this.state.BulkUploadAccessibility}
+                                            DownloadAccessibility={this.state.DownloadAccessibility}
+                                        />
+                                    </TabPane>}
+                                {this.state.activeTab === '2' &&
+                                    <TabPane tabId="2">
+                                        <IndivisualPartListing
+                                            formToggle={this.displayIndividualForm}
+                                            getDetails={this.getIndividualPartDetails}
+                                            AddAccessibility={this.state.AddAccessibility}
+                                            EditAccessibility={this.state.EditAccessibility}
+                                            DeleteAccessibility={this.state.DeleteAccessibility}
+                                            BulkUploadAccessibility={this.state.BulkUploadAccessibility}
+                                            DownloadAccessibility={this.state.DownloadAccessibility}
+                                        />
+                                    </TabPane>}
+                                {this.state.activeTab === '3' &&
+                                    <TabPane tabId="3">
+                                        <IndivisualProductListing
+                                            formToggle={this.displayIndividualProductForm}
+                                            getDetails={this.getIndividualProductDetails}
+                                            AddAccessibility={this.state.AddAccessibility}
+                                            EditAccessibility={this.state.EditAccessibility}
+                                            DeleteAccessibility={this.state.DeleteAccessibility}
+                                            BulkUploadAccessibility={this.state.BulkUploadAccessibility}
+                                            DownloadAccessibility={this.state.DownloadAccessibility}
+                                        />
+                                    </TabPane>}
+                            </TabContent>
+                        </div>
+                    </div >
+                </div>
             </ >
         );
     }
