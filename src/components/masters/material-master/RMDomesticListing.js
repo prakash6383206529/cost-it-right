a033a31e1d17cc068bfbcf1d98f1c37c2eeda106
Mahak <mahak.rawat@systematixindia.com>
2021-09-07 16:07:28 +0530

    Filtered data problem solved

--- src/components/masters/material-master/RMDomesticListing.js
+++ src/components/masters/material-master/RMDomesticListing.js
@@ -26,7 +26,7 @@ import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import ReactExport from 'react-export-excel';
-import { CheckApprovalApplicableMaster, getConfigurationKey } from '../../../helper';
+import { CheckApprovalApplicableMaster, getConfigurationKey, getFilteredRMData } from '../../../helper';
 
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
@@ -113,8 +113,7 @@ class RMDomesticListing extends Component {
 
     getFilterRMData = () => {
         if (this.props.isSimulation && CheckApprovalApplicableMaster(RM_MASTER_ID)) {
-            const list = this.props.rmDataList && this.props.rmDataList.filter((item => item.IsRMAssociated === true))
-            return list
+            return getFilteredRMData(this.props.rmDataList)
         } else {
             return this.props.rmDataList
         }
@@ -914,6 +913,7 @@ class RMDomesticListing extends Component {
                                     {(!this.props.isSimulation && !this.props.isMasterSummaryDrawer) && <AgGridColumn width={160} field="RawMaterialId" headerName="Action" type="rightAligned" cellRenderer={'totalValueRenderer'}></AgGridColumn>}
                                     <AgGridColumn field="VendorId" hide={true}></AgGridColumn>
                                     <AgGridColumn field="TechnologyId" hide={true}></AgGridColumn>
+                                    <AgGridColumn field="IsRMAssociated"></AgGridColumn>
 
                                 </AgGridReact>
                                 <div className="paging-container d-inline-block float-right">
