c16bcd30786dcdf356c4ca8cdc696c20687aad02
Ashok Makhija <ashok.makhija@systematixindia.com>
2021-10-07 16:49:46 +0530

    part master change 1) add assembly 2) add individual part change by ashok

--- src/components/masters/part-master/AddIndivisualPart.js
+++ src/components/masters/part-master/AddIndivisualPart.js
@@ -64,9 +64,9 @@ class AddIndivisualPart extends Component {
         if (res && res.data && res.data.Result) {
           const Data = res.data.Data;
           let productArray = []
-          console.log(Data, "new")
+
           Data && Data.GroupCodeList.map((item) => {
-            productArray.push({ Text: item.GroupCode, Value: "", })
+            productArray.push({ Text: item.GroupCode, Value: item.GroupCode, })
             return productArray
           })
           this.setState({ DataToCheck: Data })
@@ -437,6 +437,7 @@ class AddIndivisualPart extends Component {
                             <Field
                               label="Group Code"
                               name="ProductGroup"
+                              type="text"
                               placeholder={"Select"}
                               selection={
                                 this.state.ProductGroup == null || this.state.ProductGroup.length === 0 ? [] : this.state.ProductGroup}
