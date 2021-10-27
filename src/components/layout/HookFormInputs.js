1eece9b31a95e99dbcde45288c837256fc1a5165
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2021-10-27 11:30:39 +0530

    large data coming in Dropdown , show only when user inputs text

--- src/components/layout/HookFormInputs.js
+++ src/components/layout/HookFormInputs.js
@@ -2,6 +2,7 @@ import React from "react";
 import Select from "react-select";
 import "./formInputs.css";
 import ReactDatePicker from 'react-datepicker'
+import AsyncSelect from 'react-select/async';
 
 export const TextFieldHooks = (input) => {
 
@@ -398,5 +399,60 @@ export const RadioHookForm = (field) => {
         {errors && (errors.message || errors.type) ? <div className="text-help">{(errors.message || errors.type)}</div> : ""}
       </div>
     </>
+  )
+}
+
+
+export const AsyncSearchableSelectHookForm = (field) => {
+  const { name, label, Controller, mandatory, disabled, handleChange, rules, placeholder, defaultValue,
+    isClearable, control, errors, register, isLoading, customClassName, asyncOptions, message } = field;
+
+
+  let isDisable = (disabled && disabled === true) ? true : false;
+  let isLoader = (isLoading && isLoading === true) ? true : false;
+
+  return (
+    <div className={`w-100 mb-15 form-group-searchable-select ${customClassName}`}>
+      <label>
+        {label}
+        {mandatory && mandatory === true ? <span className="asterisk-required">*</span> : ''}
+      </label>
+      <Controller
+
+        name={name}
+        control={control}
+        rules={rules}
+        {...register}
+        defaultValue={defaultValue}
+        render={({ field: { onChange, onBlur, value, name } }) => {
+          return (
+            <AsyncSelect
+              {...field}
+              {...register}
+              name={name}
+              placeholder={placeholder}
+              isDisabled={disabled}
+              onChange={(e) => {
+                handleChange(e);
+                onChange(e)
+              }}
+              menuPlacement="auto"
+              loadOptions={asyncOptions}
+              onBlur={onBlur}
+              selected={value}
+              value={value}
+              isLoading={isLoader}
+            />
+          )
+
+        }}
+      />
+
+      {/* {errors && errors.type === 'required' ? <div className="text-help">'This field is required'</div> : ""} */}
+      {/* {errors && errors.type === 'required' ? '<div className="text-help">This field is required</div>' : ""} */}
+      {errors && errors.type === 'required' ? <div className="text-help">This field is required</div>
+        : errors && errors.type !== 'required' ? <div className="text-help">{(errors.message || errors.type)}</div> : ''}
+
+    </div>
   )
 }
