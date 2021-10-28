9fab4f7f38ee7e9a755abdcce748b7e1a72f0a7e
Mahak <mahak.rawat@systematixindia.com>
2021-10-29 00:11:41 +0530

    RUn simualtion.js conflcit resolved

--- src/components/simulation/components/RunSimulationDrawer.js
+++ src/components/simulation/components/RunSimulationDrawer.js
@@ -105,7 +105,7 @@ function RunSimulationDrawer(props) {
         }
 
 
-        if (elementObj.Text === "Additional Discount") {
+        if (elementObj.Text === "Additional Discount %") {
             setinputAdditionalDiscount(!inputAdditionalDiscount)
 
             setDisableDiscountAndOtherCost(!disableDiscountAndOtherCost)
@@ -166,7 +166,7 @@ function RunSimulationDrawer(props) {
         const DiscountOtherCost = selectedData.includes("Discount And Other Cost")
         const PaymentTerms = selectedData.includes("Payment Terms")
         const Inventory = selectedData.includes("Inventory")
-        const AdditionalDiscount = selectedData.includes("Additional Discount")
+        const AdditionalDiscount = selectedData.includes("Additional Discount %")
         const AdditionalOtherCost = selectedData.includes("Additional Other Cost")
 
         let temp = []
@@ -279,7 +279,7 @@ function RunSimulationDrawer(props) {
                                                     if (el.Value === '0') return false;
                                                     return (
                                                         <Col md="12" className="mb-3 p-0">
-                                                            <div class="custom-check1 d-inline-block">
+                                                            <div class={`custom-check1 d-inline-block ${el.Text ==="Additional Discount %" ? "drawer-side-input" : ''} ${el.Text==="Additional Other Cost" ? 'drawer-side-input-other': ''}`}>
                                                                 <label
                                                                     className="custom-checkbox mb-0"
                                                                     onChange={() => handleApplicabilityChange(el)}
@@ -289,7 +289,7 @@ function RunSimulationDrawer(props) {
                                                                     <input
                                                                         type="checkbox"
                                                                         value={"All"}
-                                                                        disabled={(el.Text === "Discount And Other Cost" && disableDiscountAndOtherCost) || (el.Text === "Additional Discount" && disableAdditionalDiscount) || (el.Text === "Additional Other Cost" && disableAdditionalOtherCost) ? true : false}
+                                                                        disabled={(el.Text === "Discount And Other Cost" && disableDiscountAndOtherCost) || (el.Text === "Additional Discount %" && disableAdditionalDiscount) || (el.Text === "Additional Other Cost" && disableAdditionalOtherCost) ? true : false}
                                                                         checked={IsAvailable(el.Value)}
                                                                     />
 
@@ -301,33 +301,26 @@ function RunSimulationDrawer(props) {
                                                                     />
                                                                 </label>
                                                                 {(el.Text === "Additional Other Cost") && inputOtherCost ?
-
-
-
-
-                                                                    <Fragment>
-
-
-
-
+                                                                 <Fragment>
+                                                                      <div className="toggle-button-per-and-fix">
+                                                                        <label className="normal-switch d-flex align-items-center pb-4 pt-3 w-fit"> <span className="mr-2">Fixed</span> 
                                                                         <Switch
-
                                                                             onChange={onChange}
-                                                                            //checked={}
+                                                                            checked={toggleSwitchLabel}
                                                                             id="normal-switch"
                                                                             disabled={false}
                                                                             background="#4DC771"
                                                                             onColor="#4DC771"
                                                                             onHandleColor="#ffffff"
                                                                             offColor="#4DC771"
-                                                                            uncheckedIcon={false}
-                                                                            checkedIcon={false}
+                                                                            uncheckedIcon={true}
+                                                                            checkedIcon={true}
                                                                             height={20}
                                                                             width={46}
-
                                                                         />
-
-                                                                        <div> {toggleSwitchLabel ? 'Percentage' : 'Fixed'}</div>
+                                                                          <span className="ml-2">Percentage</span>
+				                                                     	</label>
+                                                                        {/* <div> {toggleSwitchLabel ? 'Percentage' : 'Fixed'}</div> */}
                                                                         <TextFieldHookForm
                                                                             label=""
                                                                             name={"OtherCost"}
@@ -342,17 +335,14 @@ function RunSimulationDrawer(props) {
                                                                             errors={errors.OtherCost}
                                                                             disabled={false}
                                                                         />
-
-
-
+                                                                        </div>
                                                                     </Fragment>
-
 
                                                                     : " "
                                                                 }
 
 
-                                                                {(el.Text === "Additional Discount") && inputAdditionalDiscount ?
+                                                                {(el.Text === "Additional Discount %") && inputAdditionalDiscount ?
                                                                     <TextFieldHookForm
                                                                         label=""
                                                                         name={"Discount"}
