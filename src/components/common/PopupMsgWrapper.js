99ae80a0f514bd706ffac413c1f252ac3e8cd83c
Mahak <mahak.rawat@systematixindia.com>
2021-11-16 13:52:44 +0530

    Wrapper addedd

--- src/components/common/PopupMsgWrapper.js
+++ src/components/common/PopupMsgWrapper.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {Button,Modal,ModalHeader,ModalBody,ModalFooter} from 'reactstrap'
 
 function PopupMsgWrapper(props) {
-    
-    console.log("COMING IN POP P MESSAGE");
     return (             
         <Modal
           fade={false}
@@ -11,21 +9,21 @@ function PopupMsgWrapper(props) {
           isOpen={props.isOpen}
         >
           <ModalHeader toggle={props.closePopUp}>
-           {props.header}
+           {props.header ?props.header:'Confirm'}
           </ModalHeader>
           <ModalBody>
-           {props.message}
+           {props.message ?props.message:'You have changed details, So your all Pending for Approval costing will get Draft. Do you wish to continue?'}
           </ModalBody>
           <ModalFooter>
             <Button
               color="primary"
               onClick={props.confirmPopup}
             >
-             {props.firstButtonName}
+             {props.firstButtonName ?props.firstButtonName:'OK'}
             </Button>
             {' '}
             <Button onClick={props.closePopUp}>
-             {props.secondButtonName}
+             {props.secondButtonName ?props.secondButtonName:'Cancel'}
             </Button>
           </ModalFooter>
         </Modal>
--- src/components/simulation/components/RunSimulationDrawer.js
+++ src/components/simulation/components/RunSimulationDrawer.js
@@ -288,7 +288,7 @@ function RunSimulationDrawer(props) {
                                                     if (el.Value === '0') return false;
                                                     return (
                                                         <Col md="12" className="mb-3 p-0">
-                                                            <div class={`custom-check1 d-inline-block ${el.Text === "Additional Discount" ? "drawer-side-input" : ''} ${el.Text === "Additional Other Cost" ? 'drawer-side-input-other' : ''}`}>
+                                                            <div class={`custom-check1 d-inline-block drawer-side-input-other `}>
                                                                 <label
                                                                     className="custom-checkbox mb-0"
                                                                     onChange={() => handleApplicabilityChange(el)}
@@ -359,7 +359,7 @@ function RunSimulationDrawer(props) {
                                                                             <label className="normal-switch d-flex align-items-center pb-4 pt-3 w-fit"> <span className="mr-2">Fixed</span>
                                                                                 <Switch
                                                                                     onChange={onChangeAdditionalDiscount}
-                                                                                    checked={toggleSwitchLabel}
+                                                                                    checked={toggleSwitchAdditionalDiscount}
                                                                                     id="normal-switch"
                                                                                     disabled={false}
                                                                                     background="#4DC771"
@@ -501,7 +501,7 @@ function RunSimulationDrawer(props) {
 
                                             </Row>
                                             <Row>
-                                                <Col md="12">
+                                                <Col md="12" className="mt-4 warning-text-container">
                                                     <div className="warning-text">
                                                         <WarningMessage dClass="mr-3" message={"Unselected checkbox won't be applied in future"} />
                                                     </div>
