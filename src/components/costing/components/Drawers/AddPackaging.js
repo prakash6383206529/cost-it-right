8b0b5a73e04c77f051638c8a197a809cc2bc7acc
Ashok Makhija <ashok.makhija@systematixindia.com>
2021-11-29 12:27:22 +0530

    add packaging drawer decimal issue resolved

--- src/components/costing/components/Drawers/AddPackaging.js
+++ src/components/costing/components/Drawers/AddPackaging.js
@@ -114,7 +114,7 @@ function AddPackaging(props) {
         if (!PackageType) {
           setValue('PackagingCost', '')
         } else {
-          setValue('PackagingCost', checkForDecimalAndNull(NetRawMaterialsCost * calculatePercentage(PackagingCostPercentage), getConfigurationKey().No))
+          setValue('PackagingCost', checkForDecimalAndNull(NetRawMaterialsCost * calculatePercentage(PackagingCostPercentage), getConfigurationKey().NoOfDecimalForPrice))
         }
         break;
 
@@ -192,7 +192,7 @@ function AddPackaging(props) {
       PackagingDescription: data.PackagingDescription,
       PackagingCostFixed: 0,
       PackagingCostPercentage: PackageType ? data.PackagingCostPercentage : 0,
-      PackagingCost: data.PackagingCost,
+      PackagingCost: checkForDecimalAndNull(data.PackagingCost, getConfigurationKey().NoOfDecimalForPrice),
       Applicability: applicability ? data.Applicability.value : '',
     }
     toggleDrawer('', formData)
