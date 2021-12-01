22432cf1e31f4e4a88bac69197ea9e18e062d940
Ashok Makhija <ashok.makhija@systematixindia.com>
2021-12-01 15:26:11 +0530

    dead code removed from simulation approval listing

--- src/components/simulation/components/SimulationApprovalListing.js
+++ src/components/simulation/components/SimulationApprovalListing.js
@@ -253,26 +253,6 @@ function SimulationApprovalListing(props) {
         return cell !== null ? cell : '-'
     }
 
-    const conditionFormatter = (props) => {
-
-        // const cell = props?.valueFormatted ? props.valueFormatted : props?.value;
-
-        const status = props.node.data.Status;
-
-
-        if (status === DRAFT) {
-            return `Y`;
-        }
-        else if (status === APPROVED) {
-            return `R`
-        } else {
-            return `U`
-        }
-
-
-
-
-    }
 
     const renderVendor = (props) => {
         const cell = props?.valueFormatted ? props.valueFormatted : props?.value;
@@ -356,10 +336,7 @@ function SimulationApprovalListing(props) {
         nextPage: <span className="next-page-pg"></span>, // Next page button text
         firstPage: <span className="first-page-pg"></span>, // First page button text
         lastPage: <span className="last-page-pg"></span>,
-        //exportCSVText: 'Download Excel',
-        //onExportToCSV: this.onExportToCSV,
-        //paginationShowsTotal: true,
-        //paginationShowsTotal: this.renderPaginationShowsTotal,
+
     }
 
     const sendForApproval = () => {
@@ -383,21 +360,7 @@ function SimulationApprovalListing(props) {
             }
         })
 
-        // selectedRowData.forEach((element, index, arr) => {
-        //     if (index > 0) {
-        //         if (element.TechnologyId !== arr[index - 1].TechnologyId) {
-        //             technologyCount = technologyCount + 1
-        //         } else {
-        //             return false
-        //         }
-        //     } else {
-        //         return false
-        //     }
-        // })
 
-        // if (technologyCount > 0) {
-        //     return Toaster.warning("Technology should be same for sending multiple costing for approval")
-        // }
 
         if (count > 0) {
             Toaster.warning("Reason should be same for sending multiple costing for approval")
@@ -497,7 +460,6 @@ function SimulationApprovalListing(props) {
         customLoadingOverlay: LoaderCustom,
         customNoRowsOverlay: NoContentFound,
         reasonFormatter: reasonFormatter,
-        conditionFormatter: conditionFormatter
     };
 
 
@@ -539,7 +501,7 @@ function SimulationApprovalListing(props) {
                                 className="ag-theme-material"
                             >
                                 <AgGridReact
-                                    style={{ height: '100%', width: '100%',  }}
+                                    style={{ height: '100%', width: '100%', }}
                                     defaultColDef={defaultColDef}
                                     floatingFilter={true}
                                     domLayout='autoHeight'
@@ -559,9 +521,9 @@ function SimulationApprovalListing(props) {
                                     rowSelection={'multiple'}
                                     onSelectionChanged={onRowSelect}
                                     isRowSelectable={isRowSelectable}
-                                    
-                                    
-                                   
+
+
+
                                 >
                                     <AgGridColumn width={120} field="ApprovalNumber" cellRenderer='linkableFormatter' headerName="Token No."></AgGridColumn>
                                     {isSmApprovalListing && <AgGridColumn field="Status" headerClass="justify-content-center" cellClass="text-center" headerName='Status' cellRenderer='statusFormatter'></AgGridColumn>}
