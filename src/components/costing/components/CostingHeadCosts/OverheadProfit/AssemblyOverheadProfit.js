9458e58da752e090cbddb8cc333874baecc9bc00
Mahak <mahak.rawat@systematixindia.com>
2021-12-03 11:12:26 +0530

    Costing issue resolved

--- src/components/costing/components/CostingHeadCosts/OverheadProfit/AssemblyOverheadProfit.js
+++ src/components/costing/components/CostingHeadCosts/OverheadProfit/AssemblyOverheadProfit.js
@@ -73,6 +73,7 @@ function AssemblyOverheadProfit(props) {
     const tabData = RMCCTabData[0]
     const surfaceTabData= SurfaceTabData[0]
     const overHeadAndProfitTabData=OverheadProfitTabData[0]
+    console.log('overHeadAndProfitTabData: ', overHeadAndProfitTabData);
     const discountAndOtherTabData =DiscountCostData[0]
     let reqData = {
       "CostingId": item.CostingId,
