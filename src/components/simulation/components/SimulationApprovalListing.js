c4e84b02fffe2512576fd74fb95df225f79d5a50
Banti Prajapati <banti.prajapati@systematixindia.com>
2021-12-03 17:46:42 +0530

    next page arrow button ovellapping issue fixed

--- src/components/simulation/components/SimulationApprovalListing.js
+++ src/components/simulation/components/SimulationApprovalListing.js
@@ -585,7 +585,7 @@ function SimulationApprovalListing(props) {
                                         <option value="100">100</option>
                                     </select>
                                 </div>
-                                <div className="text-right w-100 pb-3 warning-section">
+                                <div className="text-right pb-3 warning-section">
                                     <WarningMessage message="It may take 5 minutes to update the status, please refresh." />
                                 </div>
                                 {approveDrawer &&
