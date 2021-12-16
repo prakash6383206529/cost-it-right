a0c51bae2402beaf0a801aef3475a934ee7bbd21
Banti Prajapati <banti.prajapati@systematixindia.com>
2021-12-16 18:46:35 +0530

    warning message unstable  issue fixed

--- src/components/simulation/components/SimulationApprovalListing.js
+++ src/components/simulation/components/SimulationApprovalListing.js
@@ -522,7 +522,7 @@ function SimulationApprovalListing(props) {
                             </Row>
                         </form>
 
-                        <div className="ag-grid-wrapper height-width-wrapper">
+                        <div className="ag-grid-wrapper height-width-wrapper min-height-auto ">
                             <div className="ag-grid-header">
                                 <input type="text" className="form-control table-search" id="filter-text-box" placeholder="Search " onChange={(e) => onFilterTextBoxChanged(e)} />
                             </div>
