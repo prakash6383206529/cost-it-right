9814f4630fe16b041f1524bcd1ac8a342ce5a4eb
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2021-12-18 13:26:36 +0530

    Apply condition for set deparment when no approval is coming

--- src/components/costing/components/approval/ApproveRejectDrawer.js
+++ src/components/costing/components/approval/ApproveRejectDrawer.js
@@ -211,7 +211,9 @@ function ApproveRejectDrawer(props) {
       dispatch(
         getAllSimulationApprovalList(obj, (res) => {
           const Data = res.data.DataList[1] ? res.data.DataList[1] : []
-          // setValue('dept', { label: Data.DepartmentName, value: Data.DepartmentId })
+          if (Object.keys(Data).length > 0) {
+            setValue('dept', { label: Data.DepartmentName, value: Data.DepartmentId })
+          }
           setValue('approver', { label: Data.Text ? Data.Text : '', value: Data.Value ? Data.Value : '', levelId: Data.LevelId ? Data.LevelId : '', levelName: Data.LevelName ? Data.LevelName : '' })
           let tempDropdownList = []
           res.data.DataList && res.data.DataList.map((item) => {
