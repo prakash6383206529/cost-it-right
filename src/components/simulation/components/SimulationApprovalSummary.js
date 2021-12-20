3d8690f6411b5d78531edfabf3edee4b71b5fd09
Ashok Makhija <ashok.makhija@systematixindia.com>
2021-12-20 16:20:13 +0530

    page drop down working in simulation approval summary

--- src/components/simulation/components/SimulationApprovalSummary.js
+++ src/components/simulation/components/SimulationApprovalSummary.js
@@ -79,7 +79,7 @@ function SimulationApprovalSummary(props) {
     const [IsOpen, setIsOpen] = useState(false);
     const [DataForAssemblyImpactForFg, setdataForAssemblyImpactForFg] = useState({});
     const [textFilterSearch, setTextFilterSearch] = useState('')
-    
+
 
     const dispatch = useDispatch()
 
@@ -512,8 +512,10 @@ function SimulationApprovalSummary(props) {
 
 
     const onPageSizeChanged = (newPageSize) => {
-        var value = document.getElementById('page-size').value;
-        gridApi.paginationSetPageSize(Number(value));
+
+        // var value = document.getElementById('page-size').value;
+        gridApi.paginationSetPageSize(Number(newPageSize));
+
     };
 
     const onFilterTextBoxChanged = (e) => {
