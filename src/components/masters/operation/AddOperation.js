480b86e08b4b5aa9ddc9de77050e21ecab4740c3
Banti Prajapati <banti.prajapati@systematixindia.com>
2021-12-23 15:13:17 +0530

    operation master label add and send for approval button title add

--- src/components/masters/operation/AddOperation.js
+++ src/components/masters/operation/AddOperation.js
@@ -612,8 +612,8 @@ class AddOperation extends Component {
                   <div className="col-md-6">
                     <h2>
                       {this.state.isEditFlag
-                        ? "Update OPER"
-                        : "OPER"}
+                        ? "Update Operation"
+                        : "Operation"}
                     </h2>
                   </div>
                 </div>
