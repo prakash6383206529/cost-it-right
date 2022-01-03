2e91b7c96723069993626d94ed0d93c8bdce2080
Mahak <mahak.rawat@systematixindia.com>
2022-01-03 20:52:59 +0530

    pinned action column

--- src/components/simulation/components/SimulationApprovalSummary.js
+++ src/components/simulation/components/SimulationApprovalSummary.js
@@ -972,7 +972,7 @@ function SimulationApprovalSummary(props) {
                                                                     </>
                                                                 }
                                                                 <AgGridColumn width={140} field="Variance" headerName="Variance"></AgGridColumn>
-                                                                <AgGridColumn width={140} field="SimulationCostingId" cellRenderer='buttonFormatter' floatingFilter={false} headerName="Actions" type="rightAligned"></AgGridColumn>
+                                                                <AgGridColumn width={140} field="SimulationCostingId"  pinned="right"  cellRenderer='buttonFormatter' floatingFilter={false} headerName="Actions" type="rightAligned"></AgGridColumn>
                                                                 {/* <AgGridColumn field="Status" headerName='Status' cellRenderer='statusFormatter'></AgGridColumn>
                                                                 <AgGridColumn field="SimulationId" headerName='Actions'   type="rightAligned" cellRenderer='buttonFormatter'></AgGridColumn> */}
 
