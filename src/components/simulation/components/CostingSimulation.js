070b2504531a8caf1eb1009029efe162822279a1
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-01-05 17:04:34 +0530

    Show Assembly impact in Verify Impact Drawer

--- src/components/simulation/components/CostingSimulation.js
+++ src/components/simulation/components/CostingSimulation.js
@@ -239,7 +239,6 @@ function CostingSimulation(props) {
     }
 
     const viewAssembly = (cell, row, rowIndex) => {
-        console.log('row: ', row);
         const data = row
         setDataForAssemblyImpact(data)
         setShowViewAssemblyDrawer(true)
@@ -886,6 +885,7 @@ function CostingSimulation(props) {
                                 vendorIdState={vendorIdState}
                                 EffectiveDate={simulationDetail.EffectiveDate}
                                 amendmentDetails={amendmentDetails}
+                                dataForAssemblyImpactInVerifyImpact={tableData}
                             />}
                     </div>
 
