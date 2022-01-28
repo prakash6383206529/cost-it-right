a557d5bf1b53fe8519169741bb9f6059e7b3cb39
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-01-28 19:05:58 +0530

    VERSION CHANGED V1.1.110.1

--- src/config/constants.js
+++ src/config/constants.js
@@ -922,7 +922,7 @@ export const API = {
 
   //REPORT
   getReportListing: `${BASE_URL}/dashboard/get-costings-for-dashboard`,
-  getSimualtionInsightReport:`${BASE_URL}/reports/get-simulation-insights`
+  getSimualtionInsightReport: `${BASE_URL}/reports/get-simulation-insights`
 
 }
 
@@ -1499,7 +1499,7 @@ export const GET_IMPACTED_MASTER_DATA = 'GET_IMPACTED_MASTER_DATA'
 
 // REPORT
 export const GET_REPORT_LIST = 'GET_REPORT_LIST'
-export const GET_SIMULATION_INSIGHT_REPORT='GET_SIMULATION_INSIGHT_REPORT'
+export const GET_SIMULATION_INSIGHT_REPORT = 'GET_SIMULATION_INSIGHT_REPORT'
 
 export const RM_APPROVAL_DASHBOARD = 'RM_APPROVAL_DASHBOARD'
 
@@ -1804,4 +1804,4 @@ export const LEVEL1 = 'L1';
 export const SUB_ASSEMBLY = 'Sub Assembly';
 
 //VERSION 
-export const VERSION = "V1.1.110";
+export const VERSION = "V1.1.110.1";
