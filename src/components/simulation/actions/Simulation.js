8b7b1e527e2e9b7ef76baffd93e77a6789440d3d
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-01-31 19:13:49 +0530

    continuous loader coming on verify page in surface treatment and operation simulation | FIXED

--- src/components/simulation/actions/Simulation.js
+++ src/components/simulation/actions/Simulation.js
@@ -851,7 +851,7 @@ export function getVerifySurfaceTreatmentSimulationList(token, callback) {
             if (response.data.Result) {
                 dispatch({
                     type: GET_VERIFY_SIMULATION_LIST,
-                    payload: response.data.Data.SimulationExchangeRateImpactedCostings
+                    payload: response.data.Data.SimulationSurfaceTreatmentAndOperationImpactedCosting
                 })
                 callback(response)
             }
