ac64388f48f74afaffc9a2ab18c275a3390fdaa1
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-02-23 12:12:27 +0530

    Diecasting file

---
+++ src/components/costing/components/WeightCalculatorDrawer/dieCasting/index.js
@@ -0,0 +1,136 @@
+import React, { Fragment, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import {
+  Row,
+  Col,
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from 'reactstrap'
+import classnames from 'classnames'
+import Drawer from '@material-ui/core/Drawer'
+import NonFerrous from './NonFerrous'
+
+
+function NonFerrousCalculator(props) {
+  const { rmRowData } = props
+  const [activeTab, setActiveTab] = useState('1')
+  /**
+   * @method toggleDrawer
+   * @description TOGGLE DRAWER
+   */
+  const toggleDrawer = (event, weightData = {}) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return
+    }
+    props.closeDrawer('', weightData)
+  }
+
+  /**
+   * @method toggle
+   * @description toggling the tabs
+   */
+  const toggle = (tab) => {
+    if (activeTab !== tab) {
+      setActiveTab(tab)
+    }
+  }
+
+  return (
+    <Fragment>
+      {/* <Row>
+        <Col md="2">{`RM Name: ${
+          rmRowData.RMName !== undefined ? rmRowData.RMName : ''
+        }`}</Col>
+        <Col md="2">{`Material: ${
+          rmRowData.MaterialType !== undefined ? rmRowData.MaterialType : ''
+        }`}</Col>
+        <Col md="2">{`Density(g/cm2): ${
+          rmRowData.Density !== undefined ? rmRowData.Density : ''
+        }`}</Col>
+      </Row> */}
+
+      <Row>
+        <Col>
+          <Nav tabs className="subtabs cr-subtabs-head forging-tabs  nav nav-tabs">
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => {
+                  toggle('1')
+                }}
+              >
+                GDC
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => {
+                  toggle('2')
+                }}
+              >
+                 LPDC
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '3' })}
+                onClick={() => {
+                  toggle('3')
+                }}
+              >
+                 HPDC
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={activeTab}>
+            {activeTab === '1' && (
+              <TabPane tabId="1">
+                <NonFerrous
+                  rmRowData={props.rmRowData}
+                  toggleDrawer={props.toggleDrawer}
+                  CostingViewMode={props.CostingViewMode}
+                  activeTab={activeTab}
+                  isHpdc={false}
+                  isEditFlag={props.isEditFlag}
+                />
+              </TabPane>
+            )}
+             {activeTab === '2' && (
+              <TabPane tabId="2">
+                <NonFerrous
+                  rmRowData={props.rmRowData}
+                  toggleDrawer={props.toggleDrawer}
+                  CostingViewMode={props.CostingViewMode}
+                  activeTab={activeTab}
+                  isHpdc={false}
+                  isEditFlag={props.isEditFlag}
+                />
+              </TabPane>
+            )}
+            {activeTab === '3' && (
+              <TabPane tabId="3">
+                <NonFerrous
+                  rmRowData={props.rmRowData}
+                  toggleDrawer={props.toggleDrawer}
+                  CostingViewMode={props.CostingViewMode}
+                  activeTab={activeTab}
+                  isHpdc={true}
+                  isEditFlag={props.isEditFlag}
+                />
+              </TabPane>
+            )}
+          </TabContent>
+        </Col>
+      </Row>
+    </Fragment>
+  )
+}
+export default NonFerrousCalculator
