528ca96f020fc3ca47950f258532131e93b0c9f1
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-03-01 10:37:18 +0530

    calculator issue fixed

---
+++ src/components/common/Calculator/CalculatorWrapper.js
@@ -0,0 +1,22 @@
+import Calculator from "./component/Calculator";
+import Draggable from 'react-draggable';
+import React, { useState } from "react";
+const CalculatorWrapper = ()=> {
+    const [isShowCal, setIsShowCal] = useState(false)
+    const showCalculator =()=> {
+        setIsShowCal(!isShowCal)
+	  }
+      return (
+       <>
+       <button className="CalculatorIcon cr-cl-icon cal-btn" type="buton" title="Calculator" onClick={showCalculator}></button>
+        {isShowCal && <div className="calculator-wrapper">  
+         <Draggable>
+            <div>
+              <Calculator />
+           </div>
+         </Draggable>
+       </div>}
+       </>
+      )
+}
+export default CalculatorWrapper;
