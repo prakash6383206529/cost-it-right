26221457578204a195e6ae7bc7b998fb32bdf768
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-03-01 20:07:07 +0530

    api error in adoperation

--- src/components/masters/operation/AddOperation.js
+++ src/components/masters/operation/AddOperation.js
@@ -231,7 +231,7 @@ class AddOperation extends Component {
 
   closeVendorDrawer = (e = '') => {
     this.setState({ isOpenVendor: false }, () => {
-      this.props.getVendorWithVendorCodeSelectList()
+      this.props.getVendorWithVendorCodeSelectList(() => { })
     })
   }
 
