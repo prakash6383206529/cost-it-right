f1fd5fc331702eed25a336a240bbe1d05717f7d9
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-03-08 19:03:49 +0530

    UI issue fixed in listing data

--- src/components/masters/exchange-rate-master/ExchangeRateListing.js
+++ src/components/masters/exchange-rate-master/ExchangeRateListing.js
@@ -408,6 +408,7 @@ class ExchangeRateListing extends Component {
                                     noRowsOverlayComponent={'customNoRowsOverlay'}
                                     noRowsOverlayComponentParams={{
                                         title: EMPTY_DATA,
+                                        imagClass: 'imagClass'
                                     }}
                                     frameworkComponents={this.frameworkComponents}
                                 >
