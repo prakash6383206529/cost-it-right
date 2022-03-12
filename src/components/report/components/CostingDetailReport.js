8872a846bd9ca778df5ebadd0cebebd6f5923be6
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-03-12 14:31:42 +0530

    view button add functionality

--- src/components/report/components/CostingDetailReport.js
+++ src/components/report/components/CostingDetailReport.js
@@ -54,6 +54,7 @@ function ReportListing(props) {
 
 
     const [reportListingDataStateArray, setReportListingDataStateArray] = useState([])
+    const [isLastWeek, setIsLastWeek] = useState(false)
 
 
     const dispatch = useDispatch()
@@ -163,10 +164,9 @@ function ReportListing(props) {
    * @description getting approval list table
    */
 
-    const getTableData = (skip, take, isPagination, data) => {
+    const getTableData = (skip, take, isPagination, data, isLastWeek) => {
 
-
-        dispatch(getCostingReport(skip, take, isPagination, data, (res) => {
+        dispatch(getCostingReport(skip, take, isPagination, data, isLastWeek, (res) => {
             if (res) {
                 setLoader(false)
             }
@@ -180,7 +180,7 @@ function ReportListing(props) {
     useEffect(() => {
 
         setLoader(true)
-        getTableData(0, 100, true, floatingFilterData);
+        getTableData(0, 100, true, floatingFilterData, false);
 
 
     }, [])
@@ -195,7 +195,7 @@ function ReportListing(props) {
             setPageNo(pageNo + 1)
             const nextNo = currentRowIndex + 10;
 
-            getTableData(nextNo, 100, true, floatingFilterData);
+            getTableData(nextNo, 100, true, floatingFilterData, false);
             setCurrentRowIndex(nextNo)
         }
 
@@ -208,7 +208,7 @@ function ReportListing(props) {
             setPageNo(pageNo - 1)
             const previousNo = currentRowIndex - 10;
 
-            getTableData(previousNo, 100, true, floatingFilterData);
+            getTableData(previousNo, 100, true, floatingFilterData, false);
             setCurrentRowIndex(previousNo)
 
         }
@@ -380,7 +380,7 @@ function ReportListing(props) {
 
         gridOptions.columnApi.resetColumnState();
         gridOptions.api.setFilterModel(null);
-        getTableData(0, 100, true, floatingFilterData);
+        getTableData(0, 100, true, floatingFilterData, false);
 
         setEnableSearchFilterButton(true)
 
@@ -491,7 +491,7 @@ function ReportListing(props) {
 
 
         setPageNo(1)
-        getTableData(0, 100, true, emptyObj);
+        getTableData(0, 100, true, emptyObj, false);
 
 
 
@@ -541,7 +541,8 @@ function ReportListing(props) {
         // const type_of_costing = 
         getTableData(tempPartNo, tempcreatedBy, tempRequestedBy, tempStatus)
     }
-    const lastWeekFilter = (props) => {
+    const lastWeekFilter = () => {
+        getTableData(0, 100, true, floatingFilterData, true);
     }
 
     return (
