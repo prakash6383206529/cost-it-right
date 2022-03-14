1c21c7948242c4c735584bc95d6cd5755b9a2410
Mahak <mahak.rawat@systematixindia.com>
2022-03-14 16:13:54 +0530

    PAge name changed for costing Routing

--- src/config/constants.js
+++ src/config/constants.js
@@ -1585,6 +1585,7 @@ export const TAX = 'Tax'
 export const COSTING_DETAILS_REPORT = 'Costing Details Report'
 export const SIMULATION_APPROVAL_SUM = ' Simulation Approval Summary'
 export const SIMULATION_INSIGNTS = 'Simulation Insignts'
+export const COSTING_DETAIL = 'Costing Details'
 //export const SIMULATION_HISTORY = 'Simulation History'
 
 export const SHEET_METAL = 'Sheet Metal';
