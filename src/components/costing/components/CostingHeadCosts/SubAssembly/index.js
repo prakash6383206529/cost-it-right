58cac1cea6078ad2f21f9d2945bb440f1c3f1e60
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-03-22 00:46:07 +0530

    add opertion in costing view and add icon issue fixed

--- src/components/costing/components/CostingHeadCosts/SubAssembly/index.js
+++ src/components/costing/components/CostingHeadCosts/SubAssembly/index.js
@@ -11,7 +11,6 @@ import { EMPTY_GUID } from '../../../../../config/constants';
 
 function AssemblyPart(props) {
   const { children, item, index } = props;
-  
 
   const [IsOpen, setIsOpen] = useState(false);
   const [Count, setCount] = useState(0);
@@ -20,8 +19,10 @@ function AssemblyPart(props) {
   const CostingViewMode = useContext(ViewCostingContext);
   const costData = useContext(costingInfoContext);
   const initialConfiguration = useSelector(state => state.auth.initialConfiguration)
-  const { CostingEffectiveDate } = useSelector(state => state.costing)
+  const { RMCCTabData, CostingEffectiveDate } = useSelector(state => state.costing)
   const dispatch = useDispatch()
+
+  const RMCHeaderData = RMCCTabData && RMCCTabData.length > 0 && RMCCTabData[0].CostingPartDetails !== undefined ? RMCCTabData[0].CostingPartDetails : null;
 
   const toggle = (BOMLevel, PartNumber) => {
     setIsOpen(!IsOpen)
@@ -31,7 +32,7 @@ function AssemblyPart(props) {
         CostingId: item.CostingId !== null ? item.CostingId : "00000000-0000-0000-0000-000000000000",
         PartId: item.PartId,
         AssemCostingId: costData.CostingId,
-        subAsmCostingId:props.subAssembId !== null ? props.subAssembId:EMPTY_GUID
+        subAsmCostingId: props.subAssembId !== null ? props.subAssembId : EMPTY_GUID
       }
       dispatch(getRMCCTabData(data, false, (res) => {
         if (res && res.data && res.data.Result) {
@@ -132,7 +133,7 @@ function AssemblyPart(props) {
       <tr className="costing-highlight-row accordian-row">
         <div style={{ display: 'contents' }} onClick={() => toggle(item.BOMLevel, item.PartNumber)}>
           <td>
-            <span style={{ position: 'relative' }} className={`cr-prt-nm1 cr-prt-link1 ${item && item.PartType!=="Sub Assembly"  && item.PartType!=="Assembly" && "L1"}`}>
+            <span style={{ position: 'relative' }} className={`cr-prt-nm1 cr-prt-link1 ${item && item.PartType !== "Sub Assembly" && item.PartType !== "Assembly" && "L1"}`}>
               <div className={`${item.IsOpen ? 'Open' : 'Close'}`}></div>{item && item.PartNumber}
             </span>
           </td>
@@ -176,7 +177,7 @@ function AssemblyPart(props) {
               type="button"
               className={'user-btn add-oprn-btn'}
               onClick={DrawerToggle}>
-              <div className={ `${CostingViewMode ? 'fa fa-eye pr-1' : 'plus'}`}></div>{ 'OPER'}</button>}
+              <div className={`${RMCHeaderData !== null && RMCHeaderData.CostingOperationCostResponse > 0 ? 'fa fa-eye pr-1' : 'plus'}`}></div>{'OPER'}</button>}
         </td>
       </tr>
 
