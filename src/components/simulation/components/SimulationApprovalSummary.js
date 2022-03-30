6603bb9646142f1e99942ff0f128d5bee8990f64
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-03-30 17:15:51 +0530

    Po variance binded in delta in assemblyWiseImpact api in simulationApproval Summary component

--- src/components/simulation/components/SimulationApprovalSummary.js
+++ src/components/simulation/components/SimulationApprovalSummary.js
@@ -192,7 +192,7 @@ function SimulationApprovalSummary(props) {
                 });
 
                 uniqueArr && uniqueArr.map(item => {
-                    requestData.push({ CostingId: item.CostingId, delta: item.Variance, IsSinglePartImpact: false })
+                    requestData.push({ CostingId: item.CostingId, delta: item.POVariance, IsSinglePartImpact: false })
                     return null
                 })
 
