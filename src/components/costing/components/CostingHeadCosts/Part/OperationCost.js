3337edd012fc8642670e2db640bcd02211207f65
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-04-01 12:03:41 +0530

    comment box UI fixed CIR-I1273

--- src/components/costing/components/CostingHeadCosts/Part/OperationCost.js
+++ src/components/costing/components/CostingHeadCosts/Part/OperationCost.js
@@ -388,7 +388,7 @@ function OperationCost(props) {
                             <td>
                               {(!CostingViewMode && !IsLocked) && <button className="Edit  mr-2 mb-0 align-middle" type={'button'} onClick={() => editItem(index)} />}
                               {(!CostingViewMode && !IsLocked) && <button className="Delete mb-0 align-middle" type={'button'} onClick={() => deleteItem(index, item.OperationId)} />}
-                              <Popup trigger={<button id={`popUpTriggerss${index}`} className="Comment-box ml-2" type={'button'} />}
+                              <Popup trigger={<button id={`popUpTriggerss${index}`} className="Comment-box ml-2 align-middle" type={'button'} />}
                                 position="top center">
                                 <TextAreaHookForm
                                   label="Remark:"
