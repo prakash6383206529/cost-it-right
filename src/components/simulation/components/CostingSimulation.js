0bcb5b85c9cab04ea733ce925558feca4d9d52b1
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-04-05 16:52:55 +0530

    empty error box coming issue fixed only cherry picked for minda

--- src/components/simulation/components/CostingSimulation.js
+++ src/components/simulation/components/CostingSimulation.js
@@ -845,7 +845,7 @@ function CostingSimulation(props) {
     }
     const errorBoxClass = () => {
         let temp
-        temp = status === (null || '' || undefined) ? 'd-none' : ''
+        temp = (status === '' || status === null || status === undefined) ? 'd-none' : ''
         return temp
     }
 
