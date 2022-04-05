c18feb06d868b4ce7c3a629c41a7c2818d938225
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-04-05 16:40:34 +0530

    report no data found image UI fixed

--- src/components/report/components/CostingDetailReport.js
+++ src/components/report/components/CostingDetailReport.js
@@ -476,6 +476,7 @@ function ReportListing(props) {
                         noRowsOverlayComponent={'customNoRowsOverlay'}
                         noRowsOverlayComponentParams={{
                             title: EMPTY_DATA,
+                            imagClass: 'imagClass'
                         }}
                         suppressRowClickSelection={true}
                         rowSelection={'multiple'}
