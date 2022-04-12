7cbf2c2b8b5ab8ce006fd5c1a5c450b41b70790f
Mahak <mahak.rawat@systematixindia.com>
2022-04-12 20:12:19 +0530

    CHeckbox hide for rejected costing

--- src/components/costing/components/CostingSummaryTable.js
+++ src/components/costing/components/CostingSummaryTable.js
@@ -819,7 +819,7 @@ const CostingSummaryTable = (props) => {
                               <th scope="col" className='header-name'>
                                 <div class="element w-60 d-inline-flex align-items-center">
                                   {
-                                    (data.status === DRAFT || data.status === REJECTED) && <>
+                                    (data.status === DRAFT) && <>
                                       {!pdfHead && !drawerDetailPDF && <div class="custom-check1 d-inline-block">
                                         <label
                                           className="custom-checkbox pl-0 mb-0"
