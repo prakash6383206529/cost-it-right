76a8cf0a0ab9c3d75d7141a4cd8db50e6dc68f72
Mahak <mahak.rawat@systematixindia.com>
2022-04-12 11:12:58 +0530

    NCC changes done in Voumne master and approval drawer

--- src/components/masters/volume-master/VolumeListing.js
+++ src/components/masters/volume-master/VolumeListing.js
@@ -24,6 +24,7 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import PopupMsgWrapper from '../../common/PopupMsgWrapper'
 import ScrollToTop from '../../common/ScrollToTop'
+import AddLimit from './AddLimit'
 
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
@@ -133,8 +134,8 @@ class VolumeListing extends Component {
       showData: false,
       showPopup: false,
       deletedId: '',
-      isLoader: false
-
+      isLoader: false,
+      limit: false,
     }
   }
 
@@ -403,6 +404,24 @@ class VolumeListing extends Component {
   }
 
   /**
+ * @method LimitHandleChange
+ * @description Open Limit Side Drawer
+ */
+  limitHandler = () => {
+    this.setState({ limit: true });
+  };
+
+
+  /**
+  * @method  closeLimitDrawer
+  * @description CLOSE Limit Side Drawer
+  */
+  closeLimitDrawer = () => {
+    this.setState({ limit: false });
+  };
+
+
+  /**
    * @method render
    * @description Renders the component
    */
@@ -416,7 +435,8 @@ class VolumeListing extends Component {
       isBudgetedBulkUpload,
       AddAccessibility,
       BulkUploadAccessibility,
-      DownloadAccessibility
+      DownloadAccessibility,
+      limit
     } = this.state
     const ExcelFile = ReactExport.ExcelFile;
 
@@ -468,6 +488,13 @@ class VolumeListing extends Component {
                     ) : (
                       ""
                     )}
+                    <button
+                      type="button"
+                      className={"user-btn mr5"}
+                      onClick={this.limitHandler}
+                    >
+                      Add Limit
+                    </button>
                     {AddAccessibility && (
                       <button
                         type="button"
@@ -597,6 +624,16 @@ class VolumeListing extends Component {
           {
             this.state.showPopup && <PopupMsgWrapper isOpen={this.state.showPopup} closePopUp={this.closePopUp} confirmPopup={this.onPopupConfirm} message={`${MESSAGES.VOLUME_DELETE_ALERT}`} />
           }
+
+          {limit && (
+            <AddLimit
+              isOpen={limit}
+              closeDrawer={this.closeLimitDrawer}
+              isEditFlag={false}
+              ID={""}
+              anchor={"right"}
+            />
+          )}
         </div>
       </>
     )
