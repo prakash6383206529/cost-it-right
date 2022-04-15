8399da8a15ab123a30734192ea26348006038fab
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-04-15 14:19:37 +0530

    add go to top in rmc tab

--- src/components/costing/components/costingHeaderTabs/TabRMCC.js
+++ src/components/costing/components/costingHeaderTabs/TabRMCC.js
@@ -17,6 +17,7 @@ import { ViewCostingContext } from '../CostingDetails';
 import DayTime from '../../../common/DayTimeWrapper'
 import { createToprowObjAndSave } from '../../CostingUtil';
 import _ from 'lodash'
+import ScrollToTop from '../../../common/ScrollToTop';
 
 function TabRMCC(props) {
 
@@ -1256,8 +1257,9 @@ function TabRMCC(props) {
                 className="form"
                 onSubmit={handleSubmit(onSubmit)}
               >
-                <Row>
+                <Row id='go-top-top'>
                   <Col md="12">
+                    <ScrollToTop pointProp={"go-top-top"} />
                     <Table className="table cr-brdr-main mb-0 rmcc-main-headings" size="sm">
                       <thead>
                         <tr>
