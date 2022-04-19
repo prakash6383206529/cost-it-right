edc9439d2d65f95e106dab716d07dd448cbd5158
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-04-19 15:06:53 +0530

    removed extra use effect code

--- src/components/costing/components/approval/SendForApproval.js
+++ src/components/costing/components/approval/SendForApproval.js
@@ -108,10 +108,6 @@ const SendForApproval = (props) => {
 
     }))
   }, [])
-  useEffect(() => {
-
-
-  }, [viewApprovalData])
   /**
    * @method renderDropdownListing
    * @description DROPDOWN
