97344add9e86a92eb7591454684a2d01fd1f4a49
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-04-22 13:01:39 +0530

    Plant not visible for all records | fixed

--- src/components/masters/volume-master/VolumeListing.js
+++ src/components/masters/volume-master/VolumeListing.js
@@ -13,7 +13,7 @@ import { getVendorListByVendorType } from '../actions/Material'
 import { VOLUME_DOWNLOAD_EXCEl } from '../../../config/masterData'
 import AddVolume from './AddVolume'
 import BulkUpload from '../../massUpload/BulkUpload'
-import { ADDITIONAL_MASTERS, VOLUME, VolumeMaster, ZBC } from '../../../config/constants'
+import { ADDITIONAL_MASTERS, VOLUME, VolumeMaster } from '../../../config/constants'
 import { checkPermission } from '../../../helper/util'
 import { getLeftMenu } from '../../../actions/auth/AuthActions'
 import { GridTotalFormate } from '../../common/TableGridFunctions'
@@ -26,7 +26,6 @@ import PopupMsgWrapper from '../../common/PopupMsgWrapper'
 import ScrollToTop from '../../common/ScrollToTop'
 import AddLimit from './AddLimit'
 
-const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
 const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
@@ -283,6 +282,22 @@ class VolumeListing extends Component {
     return (cellValue !== ' ' && cellValue !== null && cellValue !== '' && cellValue !== undefined) ? cellValue : '-';
   }
 
+  /**
+  * @method plantFormatter
+  */
+  plantFormatter = (props) => {
+    const rowData = props?.data;
+    let value = '-'
+    if (rowData?.IsVendor === "Vendor Based") {
+      value = rowData?.DestinationPlant
+      return value;
+    } else if (rowData?.IsVendor === "Zero Based") {
+      value = rowData?.Plant
+      return value;
+    }
+    return value;
+  }
+
   onExportToCSV = (row) => {
     return this.state.userData // must return the data which you want to be exported
   }
@@ -291,9 +306,6 @@ class VolumeListing extends Component {
     return <GridTotalFormate start={start} to={to} total={total} />
   }
 
-  plantFormatter = (cell, row, enumObject, rowIndex) => {
-    return row.IsVendor ? row.DestinationPlant : cell
-  }
   /**
    * @method actualBulkToggle
    * @description OPEN ACTUAL BULK UPLOAD DRAWER FOR BULK UPLOAD
@@ -369,7 +381,6 @@ class VolumeListing extends Component {
   };
 
   onBtExport = () => {
-    let tempArr = this.props.volumeDataList && this.props.volumeDataList
     return this.returnExcelColumn(VOLUME_DOWNLOAD_EXCEl, this.props.volumeDataList)
   };
 
@@ -428,7 +439,6 @@ class VolumeListing extends Component {
   render() {
     const { handleSubmit } = this.props
     const {
-      isEditFlag,
       showVolumeForm,
       data,
       isActualBulkUpload,
@@ -454,6 +464,7 @@ class VolumeListing extends Component {
       costingHeadRenderer: this.costingHeadFormatter,
       customNoRowsOverlay: NoContentFound,
       hyphenFormatter: this.hyphenFormatter,
+      plantFormatter: this.plantFormatter
 
     };
 
@@ -583,7 +594,7 @@ class VolumeListing extends Component {
                 <AgGridColumn field="VendorName" headerName="Vendor Name" cellRenderer={'hyphenFormatter'}></AgGridColumn>
                 <AgGridColumn field="PartNumber" headerName="Part Number"></AgGridColumn>
                 <AgGridColumn field="PartName" headerName="Part Name"></AgGridColumn>
-                <AgGridColumn field="Plant" headerName="Plant" cellRenderer={'hyphenFormatter'}></AgGridColumn>
+                <AgGridColumn field="Plant" headerName="Plant" cellRenderer={'plantFormatter'}></AgGridColumn>
                 <AgGridColumn field="BudgetedQuantity" headerName="Budgeted Quantity"></AgGridColumn>
                 <AgGridColumn field="ApprovedQuantity" headerName="Approved Quantity"></AgGridColumn>
                 <AgGridColumn field="VolumeId" width={120} headerName="Actions" type="rightAligned" floatingFilter={false} cellRenderer={'totalValueRenderer'}></AgGridColumn>
