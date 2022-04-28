43ec3b54934bc7722be70ef0fa75689fa694fcd4
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-04-28 19:19:57 +0530

    Master batch changes | checkbox made disable

--- src/components/costing/components/WeightCalculatorDrawer/Ferrous.js
+++ src/components/costing/components/WeightCalculatorDrawer/Ferrous.js
@@ -14,6 +14,7 @@ function Ferrous(props) {
     const costData = useContext(costingInfoContext)
     const dispatch = useDispatch()
     const { ferrousCalculatorReset } = useSelector(state => state.costing)
+    const { DisableMasterBatchCheckbox } = props
 
     const defaultValues = {
         castingWeight: WeightCalculatorRequest && WeightCalculatorRequest.CastingWeight !== undefined ? WeightCalculatorRequest.CastingWeight : '',
@@ -171,7 +172,7 @@ function Ferrous(props) {
     }
 
     const onSubmit = () => {
-
+        DisableMasterBatchCheckbox(true)
         let obj = {}
         obj.FerrousCastingWeightCalculatorId = WeightCalculatorRequest && WeightCalculatorRequest.ForgingWeightCalculatorId ? WeightCalculatorRequest.ForgingWeightCalculatorId : "0"
         obj.CostingRawMaterialDetailsIdRef = rmRowData.RawMaterialDetailId
