335aa7ddf25d0dbf1a0438114e0031849f0b4d4b
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-04-29 10:48:07 +0530

    Prop in bulkupload for bopimport is changed

--- src/components/masters/material-master/RMImportListing.js
+++ src/components/masters/material-master/RMImportListing.js
@@ -587,7 +587,7 @@ function RMImportListing(props) {
             isZBCVBCTemplate={true}
             messageLabel={"RM Import"}
             anchor={"right"}
-            isFinalLevelUser={isFinalLevelUser}
+            isFinalApprovar={isFinalLevelUser}
           />
         )
       }
