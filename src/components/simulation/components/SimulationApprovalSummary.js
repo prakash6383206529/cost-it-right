7f0e9f33b3b579c6b2bff3654c0edd976a8744d5
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-05-03 14:42:35 +0530

    formatter  name changed

--- src/components/simulation/components/SimulationApprovalSummary.js
+++ src/components/simulation/components/SimulationApprovalSummary.js
@@ -845,7 +845,7 @@ function SimulationApprovalSummary(props) {
         return cell != null ? temp : '-';
     }
 
-    const bopMaterailNumberFormat = (props) => {
+    const bopNumberFormat = (props) => {
         const cell = props?.valueFormatted ? props.valueFormatted : props?.value;
         const row = props?.valueFormatted ? props.valueFormatted : props?.data;
         let temp = ''
@@ -957,7 +957,7 @@ function SimulationApprovalSummary(props) {
         plantFormatter: plantFormatter,
         rawMaterailCodeSpecFormatter: rawMaterailCodeSpecFormatter,
         operationCodeFormatter: operationCodeFormatter,
-        bopMaterailNumberFormat: bopMaterailNumberFormat
+        bopNumberFormat: bopNumberFormat
     };
 
     const errorBoxClass = () => {
@@ -1347,7 +1347,7 @@ function SimulationApprovalSummary(props) {
                                                                     (isBOPDomesticOrImport || keysForDownloadSummary.IsBoughtOutPartSimulation) &&
                                                                     <>
                                                                         <AgGridColumn width={140} field="BoughtOutPartName" headerName="Bought Out Part Name" cellRenderer='bopMaterailFormat'></AgGridColumn>
-                                                                        <AgGridColumn width={140} field="BoughtOutPartNumber" headerName="Bought Out Part Number" cellRenderer='bopMaterailNumberFormat'></AgGridColumn>
+                                                                        <AgGridColumn width={140} field="BoughtOutPartNumber" headerName="Bought Out Part Number" cellRenderer='bopNumberFormat'></AgGridColumn>
                                                                         <AgGridColumn width={140} field="OldNetBoughtOutPartCost" headerName="Old BOP Cost" cellRenderer='oldBOPFormatter' ></AgGridColumn>
                                                                         <AgGridColumn width={140} field="NewNetBoughtOutPartCost" headerName="New BOP Cost" cellRenderer='newBOPFormatter'></AgGridColumn>
                                                                         <AgGridColumn width={140} field="NetBoughtOutPartCostVariance" headerName="BOP Variance" cellRenderer='BOPVarianceFormatter' ></AgGridColumn>
