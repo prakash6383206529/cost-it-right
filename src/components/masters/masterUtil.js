e77000ddce278a55fe99fb8ac7b977dc129d46b5
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-05-03 17:32:37 +0530

    group process table in costing details change UI

--- src/components/masters/masterUtil.js
+++ src/components/masters/masterUtil.js
@@ -252,7 +252,7 @@ export const ProcessGroup = (props) => {
                 </Row>
             }
             <div>
-                <table className='table border group-process-table'>
+                <table className='table border machine-group-process-table'>
                     <thead>
                         <tr>
                             <th>Group Name</th>
