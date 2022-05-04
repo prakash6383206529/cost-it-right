7d057bece0dd6d2a595d600bc5f28c24fc50a5c3
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-05-04 15:47:37 +0530

    Exchange rate simulation not working

--- src/components/simulation/components/Simulation.js
+++ src/components/simulation/components/Simulation.js
@@ -755,7 +755,7 @@ function Simulation(props) {
         //     return obj
 
         // })
-        let tempObject = tokenForSimulation.length !== 0 ? [{ SimulationId: tokenForSimulation.value }] : []
+        let tempObject = tokenForSimulation?.length !== 0 ? [{ SimulationId: tokenForSimulation?.value }] : []
 
         switch (page) {
             case RMDOMESTIC:
@@ -928,7 +928,7 @@ function Simulation(props) {
                                 </div>
                             }
 
-                            {(tokenForSimulation?.length !== 0 && tokenForSimulation !== null && tokenCheckBoxValue) && <button className='user-btn ml-2' onClick={callAPIOnClick}>
+                            {(tokenForSimulation?.length !== 0 && tokenForSimulation !== null && tokenForSimulation !== undefined && tokenCheckBoxValue) && <button className='user-btn ml-2' onClick={callAPIOnClick}>
                                 <div className='save-icon'></div>
                             </button>}
                         </Col>
