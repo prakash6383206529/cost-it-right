9ec6537c1da2997bbac04120053ef8354542d54c
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-05-06 14:28:43 +0530

    Loader true condition delayed

--- src/components/simulation/components/SimulationApprovalListing.js
+++ src/components/simulation/components/SimulationApprovalListing.js
@@ -94,8 +94,10 @@ function SimulationApprovalListing(props) {
         }
         setIsLoader(true)
         dispatch(getSimulationApprovalList(filterData, (res) => {
-            if (res.data.Result) {
-                setIsLoader(false)
+            if (res?.data?.Result) {
+                setTimeout(() => {
+                    setIsLoader(false)
+                }, 300);
             }
         }))
     }
