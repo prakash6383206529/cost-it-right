4309214fd66f1a0519f9557238928afa9a4b7c27
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-05-10 12:53:59 +0530

    BopDomestic VBC extra column removed from excel

--- src/config/masterData.js
+++ src/config/masterData.js
@@ -692,7 +692,6 @@ export const BOP_VBC_DOMESTIC = [
     { label: 'VendorPlantCode', value: 'VendorPlantCode', }, //NOUI
     { label: 'SourceVendorName', value: 'SourceVendorName', },
     { label: 'SourceVendorLocation', value: 'SourceVendorLocation', },
-    { label: 'MinimumOrderQuantity', value: 'MinimumOrderQuantity', }, //*
     { label: 'BasicRate', value: 'BasicRate', }, //*
     { label: 'EffectiveDate', value: 'EffectiveDate', },
     { label: 'Remark', value: 'Remark' }
