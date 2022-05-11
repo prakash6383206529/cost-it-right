ddb60a9c730927ddf7fa4f7d113f4d2f0add94d6
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-05-11 11:50:44 +0530

    summary process calculator disable issue resolved

--- src/components/costing/components/Drawers/ViewConversionCost.js
+++ src/components/costing/components/Drawers/ViewConversionCost.js
@@ -283,7 +283,7 @@ function ViewConversionCost(props) {
             {!isPDFShow && <td><button
               className="CalculatorIcon cr-cl-icon mr-auto ml-0"
               type={"button"}
-              disabled={item.ProcessCalculationId === EMPTY_GUID}
+              disabled={item.ProcessCalculatorId === 0}
               onClick={() => { getWeightData(index, process.ProcessList, parentIndex) }}
             /></td>}
             <td>{item.Quantity ? checkForDecimalAndNull(item.Quantity, initialConfiguration.NoOfDecimalForInputOutput) : '-'}</td>
@@ -354,7 +354,7 @@ function ViewConversionCost(props) {
                               <button
                                 className="CalculatorIcon cr-cl-icon mr-auto ml-0"
                                 type={"button"}
-                                disabled={item.ProcessCalculationId === EMPTY_GUID}
+                                disabled={item.ProcessCalculatorId === 0}
                                 onClick={() => { getWeightData(index) }}
                               /> : ''
                           }
