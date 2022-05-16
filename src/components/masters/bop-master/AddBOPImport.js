aeebf3b76a76d35ac5bd1da09a103a245a4098ad
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-05-16 17:26:09 +0530

    add start with space validation in all masters and user module

--- src/components/masters/bop-master/AddBOPImport.js
+++ src/components/masters/bop-master/AddBOPImport.js
@@ -4,7 +4,7 @@ import { Field, reduxForm, formValueSelector } from "redux-form";
 import { Row, Col, } from 'reactstrap';
 import {
   required, checkForNull, checkForDecimalAndNull, acceptAllExceptSingleSpecialCharacter, maxLength20,
-  postiveNumber, maxLength10, positiveAndDecimalNumber, maxLength512, maxLength, decimalLengthsix
+  postiveNumber, maxLength10, positiveAndDecimalNumber, maxLength512, maxLength, decimalLengthsix, checkWhiteSpaces
 } from "../../../helper/validation";
 import { renderText, searchableSelect, renderMultiSelectField, renderTextAreaField, renderDatePicker } from "../../layout/FormInputs";
 import { fetchMaterialComboAPI, getPlantBySupplier, getUOMSelectList, getCurrencySelectList, getPlantSelectListByType, } from '../../../actions/Common';
@@ -970,7 +970,7 @@ class AddBOPImport extends Component {
                               name={"BoughtOutPartNumber"}
                               type="text"
                               placeholder={"Enter"}
-                              validate={[required, acceptAllExceptSingleSpecialCharacter, maxLength20]}
+                              validate={[required, acceptAllExceptSingleSpecialCharacter, maxLength20, checkWhiteSpaces]}
                               component={renderText}
                               required={true}
                               disabled={isEditFlag ? true : false}
@@ -984,7 +984,7 @@ class AddBOPImport extends Component {
                               name={"BoughtOutPartName"}
                               type="text"
                               placeholder={"Enter"}
-                              validate={[required, acceptAllExceptSingleSpecialCharacter, maxLength(80)]}
+                              validate={[required, acceptAllExceptSingleSpecialCharacter, maxLength(80), checkWhiteSpaces]}
                               component={renderText}
                               required={true}
                               disabled={isEditFlag ? true : false}
