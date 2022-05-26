5868d4085fe10c12fb30144b6eca32f02cb2a6f8
Mahak <mahak.rawat@systematixindia.com>
2022-05-26 18:41:57 +0530

    Multiple draft costing to send for approval in progress

--- src/components/costing/components/approval/SendForApproval.js
+++ src/components/costing/components/approval/SendForApproval.js
@@ -25,6 +25,7 @@ import VerifyImpactDrawer from '../../../simulation/components/VerifyImpactDrawe
 
 const SEQUENCE_OF_MONTH = [9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8]
 const SendForApproval = (props) => {
+  const { isApprovalisting } = props
   const dispatch = useDispatch()
   const { register, handleSubmit, control, setValue, formState: { errors } } = useForm({
     mode: 'onBlur',
@@ -34,6 +35,7 @@ const SendForApproval = (props) => {
   const reasonsList = useSelector((state) => state.approval.reasonsList)
   const deptList = useSelector((state) => state.approval.approvalDepartmentList)
   const viewApprovalData = useSelector((state) => state.costing.costingApprovalData)
+  console.log('viewApprovalData: ', viewApprovalData);
 
   const partNo = useSelector((state) => state.costing.partNo)
   const initialConfiguration = useSelector(state => state.auth.initialConfiguration)
@@ -59,7 +61,7 @@ const SendForApproval = (props) => {
 
   useEffect(() => {
     let obj = {}
-    obj.TechnologyId = partInfo.TechnologyId
+    obj.TechnologyId = props.isApprovalisting ? props.technologyId : partInfo.TechnologyId
     obj.DepartmentId = '00000000-0000-0000-0000-000000000000'
     obj.LoggedInUserLevelId = userDetails().LoggedInLevelId
     obj.LoggedInUserId = userDetails().LoggedInUserId
@@ -342,7 +344,7 @@ const SendForApproval = (props) => {
 
       let tempObj = {}
       tempObj.ApprovalProcessId = "00000000-0000-0000-0000-000000000000"
-      tempObj.TypeOfCosting = data.typeOfCosting === 0 ? 'ZBC' : 'VBC'
+      tempObj.TypeOfCosting = (data.typeOfCosting === 0 || data.typeOfCosting === 'ZBC') ? 'ZBC' : 'VBC'
       tempObj.PlantId =
         Number(data.typeOfCosting) === 0 ? data.plantId : ''
       tempObj.PlantNumber =
@@ -359,10 +361,10 @@ const SendForApproval = (props) => {
       // tempObj.ECNNumber = 1;
       tempObj.EffectiveDate = DayTime(data.effectiveDate).format('YYYY-MM-DD HH:mm:ss')
       tempObj.RevisionNumber = partNo.revisionNumber
-      tempObj.PartName = partNo.partName
+      tempObj.PartName = isApprovalisting ? data.partName : partNo.partName
       // tempObj.PartName = "Compressor"; // set data for this is in costing summary,will come here
-      tempObj.PartNumber = partNo.partNumber //label
-      tempObj.PartId = partNo.partId
+      tempObj.PartNumber = isApprovalisting ? data.partNumber : partNo.partNumber //label
+      tempObj.PartId = isApprovalisting ? data.partId : partNo.partId
       // tempObj.PartNumber = "CP021220";// set data for this is in costing summary,will come here
       tempObj.FinancialYear = financialYear
       tempObj.OldPOPrice = data.oldPrice
@@ -394,20 +396,21 @@ const SendForApproval = (props) => {
     })
 
     obj.CostingsList = temp
+    console.log('obj: ', obj);
 
 
     // debounce_fun()
     // 
     // props.closeDrawer()
-    dispatch(
-      sendForApprovalBySender(obj, (res) => {
-        setIsDisable(false)
-        Toaster.success(viewApprovalData.length === 1 ? `Costing ID ${viewApprovalData[0].costingName} has been sent for approval to ${approver.split('(')[0]}.` : `Costings has been sent for approval to ${approver.split('(')[0]}.`)
-        props.closeDrawer('', 'Submit')
-        dispatch(setCostingApprovalData([]))
-        dispatch(setCostingViewData([]))
-      }),
-    )
+    // dispatch(
+    //   sendForApprovalBySender(obj, (res) => {
+    //     setIsDisable(false)
+    //     Toaster.success(viewApprovalData.length === 1 ? `Costing ID ${viewApprovalData[0].costingName} has been sent for approval to ${approver.split('(')[0]}.` : `Costings has been sent for approval to ${approver.split('(')[0]}.`)
+    //     props.closeDrawer('', 'Submit')
+    //     dispatch(setCostingApprovalData([]))
+    //     dispatch(setCostingViewData([]))
+    //   }),
+    // )
   }), 500)
 
   const handleApproverChange = (data) => {
@@ -540,15 +543,15 @@ const SendForApproval = (props) => {
                     <Row className="px-3">
                       <Col md="12">
                         <h6 className="left-border d-inline-block mr-4">
-                          {data.typeOfCosting === 0 ? 'ZBC' : `${data.vendorName}`}
+                          {(data.typeOfCosting === 0 || data.typeOfCosting === 'ZBC') ? 'ZBC' : `${data.vendorName}`}
                         </h6>
                         <div className=" d-inline-block mr-4">
                           {`Part No.:`}{" "}
-                          <span className="grey-text">{`${partNo.partNumber}`}</span>
+                          <span className="grey-text">{`${isApprovalisting ? data.partNumber : partNo.partNumber}`}</span>
                         </div>
                         <div className=" d-inline-block mr-4">
-                          {data.typeOfCosting === 0 ? `Plant Code:` : `Vendor Code`}{" "}
-                          <span className="grey-text">{data.typeOfCosting === 0 ? `${data.plantCode}` : `${data.vendorCode}`}</span>
+                          {(data.typeOfCosting === 0 || data.typeOfCosting === 'ZBC') ? `Plant Code:` : `Vendor Code`}{" "}
+                          <span className="grey-text">{(data.typeOfCosting === 0 || data.typeOfCosting === 'ZBC') ? `${data.plantCode}` : `${data.vendorCode}`}</span>
                         </div>
                         <div className=" d-inline-block">
                           {`Costing Id:`}{" "}
