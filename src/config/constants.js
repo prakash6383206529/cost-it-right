ff4a1798c38fee9c202d2fb12766c2f5c9e3a4d6
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-05-30 10:10:58 +0530

    version update

--- src/config/constants.js
+++ src/config/constants.js
@@ -15,9 +15,9 @@ export const config = () => {
 }
 
 // DEVELOPMENT
-// const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 // const BASE_URL = `https://demov3api.costitright.com/api/v1`;
-const BASE_URL = `http://10.148.80.4:10122/api/v1`;
+// const BASE_URL = `http://10.148.80.4:10122/api/v1`;
 
 //FILE URL
 export const FILE_URL = `${process.env.REACT_APP_FILE_URL}`;
@@ -1888,4 +1888,4 @@ export const IV = 'ozzzguugcusjqmbj'
 
 
 //VERSION 
-export const VERSION = "V1.2.240.1";
+export const VERSION = "V1.2.240.2";
