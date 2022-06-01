be6f19fdd8880aaefe8d01a43e6e8d47391e26a8
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-06-01 19:48:57 +0530

    changes in costing simulation column

--- src/config/masterData.js
+++ src/config/masterData.js
@@ -2468,6 +2468,20 @@ export const CostingSimulationDownloadST = [
     { label: "NewNetSurfaceTreatmentCost", value: "NewNetSurfaceTreatmentCost" },
     { label: "STVariance", value: "STVariance" },
 
+
+
+    { label: "OldSurfaceTreatmentRate", value: "OldSurfaceTreatmentRate" },
+    { label: "NewSurfaceTreatmentRate", value: "NewSurfaceTreatmentRate" },
+    { label: "OldSTCost", value: "OldSTCost" },
+    { label: "NewSTCost", value: "NewSTCost" },
+    { label: "OldSurfaceTreatmentCost", value: "OldSurfaceTreatmentCost" },
+    { label: "NewSurfaceTreatmentCost", value: "NewSurfaceTreatmentCost" },
+    { label: "OldTranspotationCost", value: "OldTranspotationCost" },
+    { label: "NewTranspotationCost", value: "NewTranspotationCost" },
+    { label: "OldNetSurfaceTreatmentCost", value: "OldNetSurfaceTreatmentCost" },
+    { label: "NewNetSurfaceTreatmentCost", value: "NewNetSurfaceTreatmentCost" },
+    { label: "STVariance", value: "STVariance" },
+
     { label: "OldOverheadCost", value: "OldOverheadCost" },
     { label: "NewOverheadCost", value: "NewOverheadCost" },
     { label: "OldProfitCost", value: "OldProfitCost" },
@@ -2510,6 +2524,8 @@ export const CostingSimulationDownloadOperation = [
     { label: "Operation Quantity", value: "Quantity" },
     { label: "OldOperationCost", value: "OldOperationCost" },
     { label: "NewOperationCost", value: "NewOperationCost" },
+    { label: "OldNetOperationCost", value: "OldNetOperationCost" },
+    { label: "NewNetOperationCost", value: "NewNetOperationCost" },
     { label: "OperationCostVariance", value: "OperationCostVariance" },
 
     { label: "OldOverheadCost", value: "OldOverheadCost" },
@@ -2552,6 +2568,8 @@ export const CostingSimulationDownloadBOP = [
     { label: "NewNetBoughtOutPartCost", value: "NewNetBoughtOutPartCost" },
     { label: "NetBoughtOutPartCostVariance", value: "NetBoughtOutPartCostVariance" },
     { label: "BOP Quantity", value: "BoughtOutPartQuantity" },
+    { label: "OldBOPRate", value: "OldBOPRate" },
+    { label: "NewBOPRate", value: "NewBOPRate" },
     { label: "OldBOPCost", value: "OldBOPCost" },
     { label: "NewBOPCost", value: "NewBOPCost" },
     { label: "BOPVariance", value: "BOPVariance" },
