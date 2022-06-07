ae5622a0032ba9b2b5d1b3815ccfdcf2469b8fa0
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-06-07 12:27:47 +0530

    Empty currency when checkbox Change Currency

--- src/components/costing/components/costingHeaderTabs/TabDiscountOther.js
+++ src/components/costing/components/costingHeaderTabs/TabDiscountOther.js
@@ -467,6 +467,7 @@ function TabDiscountOther(props) {
   * @description TOGGLE CURRENCY CHANGE
   */
   const onPressChangeCurrency = () => {
+    setValue('Currency', '')
     setCurrency([])
     setIsCurrencyChange(!IsCurrencyChange)
     setCurrencyExchangeRate(0)
