0d1e7abd853079a10d9763bd0fd73c5329def332
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-06-09 11:16:41 +0530

    Convert part to assembly changes for Part number not coming in Editing BOM

--- src/components/masters/part-master/BOMViewer.js
+++ src/components/masters/part-master/BOMViewer.js
@@ -479,6 +479,7 @@ class BOMViewer extends Component {
                 anchor={"right"}
                 setChildPartsData={this.setChildPartsData}
                 BOMViewerData={this.state.flowpoints}
+                partAssembly={this.props.partAssembly}
               />
             )}
             {isOpenVisualDrawer && (
