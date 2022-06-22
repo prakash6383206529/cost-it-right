85c5cbb0f911880b0958b0b4815c13859d9e095d
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-06-22 19:29:26 +0530

    CIR-I1794|stop api call on cancel in simulation history

--- src/components/simulation/components/SimulationApprovalListing.js
+++ src/components/simulation/components/SimulationApprovalListing.js
@@ -284,9 +284,12 @@ function SimulationApprovalListing(props) {
         setApproveDrawer(true)
     }
 
-    const closeDrawer = (e = '') => {
-        setApproveDrawer(false)
+    const closeDrawer = (e = '', type) => {
         gridApi.deselectAll()
+        setApproveDrawer(false)
+        if (type !== 'cancel') {
+            getTableData()
+        }
         setSelectedRowData([])
     }
 
