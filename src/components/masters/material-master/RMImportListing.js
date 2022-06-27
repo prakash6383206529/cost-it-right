bb62bf83f8cf948748547bd6aa34cb52cf40f803
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-06-27 19:58:39 +0530

    simulation checks removed from all masters

--- src/components/masters/material-master/RMImportListing.js
+++ src/components/masters/material-master/RMImportListing.js
@@ -157,16 +157,6 @@ function RMImportListing(props) {
   }, [])
 
 
-  const getFilterRMData = () => {
-    if (isSimulation) {
-      return getFilteredData(rmImportDataList, RM_MASTER_ID)
-    } else {
-
-      return rmImportDataList
-    }
-  }
-
-
   /**
   * @method hideForm
   * @description HIDE DOMESTIC, IMPORT FORMS
@@ -531,7 +521,7 @@ function RMImportListing(props) {
         tempArr.push(item.data)
       }))
     } else {
-      tempArr = getFilterRMData()
+      tempArr = rmImportDataList
     }
     return returnExcelColumn(RMIMPORT_DOWNLOAD_EXCEl, tempArr)
   };
@@ -716,7 +706,7 @@ function RMImportListing(props) {
           </Row>
           <Row>
             <Col>
-              <div className={`ag-grid-wrapper ${getFilterRMData() && getFilterRMData()?.length <= 0 ? "overlay-contain" : ""}`}>
+              <div className={`ag-grid-wrapper ${rmImportDataList && rmImportDataList?.length <= 0 ? "overlay-contain" : ""}`}>
                 <div className={`ag-theme-material ${loader && "max-loader-height"}`}>
                   <AgGridReact
                     style={{ height: '100%', width: '100%' }}
@@ -724,7 +714,7 @@ function RMImportListing(props) {
                     floatingFilter={true}
 
                     domLayout='autoHeight'
-                    rowData={getFilterRMData()}
+                    rowData={rmImportDataList}
                     pagination={true}
                     paginationPageSize={globalTake}
                     onGridReady={onGridReady}
