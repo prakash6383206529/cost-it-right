4c1466ef955349f0767e038b54b439eadacf7ca2
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-06-27 20:11:20 +0530

    costing detail report pagination code updated

--- src/components/report/components/CostingDetailReport.js
+++ src/components/report/components/CostingDetailReport.js
@@ -508,7 +508,7 @@ function ReportListing(props) {
         setPageNo(1)
         setCurrentRowIndex(0)
         getTableData(0, defaultPageSize, true, floatingFilterData, false, true);
-        setGlobalTake(10)
+        setGlobalTake(defaultPageSize)
         setPageSize10(true)
         setPageSize50(false)
         setPageSize100(false)
