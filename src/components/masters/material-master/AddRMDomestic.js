0a01d9ea88128701ec584c963cc71f064fb91460
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-06-28 18:05:48 +0530

    CIR 2181 RM Master edit issue resolved

--- src/components/masters/material-master/AddRMDomestic.js
+++ src/components/masters/material-master/AddRMDomestic.js
@@ -1140,7 +1140,6 @@ class AddRMDomestic extends Component {
 
         }
 
-
         if (isSourceChange) {
           this.setState({ approveDrawer: true, approvalObj: formData })
           this.setState({ setDisable: false })
@@ -1148,11 +1147,9 @@ class AddRMDomestic extends Component {
 
         } else {
 
-
           if (isEditFlag) {
-
             if (uploadAttachements && DropdownChanged && Number(DataToChange.BasicRatePerUOM) === values.BasicRate && Number(DataToChange.ScrapRate) === values.ScrapRate
-              && Number(DataToChange.NetLandedCost) === values.NetLandedCost && DataToChange.Remark === values.Remark
+              && Number(DataToChange.NetLandedCost) === values.NetLandedCost && DataToChange.Remark === (values.Remark === '' ? null : values.Remark)
               && (Number(DataToChange.CutOffPrice) === values.cutOffPrice || values.cutOffPrice === undefined)
               && DataToChange.RawMaterialCode === values.Code) {
 
