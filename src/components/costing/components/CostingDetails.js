d38b15fba907da6afbbecf6334a9217e6efcf77c
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-07-08 16:19:53 +0530

    Lable for part number in costing updated to Assembly No./Part No.

--- src/components/costing/components/CostingDetails.js
+++ src/components/costing/components/CostingDetails.js
@@ -1722,7 +1722,7 @@ function CostingDetails(props) {
                       </Col>
                       <Col className="col-md-15">
                         <AsyncSearchableSelectHookForm
-                          label={"Assembly/Part No."}
+                          label={"Assembly No./Part No."}
                           name={"Part"}
                           placeholder={"Enter"}
                           Controller={Controller}
