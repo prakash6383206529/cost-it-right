0b188c46860eb6856ba3036c1134ed8870c67638
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-07-11 15:25:34 +0530

    CIR-I2273 | field disabled on view mode in calculator data

--- src/components/costing/components/WeightCalculatorDrawer/sheetMetal/Pipe.js
+++ src/components/costing/components/WeightCalculatorDrawer/sheetMetal/Pipe.js
@@ -15,7 +15,6 @@ import { reactLocalStorage } from 'reactjs-localstorage'
 import Toaster from '../../../../common/Toaster'
 import { G, KG, MG, STD, } from '../../../../../config/constants'
 import { AcceptableSheetMetalUOM } from '../../../../../config/masterData'
-import { ViewCostingContext } from '../../CostingDetails'
 import { debounce } from 'lodash'
 
 function IsolateReRender(control) {
@@ -53,10 +52,9 @@ function Pipe(props) {
     }
   }
 
-  const { rmRowData, isEditFlag, item } = props
+  const { rmRowData, isEditFlag, item, CostingViewMode } = props
 
   const costData = useContext(costingInfoContext)
-  const CostingViewMode = useContext(ViewCostingContext);
 
   const defaultValues = {
 
@@ -838,9 +836,8 @@ function Pipe(props) {
                 </Col>
               </Row>
             </div>
-            {
-              isEditFlag &&
 
+            {!CostingViewMode &&
               <div className="col-sm-12 text-right px-0 mt-4">
                 <button
                   type={'button'}
