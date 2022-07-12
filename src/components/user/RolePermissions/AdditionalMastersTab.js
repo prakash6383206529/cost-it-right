4fd1fc050e8c6978e76450714244aefc810bc40b
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-07-12 16:41:54 +0530

    RenderAction function made common for all tabs in user module

--- src/components/user/RolePermissions/AdditionalMastersTab.js
+++ src/components/user/RolePermissions/AdditionalMastersTab.js
@@ -8,7 +8,7 @@ import { Table, } from 'reactstrap';
 import NoContentFound from "../../common/NoContentFound";
 import { EMPTY_DATA } from "../../../config/constants"
 import { ADDITIONAL_MASTERS, } from "../../../config/constants";
-import Switch from "react-switch";
+import { renderActionCommon } from "../userUtil"
 
 class AdditionalMastersTab extends Component {
     constructor(props) {
@@ -25,13 +25,6 @@ class AdditionalMastersTab extends Component {
         }
     }
 
-    /**
-    * @method componentDidMount
-    * @description used to called after mounting component
-    */
-    componentDidMount() {
-
-    }
 
     UNSAFE_componentWillReceiveProps(nextProps) {
         if (nextProps.data !== this.state.data) {
@@ -191,60 +184,7 @@ class AdditionalMastersTab extends Component {
     */
     renderAction = (actions, parentIndex) => {
 
-
-        const { actionSelectList } = this.state;
-
-        let actionHeads = []
-
-        const { actionData } = this.state;
-        let actionNames = actionData && actionData.find(el => el.ModuleName === ADDITIONAL_MASTERS)
-        if (actionNames !== undefined) {
-            actionSelectList && actionSelectList.map((item, index) => {
-                if (item.Value === 0) return false;
-                if (actionNames.ActionItems && actionNames.ActionItems.includes(item.Text)) {
-                    actionHeads.push(item.Value)           //COLLECTING ACTION ID OF ALL ACTIONS IN HEADER 
-                }
-            })
-        }
-
-        return actionHeads && actionHeads.map((el, i) => {
-            let value = false    // BOOLEAN VALUE TO MANAGE WEATHER TO RETURN A SWITCH BUTTON OR A EMPTY DIV
-
-            return actions && actions.map((item, index) => {        // ACTIONS ARRAY IS ALL ACTION ID OF THE ACTUAL PERMISION THAT IS GIVEN
-
-                if (item.ActionId !== el && index !== actions.length - 1) { return false }    // RETURN FALSE IF ACTION ID IS NOT EQUAL TO ELEMENT IN THE ACTIONHEAD ARRAY & INDEX IS NOT THE LAST INDEX
-                else if (item.ActionId === el) {
-                    value = true          // MAKING VALUE TRUE IF A SWITCH BUTTON WILL BE RETURNED (OF GIVEN PERMISSION)
-                }
-                if (item.ActionId !== el && index === actions.length - 1 && value === false) {   // RETURN EMPTY DIV IF ACTION ID IS NOT EQUAL TO ELEMENT IN THE ACTIONHED ARRAY & INDEX IS THE LAST INDEX
-                    return <div></div>
-                }
-                if (item.ActionId !== el && index === actions.length - 1 && value === true) { return false }
-
-                return (
-                    <td className="text-center">
-                        {
-                            <label htmlFor="normal-switch" className="normal-switch">
-                                <Switch
-                                    onChange={() => this.actionCheckHandler(parentIndex, index)}
-                                    checked={item.IsChecked}
-                                    value={item.ActionId}
-                                    id="normal-switch"
-                                    onColor="#4DC771"
-                                    onHandleColor="#ffffff"
-                                    offColor="#959CB6"
-                                    checkedIcon={false}
-                                    uncheckedIcon={false}
-                                    height={18}
-                                    width={40}
-                                />
-                            </label>
-                        }
-                    </td>
-                )
-            })
-
-        })
+        return renderActionCommon(actions, parentIndex, this, ADDITIONAL_MASTERS)
     }
 
     /**
