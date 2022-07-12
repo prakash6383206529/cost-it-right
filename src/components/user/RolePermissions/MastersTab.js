c79ad70a4c428ecbd447a323bb1a60a7d8f03ecc
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-07-12 13:00:53 +0530

    User tab manage roles and permisions toggle swtich issue resolved

--- src/components/user/RolePermissions/MastersTab.js
+++ src/components/user/RolePermissions/MastersTab.js
@@ -191,12 +191,36 @@ class MastersTab extends Component {
   * @description used to render row of actions
   */
   renderAction = (actions, parentIndex) => {
-    const { actionSelectList } = this.state;
 
-    return actionSelectList && actionSelectList.map((el, i) => {
-      if (el.Value === 0) return false;
-      return actions && actions.map((item, index) => {
-        if (el.Value !== item.ActionId) return false;
+    const { actionSelectList } = this.state;
+    let actionHeads = []
+
+    const { actionData } = this.state;
+    let actionNames = actionData && actionData.find(el => el.ModuleName === MASTERS)
+    if (actionNames !== undefined) {
+      actionSelectList && actionSelectList.map((item, index) => {
+        if (item.Value === 0) return false;
+        if (actionNames.ActionItems && actionNames.ActionItems.includes(item.Text)) {
+          actionHeads.push(item.Value)    //COLLECTING ACTION ID OF ALL ACTIONS IN HEADER 
+        }
+      })
+    }
+
+
+    return actionHeads && actionHeads.map((el, i) => {
+      let value = false    // BOOLEAN VALUE TO MANAGE WEATHER TO RETURN A SWITCH BUTTON OR A EMPTY DIV
+
+      return actions && actions.map((item, index) => {    // ACTIONS ARRAY IS ALL ACTION ID OF THE ACTUAL PERMISION THAT IS GIVEN
+
+        if (item.ActionId !== el && index !== actions.length - 1) { return false }   // RETURN FALSE IF ACTION ID IS NOT EQUAL TO ELEMENT IN THE ACTIONHEAD ARRAY & INDEX IS NOT THE LAST INDEX
+        else if (item.ActionId === el) {
+          value = true                 // MAKING VALUE TRUE IF A SWITCH BUTTON WILL BE RETURNED (OF GIVEN PERMISSION)
+        }
+        if (item.ActionId !== el && index === actions.length - 1 && value === false) {        // RETURN EMPTY DIV IF ACTION ID IS NOT EQUAL TO ELEMENT IN THE ACTIONHED ARRAY & INDEX IS THE LAST INDEX
+          return <div></div>
+        }
+        if (item.ActionId !== el && index === actions.length - 1 && value === true) { return false }
+
         return (
           <td className="text-center">
             {
@@ -219,7 +243,9 @@ class MastersTab extends Component {
           </td>
         )
       })
+
     })
+
   }
 
   /**
