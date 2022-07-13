a865b9b7a3688861ad8ad14ad7c74becf96eecc1
Banti Prajapati <banti.prajapati@systematixindia.com>
2022-07-13 18:52:26 +0530

    replace function name from checkSpaceForCode to checkSpacesInString

--- src/components/masters/machine-master/AddMachineRate.js
+++ src/components/masters/machine-master/AddMachineRate.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import { Field, reduxForm, formValueSelector, isDirty } from "redux-form";
 import { Row, Col, Table } from 'reactstrap';
 import {
-  required, checkForNull, number, postiveNumber, checkForDecimalAndNull, acceptAllExceptSingleSpecialCharacter,
-  checkWhiteSpaces, maxLength80, maxLength10, positiveAndDecimalNumber, maxLength512, checkSpaceForCode
+  required, checkForNull, postiveNumber, checkForDecimalAndNull, acceptAllExceptSingleSpecialCharacter,
+  checkWhiteSpaces, maxLength80, maxLength10, positiveAndDecimalNumber, maxLength512, checkSpacesInString
 } from "../../../helper/validation";
 import { renderText, searchableSelect, renderTextAreaField, focusOnError, renderDatePicker } from "../../layout/FormInputs";
 import { getPlantSelectListByType, getPlantBySupplier, getUOMSelectList } from '../../../actions/Common';
@@ -1382,7 +1382,7 @@ class AddMachineRate extends Component {
                             name={"MachineNumber"}
                             type="text"
                             placeholder={'Enter'}
-                            validate={initialConfiguration.IsMachineNumberConfigure ? [] : [required, checkSpaceForCode]}
+                            validate={initialConfiguration.IsMachineNumberConfigure ? [] : [required, checkSpacesInString]}
                             component={renderText}
                             required={initialConfiguration.IsMachineNumberConfigure ? false : true}
                             onBlur={this.checkUniqNumber}
--- src/components/masters/supplier-master/AddVendorDrawer.js
+++ src/components/masters/supplier-master/AddVendorDrawer.js
@@ -4,7 +4,7 @@ import { Field, reduxForm } from "redux-form";
 import { Container, Row, Col, } from 'reactstrap';
 import {
     required, upper, email, minLength7, maxLength70, maxLength80, minLength10, maxLength71, maxLength5, maxLength12, alphaNumeric, acceptAllExceptSingleSpecialCharacter,
-    postiveNumber, maxLength6, checkWhiteSpaces, checkSpaceForCode
+    postiveNumber, maxLength6, checkWhiteSpaces, checkSpacesInString
 } from "../../../helper/validation";
 import { renderText, renderEmailInputField, renderMultiSelectField, searchableSelect, renderNumberInputField } from "../../layout/FormInputs";
 import { createSupplierAPI, updateSupplierAPI, getSupplierByIdAPI, getRadioButtonSupplierType, getVendorTypesSelectList, } from '../actions/Supplier';
@@ -490,7 +490,7 @@ class AddVendorDrawer extends Component {
                                             name={"VendorCode"}
                                             type="text"
                                             placeholder={''}
-                                            validate={[required, alphaNumeric, maxLength71, checkWhiteSpaces, checkSpaceForCode]}
+                                            validate={[required, alphaNumeric, maxLength71, checkWhiteSpaces, checkSpacesInString]}
                                             component={renderText}
                                             required={true}
                                             normalize={upper}
