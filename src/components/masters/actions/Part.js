189438a65669777556895849d1af36afe785d6a8
Banti Prajapati <banti.prajapati@softude.com>
2022-07-18 15:03:37 +0530

    CIR-I1904 | no content image in part master and machine master issue fixed

--- src/components/masters/actions/Part.js
+++ src/components/masters/actions/Part.js
@@ -107,7 +107,7 @@ export function getPartDataList(skip, take, obj, isPagination, callback) {
         var queryParams3 = `effectiveDate=${obj.newDate !== null && obj.newDate !== undefined ? obj.newDate : ""}&partNumber=${obj.PartNumber !== null || obj.PartNumber !== "" ? obj.PartNumber : ""}&partName=${obj.PartName !== null || obj.PartName !== "" ? obj.PartName : ""}&ecnNumber=${obj.ECNNumber !== null || obj.ECNNumber !== "" ? obj.ECNNumber : ""}&revisionNumber=${obj.RevisionNumber !== null || obj.RevisionNumber !== "" ? obj.RevisionNumber : ""}&drawingNumber=${obj.DrawingNumber !== null || obj.DrawingNumber !== "" ? obj.DrawingNumber : ""}`
         const request = axios.get(`${API.getPartDataList}?${queryParams}&${queryParams1}&${queryParams2}&${queryParams3}`, config());
         request.then((response) => {
-            if (response?.data?.Result === true) {
+            if (response?.data?.Result === true || response.status === 204) {
                 dispatch({
                     type: GET_ALL_NEW_PARTS_SUCCESS,
                     payload: response?.status === 204 ? [] : response.data.DataList,
