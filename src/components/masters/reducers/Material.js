d601d53d20e5d5960c8241169ae7a545be5f5e7f
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-07-25 12:36:10 +0530

    dynamic parameters added in approval status tab pagination for all masters

--- src/components/masters/reducers/Material.js
+++ src/components/masters/reducers/Material.js
@@ -324,10 +324,39 @@ export default function materialReducer(state = initialState, action) {
             //     }
             // })
 
+            let array = []
+            if (action.payload[0].Plant !== undefined) {
+                array = action.payload && action.payload.filter((item) => {
+                    return item.Plants = item.Plant
+                })
+            }
+
+            if (action.payload[0].OperationId !== undefined) {
+                array = action.payload && action.payload.filter((item) => {
+                    return (item.TechnologyName = item.Technology,
+                        item.UOM = item.UnitOfMeasurement,
+                        item.BasicRate = item.Rate
+
+                    )
+                })
+            }
+
+            if (action.payload[0].MachineId !== undefined || action.payload[0].MachineId !== null) {
+                array = action.payload && action.payload.filter((item) => {
+                    return (
+                        item.BasicRate = item.MachineRate
+
+                    )
+                })
+            }
+
+            if (array.length === 0) {
+                array = action.payload
+            }
             return {
                 ...state,
                 loading: false,
-                approvalList: action.payload
+                approvalList: array
             }
         default:
             return state;
