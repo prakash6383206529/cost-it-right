a07065f9d17bfe8c8479508e3f3375961acdcb1e
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-07-25 14:15:27 +0530

    toaster message correction

--- src/components/masters/bop-master/AddBOPDomestic.js
+++ src/components/masters/bop-master/AddBOPDomestic.js
@@ -591,7 +591,7 @@ class AddBOPDomestic extends Component {
     if (DropdownChanged && (DataToCheck.Remark) === (values.Remark) && uploadAttachements && Number(DataToCheck.BasicRate) === Number(values.BasicRate) &&
       ((DataToCheck.Source ? DataToCheck.Source : '-') === (values.Source ? values.Source : '-')) &&
       ((DataToCheck.SourceLocation ? DataToCheck.SourceLocation : '-') === (sourceLocation.value ? sourceLocation.value : '-'))) {
-      Toaster.warning('Please change data to send bop for approval')
+      Toaster.warning('Please change data to send BOP for approval')
       return false;
     }
 
@@ -709,14 +709,14 @@ class AddBOPDomestic extends Component {
             Number(DataToCheck.BasicRate) === Number(values.BasicRate) && DataToCheck.Remark === values.Remark && uploadAttachements &&
             ((DataToCheck.Source ? DataToCheck.Source : '-') === (values.Source ? values.Source : '-')) &&
             ((DataToCheck.SourceLocation ? DataToCheck.SourceLocation : '-') === (sourceLocation.value ? sourceLocation.value : '-'))) {
-            Toaster.warning('please change data to send bop for approval')
+            Toaster.warning('Please change data to send BOP for approval')
             // this.cancel()
             return false;
           }
         }
         if (Boolean(DataToCheck.IsVendor) === false) {
           if (Number(DataToCheck.BasicRate) === Number(values.BasicRate) && DataToCheck.Remark === values.Remark && uploadAttachements) {
-            Toaster.warning('please change data to send bop for approval')
+            Toaster.warning('Please change data to send BOP for approval')
             // this.cancel()
             return false;
           }
@@ -727,7 +727,7 @@ class AddBOPDomestic extends Component {
         }
 
         if ((DataToCheck.Remark) === (values.Remark) && uploadAttachements) {
-          Toaster.warning('Please change data to send bop for approval')
+          Toaster.warning('Please change data to send BOP for approval')
           return false;
         } else {
 
