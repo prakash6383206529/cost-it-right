f20f3e082b0e535c21b1637b0b0cc544e330e9ed
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-07-25 11:11:08 +0530

    CIR-I2278|Masters>BOP Master>manage SOB >Update API is not called

--- src/components/masters/bop-master/SOBListing.js
+++ src/components/masters/bop-master/SOBListing.js
@@ -169,13 +169,15 @@ class SOBListing extends Component {
   * @method closeDrawer
   * @description Filter listing
   */
-  closeDrawer = (e = '') => {
+  closeDrawer = (e = '', type) => {
     this.setState({
       isOpen: false,
       isEditFlag: false,
       ID: '',
     }, () => {
-      this.getDataList()
+      if (type === 'submit') {
+        this.getDataList()
+      }
     })
   }
 
