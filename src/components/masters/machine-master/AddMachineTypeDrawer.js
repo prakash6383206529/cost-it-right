8b6c1462b2b61c23c1e919fcab8bfe0ed3708a55
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-07-27 19:35:35 +0530

    CIR-I2458| Labour masters | Add labour type | Labour type should not come in drop down list once user select && CIR-I2456|Labour masters | Add labour type | Employed | Add machine type & labour type | In labour type drop down data is missing

--- src/components/masters/machine-master/AddMachineTypeDrawer.js
+++ src/components/masters/machine-master/AddMachineTypeDrawer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from "redux-form";
 import { Container, Row, Col, } from 'reactstrap';
+import { getLabourTypeByMachineTypeSelectList, } from '../actions/Labour'
 import { required, checkWhiteSpaces } from "../../../helper/validation";
 import { renderText, renderMultiSelectField, } from "../../layout/FormInputs";
 import { createMachineType } from '../actions/MachineMaster';
@@ -78,7 +79,6 @@ class AddMachineTypeDrawer extends Component {
   */
   onSubmit = (values) => {
     const { labourType } = this.state;
-
     let labourTypeIds = labourType && labourType.map(el => el.Value)
 
     /** Update existing detail of supplier master **/
@@ -100,6 +100,7 @@ class AddMachineTypeDrawer extends Component {
       this.props.reset()
       this.props.createMachineType(formData, (res) => {
         if (res.data.Result) {
+          this.props.getLabourTypeByMachineTypeSelectList(res?.data?.Identity, () => { })
           Toaster.success(MESSAGES.MACHINE_TYPE_ADD_SUCCESS);
           this.toggleDrawer('', formData)
         }
@@ -235,6 +236,7 @@ function mapStateToProps({ comman }) {
 export default connect(mapStateToProps, {
   createMachineType,
   getLabourTypeSelectList,
+  getLabourTypeByMachineTypeSelectList
 })(reduxForm({
   form: 'AddMachineTypeDrawer',
   enableReinitialize: true,
