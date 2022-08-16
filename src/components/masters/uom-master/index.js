302a87d8ee591ace49db44bed17692e462560258
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-08-16 13:00:40 +0530

    UOM master listing header checkbox removed

--- src/components/masters/uom-master/index.js
+++ src/components/masters/uom-master/index.js
@@ -335,7 +335,6 @@ class UOMMaster extends Component {
       filter: true,
       sortable: true,
       headerCheckboxSelectionFilteredOnly: true,
-      headerCheckboxSelection: isFirstColumn,
       checkboxSelection: isFirstColumn
     };
     const frameworkComponents = {
