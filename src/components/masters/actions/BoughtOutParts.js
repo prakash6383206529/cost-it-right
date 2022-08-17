dac8a223b000b62e7f158d41a320c8f575ce4de7
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-08-17 19:57:06 +0530

    CIR-I2594|Master | BOP Master| Manage SOB Drawer

--- src/components/masters/actions/BoughtOutParts.js
+++ src/components/masters/actions/BoughtOutParts.js
@@ -527,11 +527,12 @@ export function getManageBOPSOBDataList(data, callback) {
  * @method getManageBOPSOBById
  * @description GET MANAGE BOP SOB BY ID
  */
-export function getManageBOPSOBById(bopId, callback) {
+export function getManageBOPSOBById(boughtOutPartNumber, callback) {
     return (dispatch) => {
         dispatch({ type: API_REQUEST });
-        if (bopId !== '') {
-            axios.get(`${API.getManageBOPSOBById}/${bopId}`, config())
+        if (boughtOutPartNumber !== '') {
+            const queryParams = `boughtOutPartNumber=${boughtOutPartNumber}`
+            axios.get(`${API.getManageBOPSOBById}?${queryParams}`, config())
                 .then((response) => {
                     if (response.data.Result) {
                         dispatch({
