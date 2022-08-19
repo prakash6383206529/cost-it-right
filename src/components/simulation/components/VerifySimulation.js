92862169797e965e8e1b9a54a5d84c2a04d04445
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-08-19 16:43:50 +0530

    unused code removed

--- src/components/simulation/components/VerifySimulation.js
+++ src/components/simulation/components/VerifySimulation.js
@@ -336,7 +336,6 @@ function VerifySimulation(props) {
 
     const onRowSelect = () => {
         var selectedRows = gridApi.getSelectedRows();
-        if (JSON.stringify(selectedRows) === JSON.stringify('')) return false
         setSelectedRowData(selectedRows)
     }
 
