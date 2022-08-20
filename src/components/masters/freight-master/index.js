9847f9279acf8b504f88af115050634957fa37e8
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-08-20 16:31:38 +0530

    active tab string changes

--- src/components/masters/freight-master/index.js
+++ src/components/masters/freight-master/index.js
@@ -163,7 +163,7 @@ class FreightMaster extends Component {
 
                             <TabContent activeTab={this.state.activeTab}>
 
-                                {this.state.activeTab === 1 &&
+                                {this.state.activeTab === '1' &&
                                     <TabPane tabId="1">
                                         <FreightListing
                                             displayForm={this.displayFreightForm}
@@ -176,7 +176,7 @@ class FreightMaster extends Component {
                                         />
                                     </TabPane>}
 
-                                {this.state.activeTab === 2 &&
+                                {this.state.activeTab === '2' &&
                                     <TabPane tabId="2">
                                         <PackagListing
                                             displayForm={this.displayPackagForm}
