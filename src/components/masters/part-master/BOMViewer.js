52806ee073e6c2fc9a9b9127809ed18f53aa34b0
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-08-22 14:54:59 +0530

    BOP Update quantity drawer work in progress in Bom Viewer

--- src/components/masters/part-master/BOMViewer.js
+++ src/components/masters/part-master/BOMViewer.js
@@ -25,6 +25,7 @@ class BOMViewer extends Component {
       displayEditIcon: false,
       EditIndex: '',
       updatedQuantity: '',
+      BOPUom: '',
       isOpenVisualDrawer: false,
       selected_point: null,
       flowpoints: [],
@@ -240,8 +241,8 @@ class BOMViewer extends Component {
   * @method editLevelOne
   * @description EDIT LEVEL 1 QUANTITY IN DRAWER
   */
-  editLevelOne(index, quantity, partType) {
-    this.setState({ isOpenVisualDrawer: true, EditIndex: index, updatedQuantity: quantity, partType: partType })
+  editLevelOne(index, quantity, partType, BoughtOutPartUOM) {
+    this.setState({ isOpenVisualDrawer: true, EditIndex: index, updatedQuantity: quantity, partType: partType, BOPUom: BoughtOutPartUOM })
   }
 
   /**
@@ -476,7 +477,7 @@ class BOMViewer extends Component {
                                     <button
                                       className="Edit"
                                       onClick={() =>
-                                        this.editLevelOne(i, el.Quantity, el.PartType)
+                                        this.editLevelOne(i, el.Quantity, el.PartType, el.BoughtOutPartUOM)
                                       }
                                     />
                                   </span>
@@ -549,6 +550,7 @@ class BOMViewer extends Component {
                 anchor={"right"}
                 updatedQuantity={this.state.updatedQuantity}
                 partType={this.state.partType}
+                BOPUom={this.state.BOPUom}
               />
             )}
           </>
