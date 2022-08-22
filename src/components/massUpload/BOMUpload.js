8f6bab91e39e72ea244f4f2ee74fd94f2d56456a
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-08-22 16:58:46 +0530

    CIR-I2018 | Empty object in payload fixed

--- src/components/massUpload/BOMUpload.js
+++ src/components/massUpload/BOMUpload.js
@@ -82,7 +82,8 @@ class BOMUpload extends Component {
           let fileData = [];
 
           resp.rows.map((val, index) => {
-            if (index > 0) {
+            if (val === []) return false
+            if (index > 0 && val?.length > 0) {
 
               // BELOW CODE FOR HANDLE EMPTY CELL VALUE
               const i = val.findIndex(e => e === undefined);
