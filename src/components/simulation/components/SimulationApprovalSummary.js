078ac59b032f531262da37eb3538ed67575845eb
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-08-26 19:34:39 +0530

    NoOfDecimalForPrice key for Impact per year

--- src/components/simulation/components/SimulationApprovalSummary.js
+++ src/components/simulation/components/SimulationApprovalSummary.js
@@ -1045,7 +1045,7 @@ function SimulationApprovalSummary(props) {
                                             </th>
                                             <th className="align-top">
                                                 <span className="d-block grey-text">{`Impact for Quarter(INR):`}</span>
-                                                <span className="d-block">{simulationDetail && (simulationDetail?.TotalImpactPerQuarter ? simulationDetail?.TotalImpactPerQuarter : '-')}</span>
+                                                <span className="d-block">{simulationDetail && (simulationDetail?.TotalImpactPerQuarter ? checkForDecimalAndNull(simulationDetail?.TotalImpactPerQuarter, getConfigurationKey.NoOfDecimalForPrice) : '-')}</span>
                                             </th>
                                             {/* <th className="align-top">
                                                 <span className="d-block grey-text">{`Impact for Annum(INR):`}</span>
