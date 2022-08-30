ee1c2e2a36bc16fa63364682a66fd76d997d80ff
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-08-30 18:17:01 +0530

    CIR-I2553|Additional master | Reason master | Download File | Reason Active in- actve section missing

--- src/components/masters/plant-master/ZBCPlantListing.js
+++ src/components/masters/plant-master/ZBCPlantListing.js
@@ -522,6 +522,7 @@ class ZBCPlantListing extends Component {
                                 imagClass: 'imagClass'
                             }}
                             rowSelection={'multiple'}
+                            suppressRowClickSelection={true}
                             onSelectionChanged={this.onRowSelect}
                             onFilterModified={this.onFloatingFilterChanged}
                             frameworkComponents={frameworkComponents}
