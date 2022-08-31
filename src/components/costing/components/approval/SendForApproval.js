fa169dc8789c5331ee1cadff5da99e1aa58667c4
Banti Prajapati <banti.prajapati@softude.com>
2022-08-31 16:11:28 +0530

    CIR-I2366 | effective date issue fixed

--- src/components/costing/components/approval/SendForApproval.js
+++ src/components/costing/components/approval/SendForApproval.js
@@ -479,7 +479,7 @@ const SendForApproval = (props) => {
       setIsVerifyImpactDrawer(false);
     }
   }
-  const tooltipText = `The current impact is calculated based on the data present in the volume master (${effectiveDate}).`
+
   return (
     <Fragment>
       <Drawer
@@ -650,7 +650,7 @@ const SendForApproval = (props) => {
                           </Col>
                           <Col md="4">
                             <div className="form-group">
-                              <TooltipCustom tooltipText={tooltipText} />
+                              <TooltipCustom tooltipText={`The current impact is calculated based on the data present in the volume master (${data.effectiveDate !== "" ? DayTime(data.effectiveDate).format('DD/MM/YYYY') : ""}).`} />
                               <label>Annual Impact</label>
                               <label className={data.oldPrice === 0 ? `form-control bg-grey input-form-control` : `form-control bg-grey input-form-control ${data.annualImpact < 0 ? 'green-value' : 'red-value'}`}>
                                 {data.annualImpact && data.annualImpact ? checkForDecimalAndNull(data.annualImpact, initialConfiguration.NoOfDecimalForPrice) : 0}
