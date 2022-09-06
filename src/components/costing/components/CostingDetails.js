757e25835c579508bf9c1397ab055cbfc16184ea
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-09-06 14:51:42 +0530

    CIR-I2773 | SOB accepting Minus values ZBC

--- src/components/costing/components/CostingDetails.js
+++ src/components/costing/components/CostingDetails.js
@@ -686,21 +686,21 @@ function CostingDetails(props) {
    * @method checkSOBNegativeExist
    * @description HANDLE COSTING CHANGE
    */
-  const checkSOBNegativeExist = () => {
+  const checkSOBNegativeExist = (costingHead) => {
     let checkIfNegative = false
-
-    zbcPlantGrid && zbcPlantGrid.length > 0 && zbcPlantGrid.filter(el => {
-      if (el.ShareOfBusinessPercent < 0) {
-        checkIfNegative = true
-      }
-    }, 0)
-
-    vbcVendorGrid && vbcVendorGrid.length > 0 && vbcVendorGrid.filter(el => {
-      if (el.ShareOfBusinessPercent < 0) {
-        checkIfNegative = true
-      }
-    }, 0)
-
+    if (costingHead === ZBC) {
+      zbcPlantGrid && zbcPlantGrid.length > 0 && zbcPlantGrid.filter(el => {
+        if (el.ShareOfBusinessPercent < 0) {
+          checkIfNegative = true
+        }
+      }, 0)
+    } else {
+      vbcVendorGrid && vbcVendorGrid.length > 0 && vbcVendorGrid.filter(el => {
+        if (el.ShareOfBusinessPercent < 0) {
+          checkIfNegative = true
+        }
+      }, 0)
+    }
     return checkIfNegative
   }
 
@@ -1527,6 +1527,9 @@ function CostingDetails(props) {
     if (findIndex !== -1) {
       Toaster.warning('SOB could not be empty.')
       return false;
+    } else if (checkSOBNegativeExist(ZBC)) {
+      Toaster.warning('SOB could not be negative.')
+      return false;
     } else {
       setZBCEnableSOBField(!isZBCSOBEnabled)
     }
@@ -1541,7 +1544,7 @@ function CostingDetails(props) {
     if (!checkSOBTotal()) {
       Toaster.warning('SOB Should not be greater than 100.')
       return false
-    } else if (checkSOBNegativeExist()) {
+    } else if (checkSOBNegativeExist(VBC)) {
       Toaster.warning('SOB could not be negative.')
       return false;
     } else if (findIndex !== -1) {
