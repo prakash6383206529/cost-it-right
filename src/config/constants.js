0d318c1ebb1dd995703ebfad1f59c4c530c3cc2d
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-09-13 17:46:13 +0530

    typos corrected in single dropdown floating filter

--- src/config/constants.js
+++ src/config/constants.js
@@ -1960,7 +1960,7 @@ export const statusOptions = [
   { label: "Rejected", value: "4" },
   { label: "History", value: "5" },
   { label: "AwaitingApproval", value: "6" },
-  { label: "SendForApproval", value: "7" },
+  // { label: "SendForApproval", value: "7" },
   { label: "ApprovedByAssembly", value: "8" },
   { label: "ApprovedBySimulation", value: "9" },
   { label: "CreatedByAssembly", value: "10" },
