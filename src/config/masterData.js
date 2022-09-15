4b3edcf7ab7df97a34b0e55606b705eb06ab457e
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-09-15 12:27:57 +0530

    CIR-I2905|Plant(code) added in download interest rate

--- src/config/masterData.js
+++ src/config/masterData.js
@@ -1939,7 +1939,7 @@ export const BOPImportSimulation = [
 
 export const OverheadProfitSimulation = [
     { label: "CostingHead", value: "IsVendor" },
-    { label: "ClientName", value: "ClientName" },
+    { label: "CustomerName", value: "ClientName" },
     { label: "VendorName", value: "VendorName" },
     { label: "ModelType", value: "ModelType" },
     { label: "OverheadApplicabilityType", value: "OverheadApplicabilityType", },
@@ -2808,6 +2808,7 @@ export const POWERLISTING_VENDOR_DOWNLOAD_EXCEL = [
 
 export const INTERESTRATE_DOWNLOAD_EXCEl = [
     { label: "Costing Head", value: "CostingHead", },
+    { label: "Plant(Code)", value: "PlantName", },
     { label: "Vendor Name", value: "VendorName", },
     { label: "ICC Applicability", value: "ICCApplicability", },
     { label: "Annual ICC(%)", value: "ICCPercent", },
@@ -2921,7 +2922,7 @@ export const OVERHEAD_DOWNLOAD_EXCEl = [
     { label: "Costing Head", value: "CostingHead", },
     { label: "Plant(Code)", value: "PlantName", },
     { label: "Vendor(Code)", value: "VendorName", },
-    { label: "Client Name", value: "ClientName", },
+    { label: "Customer Name", value: "ClientName", },
     { label: "Model Type", value: "ModelType", },
     { label: "Overhead Applicability", value: "OverheadApplicabilityType", },
     { label: "Overhead Applicability(%)", value: "OverheadPercentage", },
@@ -3012,7 +3013,7 @@ export const PROFIT_DOWNLOAD_EXCEl = [
     { label: "Costing Head", value: "CostingHead", },
     { label: "Plant(Code)", value: "PlantName", },
     { label: "Vendor(code)", value: "VendorName", },
-    { label: "Client Name", value: "ClientName", },
+    { label: "Customer Name", value: "ClientName", },
     { label: "Model Type", value: "ModelType", },
     { label: "Profit Applicability", value: "ProfitApplicabilityType", },
     { label: "Profit Applicability(%)", value: "ProfitPercentage", },
