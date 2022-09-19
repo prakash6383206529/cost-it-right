146e80c7899d2f0c9fc6b4db4c1d7aadae7e6098
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-09-19 14:41:32 +0530

    V1.2.535

--- src/config/constants.js
+++ src/config/constants.js
@@ -16,9 +16,9 @@ export const config = () => {
 
 
 // DEVELOPMENT
-// const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 // const BASE_URL = `http://10.10.0.43/RohitCIR/api/v1`;
-const BASE_URL = `https://demov3api.costitright.com/api/v1`;
+//const BASE_URL = `https://demov3api.costitright.com/api/v1`;
 //const BASE_URL = `http://10.10.1.100:1002/api/v1`;
 // const BASE_URL = `https://upsapi.costitright.com/api/v1`; 
 //FILE URL
@@ -1985,4 +1985,4 @@ export const KEY = 'gQUJ79YKYm22Cazw';
 export const IV = 'eTEFSa0PinFKTQNB'
 
 //VERSION 
-export const VERSION = "V1.2.534";
+export const VERSION = "V1.2.535";
