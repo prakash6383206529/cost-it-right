145e1c8c4a4fe933219aed6515316bd5a9578cd7
Banti Prajapati <banti.prajapati@softude.com>
2022-09-21 14:27:21 +0530

    CIR-I2888 | overlapping on the below scroller issue fixed

--- src/components/masters/overhead-profit-master/OverheadListing.js
+++ src/components/masters/overhead-profit-master/OverheadListing.js
@@ -64,7 +64,8 @@ function OverheadListing(props) {
 
     var floatingFilterOverhead = {
         maxValue: 1,
-        suppressFilterButton: true
+        suppressFilterButton: true,
+        component: 'overhead'
     }
 
     var filterParams = {
@@ -112,7 +113,7 @@ function OverheadListing(props) {
         else {
             setNoData(false)
         }
-        dispatch(getGridHeight(overheadProfitList?.length))
+        dispatch(getGridHeight({ value: overheadProfitList?.length, component: 'overhead' }))
     }, [overheadProfitList])
 
 
