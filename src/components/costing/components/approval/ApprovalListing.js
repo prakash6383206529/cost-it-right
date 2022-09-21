1848aa19c3c09148c3ecdeef9477cb34364615e3
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-09-21 18:32:12 +0530

    costing approval Ncc condition fixed

--- src/components/costing/components/approval/ApprovalListing.js
+++ src/components/costing/components/approval/ApprovalListing.js
@@ -638,7 +638,7 @@ function ApprovalListing(props) {
       return false
     }
 
-    if (selectedRowData && selectedRowData[0]?.IsRegularizationLimitCrossed) {
+    if (selectedRowData && selectedRowData[0]?.IsRegularizationLimitCrossed !== 'No') {
       setShowPopup(true)
     } else {
       sendForApprovalDrawer()
