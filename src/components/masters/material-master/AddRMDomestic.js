fab5649203aba6fedc86cab045301ef78c8c51bb
Banti Prajapati <banti.prajapati@softude.com>
2022-09-22 20:44:31 +0530

    CIR-I2954 | save and send for approval button showing issue fixed

--- src/components/masters/material-master/AddRMDomestic.js
+++ src/components/masters/material-master/AddRMDomestic.js
@@ -113,7 +113,8 @@ class AddRMDomestic extends Component {
       inputLoader: false,
       attachmentLoader: false,
       showErrorOnFocus: false,
-      showErrorOnFocusDate: false
+      showErrorOnFocusDate: false,
+      finalApprovalLoader: false
     }
   }
   /**
@@ -157,9 +158,11 @@ class AddRMDomestic extends Component {
         LoggedInUserLevelId: userDetails().LoggedInMasterLevelId,
         LoggedInUserId: loggedInUserId()
       }
+      this.setState({ finalApprovalLoader: true })
       this.props.masterFinalLevelUser(obj, (res) => {
         if (res.data.Result) {
           this.setState({ isFinalApprovar: res.data.Data.IsFinalApprovar })
+          this.setState({ finalApprovalLoader: false })
         }
       })
     }
@@ -1200,7 +1203,7 @@ class AddRMDomestic extends Component {
 
     return (
       <>
-        {this.state.isLoader && <LoaderCustom customClass="add-page-loader" />}
+        {(this.state.isLoader || this.state.finalApprovalLoader) && <LoaderCustom customClass="add-page-loader" />}
         <div className="container-fluid">
           <div>
             <div className="login-container signup-form">
