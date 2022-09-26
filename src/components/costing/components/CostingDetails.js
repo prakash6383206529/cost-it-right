15f9df64b42c3450e93c3dd16b2f31d8095670bf
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-09-26 18:15:54 +0530

    CIR-I3034 | Costing |  Should not accept  more than 100% | SOB must not accept after decimal value

--- src/components/costing/components/CostingDetails.js
+++ src/components/costing/components/CostingDetails.js
@@ -461,7 +461,7 @@ function CostingDetails(props) {
     if (!isNaN(event.target.value)) {
       tempData = {
         ...tempData,
-        ShareOfBusinessPercent: parseInt(event.target.value),
+        ShareOfBusinessPercent: event.target.value,
         isSOBChanged: checkIsZBCSOBChanged(event, index),
       }
       tempArray = Object.assign([...zbcPlantGrid], { [index]: tempData })
@@ -667,7 +667,7 @@ function CostingDetails(props) {
     if (!isNaN(event.target.value)) {
       tempData = {
         ...tempData,
-        ShareOfBusinessPercent: parseInt(event.target.value),
+        ShareOfBusinessPercent: event.target.value,
         isSOBChanged: checkIsVBCSOBChanged(event, index),
       }
       tempArray = Object.assign([...vbcVendorGrid], { [index]: tempData })
