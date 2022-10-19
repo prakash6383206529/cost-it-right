fdb4f721aef77500886823443e56ff521e84211d
Mahak <mahak.rawat@systematixindia.com>
2022-10-19 15:41:02 +0530

    Version update to 582

--- src/config/constants.js
+++ src/config/constants.js
@@ -2018,4 +2018,4 @@ export const IV = 'eTEFSa0PinFKTQNB'
 export const KEYRFQ = "UAGSqTBCbZ8JqHJl"
 export const IVRFQ = "8vFNmRQEl91nOtrM"
 //VERSION 
-export const VERSION = "V1.2.581";
+export const VERSION = "V1.2.582";
