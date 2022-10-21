662faba49c712affeed354930dba41f5699b984d
Banti Prajapati <banti.prajapati@softude.com>
2022-10-21 12:14:25 +0530

    version update :2~V1.2.584

--- src/config/constants.js
+++ src/config/constants.js
@@ -2080,4 +2080,4 @@ export const IV = 'ozzzguugcusjqmbj'
 export const KEYRFQ = "UAGSqTBCbZ8JqHJl"
 export const IVRFQ = "8vFNmRQEl91nOtrM"
 //VERSION 
-export const VERSION = "V1.2.589";
+export const VERSION = "V1.2.584";
