99c52839227f1f237a566aca4e5bbc25b772e639
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-11-08 17:23:47 +0530

    common function checks correction

--- src/components/common/CommonFunctions.js
+++ src/components/common/CommonFunctions.js
@@ -5,7 +5,7 @@ export const autoCompleteDropdown = (inputValue, dropdownArray, tempBoolean = fa
     tempArr = dropdownArray && dropdownArray.filter(i => {
         return i.Text?.toLowerCase().includes(inputValue?.toLowerCase())
     });
-    if (tempArr?.length) {
+    if (tempArr?.length <= 100) {
         tempArr && tempArr.map(item => {
             if (item.Value === '0') return tempArr
             if (tempBoolean && selectedParts.includes(item.Value)) return false
@@ -17,6 +17,7 @@ export const autoCompleteDropdown = (inputValue, dropdownArray, tempBoolean = fa
         const Arr = tempArr?.slice(0, 100)
         Arr && Arr.map(item => {
             if (item.Value === '0') return tempArr
+            if (tempBoolean && selectedParts.includes(item.Value)) return false
             finalArr.push({ label: item.Text, value: item.Value })
             return null
         })
