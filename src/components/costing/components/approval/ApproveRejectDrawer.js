777b58e7c018613aa85f8899567865df22572f73
Banti Prajapati <banti.prajapati@softude.com>
2022-11-11 15:24:11 +0530

    CIR-I3212 | final approval input field empty if permisson is not available

--- src/components/costing/components/approval/ApproveRejectDrawer.js
+++ src/components/costing/components/approval/ApproveRejectDrawer.js
@@ -94,7 +94,7 @@ function ApproveRejectDrawer(props) {
         const Data = res.data.SelectList
         const departObj = Data && Data.filter(item => item.Value === (type === 'Sender' ? userData.DepartmentId : simulationDetail.DepartmentId))
         setValue('dept', { label: departObj[0].Text, value: departObj[0].Value })
-        getApproversList(departObj[0].Value)
+        getApproversList(departObj[0].Value, departObj[0].Text)
 
       }))
 
@@ -131,7 +131,7 @@ function ApproveRejectDrawer(props) {
 
 
 
-  const getApproversList = (departObj) => {
+  const getApproversList = (departObj, departmentName) => {
     let values = []
     let approverDropdownValue = []
     let count = 0
@@ -243,6 +243,8 @@ function ApproveRejectDrawer(props) {
 
             Toaster.warning('User does not exist on next level for selected simulation.')
             setApprovalDropDown([])
+            setValue('dept', { label: departmentName, value: departObj })
+            setValue('approver', '')
             return false
           }
         }))
@@ -645,7 +647,7 @@ function ApproveRejectDrawer(props) {
         setApprovalDropDown(tempDropdownList)
       }))
     } else {
-      getApproversList(value.value)
+      getApproversList(value.value, value.label)
       // dispatch(
       //   getAllSimulationApprovalList(simObj, (res) => {
       //     res.data.DataList &&
