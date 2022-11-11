cdc70dff27c931fb6f4b7776bfab1d04c0413443
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-11-11 18:46:49 +0530

    CIR-I3308|Effective date check corrected

--- src/components/massUpload/BulkUpload.js
+++ src/components/massUpload/BulkUpload.js
@@ -261,7 +261,7 @@ class BulkUpload extends Component {
 
                             let obj = {}
                             val.map((el, i) => {
-                                if ((fileHeads[i] === 'EffectiveDate' || fileHeads[i] === 'DateOfPurchase') && typeof el === 'string') {
+                                if ((fileHeads[i] === 'EffectiveDate' || fileHeads[i] === 'DateOfPurchase') && typeof el === 'string' && el !== '') {
                                     el = (DayTime(Date(el))).format('YYYY-MM-DD 00:00:00')
                                 }
                                 if (fileHeads[i] === 'EffectiveDate' && typeof el === 'number') {
