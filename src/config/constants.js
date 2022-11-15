f398c6e587064b045b1f5fdebf4f2c97b10cc832
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-11-15 12:00:08 +0530

    version updated to V1.2.633

--- src/config/constants.js
+++ src/config/constants.js
@@ -2154,4 +2154,4 @@ export const IV = 'ozzzguugcusjqmbj'
 export const KEYRFQ = "UAGSqTBCbZ8JqHJl"
 export const IVRFQ = "8vFNmRQEl91nOtrM"
 //VERSION 
-export const VERSION = "V1.2.632";
+export const VERSION = "V1.2.633";
