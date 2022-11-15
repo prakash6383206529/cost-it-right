c2a29b31317663fbc4e550e5a69dde99ad8bfe70
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-11-15 12:57:56 +0530

    VERSION CHANGED V1.2.635

--- src/config/constants.js
+++ src/config/constants.js
@@ -2156,4 +2156,4 @@ export const IV = 'ozzzguugcusjqmbj'
 export const KEYRFQ = "UAGSqTBCbZ8JqHJl"
 export const IVRFQ = "8vFNmRQEl91nOtrM"
 //VERSION 
-export const VERSION = "V1.2.634";
+export const VERSION = "V1.2.635";
