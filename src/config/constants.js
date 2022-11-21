208e901f092cc5d31d3d681b325cc91e16e2e711
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-11-21 15:36:59 +0530

    V1.2.638

--- src/config/constants.js
+++ src/config/constants.js
@@ -2156,4 +2156,4 @@ export const IV = 'ozzzguugcusjqmbj'
 export const KEYRFQ = "UAGSqTBCbZ8JqHJl"
 export const IVRFQ = "8vFNmRQEl91nOtrM"
 //VERSION 
-export const VERSION = "V1.2.637";
+export const VERSION = "V1.2.638";
