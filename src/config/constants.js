05d6d28e6562a310e96fa9df7d25e0c489898146
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-11-25 11:51:43 +0530

    version updated to V1.2.643

--- src/config/constants.js
+++ src/config/constants.js
@@ -16,11 +16,12 @@ export const config = () => {
 }
 
 // DEVELOPMENT
-const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+// const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 // const BASE_URL = `http://10.10.8.160/api/v1`;
 // const BASE_URL = `http://10.10.0.43/RohitCIR/api/v1`;
 // const BASE_URL = `https://demov3api.costitright.com/api/v1`;
-//const BASE_URL = `http://10.10.8.117/CIR/api/v1`;
+// const BASE_URL = `http://10.10.8.117/CIR/api/v1`;
+const BASE_URL = `http://10.10.1.100:1005/api/v1`;
 // const BASE_URL = `https://upsapi.costitright.com/api/v1`; 
 //FILE URL
 export const FILE_URL = `${process.env.REACT_APP_FILE_URL}`;
@@ -2133,7 +2134,6 @@ export const statusOptions = [
   { label: "ApprovedByASMSimulation", value: "16" },
   { label: "Linked", value: "17" },
   { label: "RejectedBySystem", value: "18" },
-
 ]
 //AUTOCOMPLETE IN PART
 export const searchCount = 3
@@ -2155,5 +2155,6 @@ export const IV = 'ozzzguugcusjqmbj'
 
 export const KEYRFQ = "UAGSqTBCbZ8JqHJl"
 export const IVRFQ = "8vFNmRQEl91nOtrM"
+
 //VERSION 
-export const VERSION = "V1.2.642";
+export const VERSION = "V1.2.643";
