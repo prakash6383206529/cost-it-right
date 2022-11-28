be9016ec07b0abd0f2360186a832711fc4cf0f04
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-11-28 19:33:59 +0530

    Version updated to V1.2.648

--- src/config/constants.js
+++ src/config/constants.js
@@ -21,7 +21,7 @@ const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 // const BASE_URL = `http://10.10.0.43/RohitCIR/api/v1`;
 // const BASE_URL = `https://demov3api.costitright.com/api/v1`;
 // const BASE_URL = `http://10.10.8.117/CIR/api/v1`;
-// const BASE_URL = `http://10.10.1.100:1002/api/v1`;
+// const BASE_URL = `http://10.10.1.100:1005/api/v1`;
 // const BASE_URL = `https://upsapi.costitright.com/api/v1`; 
 //FILE URL
 export const FILE_URL = `${process.env.REACT_APP_FILE_URL}`;
@@ -2157,4 +2157,4 @@ export const KEYRFQ = "UAGSqTBCbZ8JqHJl"
 export const IVRFQ = "8vFNmRQEl91nOtrM"
 
 //VERSION 
-export const VERSION = "V1.2.647";
+export const VERSION = "V1.2.648";
