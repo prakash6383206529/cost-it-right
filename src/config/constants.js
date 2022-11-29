7937401e4cc44e2a3f09444135ca75c18d628f07
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-11-29 20:33:39 +0530

    Cost movement report by master in progress

--- src/config/constants.js
+++ src/config/constants.js
@@ -990,6 +990,7 @@ export const API = {
   getCostingReport: `${BASE_URL}/reports/get-costing-report`,
   getCostingBenchMarkRmReport: `${BASE_URL}/reports/get-rawmaterial-cost-benchmarking-report`,
   getCostingBenchMarkBopReport: `${BASE_URL}/reports/get-bop-cost-benchmarking-report`,
+  getCostMovementReport: `${BASE_URL}/reports/get-cost-movement-report-by-master`,
 
 
   //RFQ
