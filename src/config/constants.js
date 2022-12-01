e18e5fa44fb8c2cd80ad77bce49139ef0cd13e9f
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2022-12-01 15:13:41 +0530

    CIR-I3559|Spacing between vendor and plant issue resolved

--- src/config/constants.js
+++ src/config/constants.js
@@ -1803,7 +1803,7 @@ export const VIEW_COSTING_DATA = {
   partNumber: 'Part Number',
   partName: 'Part Name',
   RevisionNumber: 'Revision Number',
-  vendor: 'Vendor (Code)',
+  vendorName: 'Vendor (Code)',
   customer: 'Customer (Code)',
   plantExcel: 'Plant (Code)',
   status: 'Status',
