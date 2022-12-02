140c8a7a4f0bade54fd4fcf814d29582164a3fc9
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-12-02 19:53:20 +0530

    Cost movement report on-going

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,6 +56,7 @@ import SimulationInsights from './report/components/SimulationInsights'
 import SimulationRoutes from './simulation/Routes'
 import CommonApproval from './masters/material-master/CommonApproval'
 import RfqListing from './rfq/RfqListing'
+import CostMovementReport from './report/components/CostMovementReport/CostMovementReport'
 const CustomHeader = {
   'Content-Type': 'application/x-www-form-urlencoded',
   'Access-Control-Allow-Origin': '*',
@@ -337,14 +338,11 @@ class Main extends Component {
                     <Route path="/simulation-history" component={AuthMiddleware(SimulationRoutes, Simulation_Page)} />
 
                     <Route path='/simulation-approval-summary' component={AuthMiddleware(SimulationApprovalSummary, Simulation_Page)} />
-
                     <Route path="/simulation" component={SimulationRoutes} exact={true} />
-
                     <Route path="/simulation-upload" component={AuthMiddleware(SimulationUpload, Simulation_Upload)} />
-
-
                     <Route path="/costing-breakup-report" component={AuthMiddleware(CostingDetailReport, COSTING_BREAKUP_DETAILS_REPORT)} />
                     <Route path="/master-benchmarking-report" component={CostingBenchmarkReport} />
+                    <Route path="/cost-movement-report" component={CostMovementReport} />
                     {/*  NEED TO ADD PATH FROM BACKEND */}
                     <Route path="/simulation-insights" component={SimulationInsights} />
                     <Route path="/rfq-listing" component={AuthMiddleware(RfqListing, RFQ)} />
