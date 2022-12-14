0d0d1840379cf1962e537b062d51c1e17fe45dfa
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-12-14 19:13:40 +0530

    assemby technology simulation

--- src/components/simulation/components/RunSimulationDrawer.js
+++ src/components/simulation/components/RunSimulationDrawer.js
@@ -228,7 +228,7 @@ function RunSimulationDrawer(props) {
         // obj.IsProvisional = provisionalCheck
         // obj.LinkingTokenNumber = linkingTokenNumber != '' ? linkingTokenNumber : tokenNo
         temp.push(obj)
-        if (IdForMultiTechnology.includes(String(selectedMasterForSimulation.value))) {
+        if (checkForNull(selectedMasterForSimulation.value) === ASSEMBLY_TECHNOLOGY) {
             dispatch(runSimulationOnSelectedAssemblyTechnologyCosting({ ...objs, EffectiveDate: DayTime(date !== null ? date : "").format('YYYY/MM/DD HH:mm'), IsProvisional: provisionalCheck, SimulationApplicability: temp, SimulationId: props?.token }, (res) => {
                 checkForResponse(res)
             }))
