f74702dd6b130be36beb290643080dfb0ebac11d
Ashok Makhija <ashok.makhija@systematixindia.com>
2022-12-15 20:14:47 +0530

    CIR 3756 Master movement report currency issue resolved

--- src/components/dashboard/CostMovementGraph.js
+++ src/components/dashboard/CostMovementGraph.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Line } from 'react-chartjs-2';
-import { data1,options1} from "./ChartsDashboard";
+import { Bar, Line } from 'react-chartjs-2';
+import { data1, options1 } from "./ChartsDashboard";
 
 export function Costmovementgraph(props) {
-    const {graphData=data1,graphOptions=options1,graphHeight} = props;
+    const { graphData = data1, graphOptions = options1, graphHeight } = props;
     return (
         <>
             <div className="graph-container">
-                <Line data={graphData}  options={graphOptions} height={graphHeight} />
+                <Bar data={graphData} options={props.options1} height={graphHeight} />
             </div>
         </>
     )
