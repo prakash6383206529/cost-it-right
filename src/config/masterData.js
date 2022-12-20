d3050528824248fbec84a7f4281a03a6e28986b8
Mahak <mahak.rawat@systematixindia.com>
2022-12-20 15:14:36 +0530

    Add double RM in Sheet metal technology

--- src/config/masterData.js
+++ src/config/masterData.js
@@ -1782,7 +1782,7 @@ export const AcceptableSheetMetalUOM = ['Kilogram', 'Gram', 'Milligram']
 export const MULTIPLERMTECHNOLOGY = [4, 5, 7, 8, 14, 16, 17, 6, 9, 10, 2, 15, 23]
 
 export function isMultipleRMAllow(technology) {
-    const allowedMultipleRM = [4, 5, 7, 8, 14, 16, 17, 6, 9, 10, 2, 15, 23]
+    const allowedMultipleRM = [1, 4, 5, 7, 8, 14, 16, 17, 6, 9, 10, 2, 15, 23]
     return allowedMultipleRM.includes(technology);
 }
 
