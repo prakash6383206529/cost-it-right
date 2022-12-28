5ed1b1cf0d00cfe6f830f87597e14a056f44bbe5
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2022-12-28 18:57:57 +0530

    CIR-I3868 | RFQ| Part number should come on basis of revision no

--- src/components/common/CommonFunctions.js
+++ src/components/common/CommonFunctions.js
@@ -25,3 +25,30 @@ export const autoCompleteDropdown = (inputValue, dropdownArray, tempBoolean = fa
 
     }
 }
+
+export const autoCompleteDropdownPart = (inputValue, dropdownArray, tempBoolean = false, selectedParts = []) => {
+    let tempArr = []
+    let finalArr = []
+    tempArr = dropdownArray && dropdownArray.filter(i => {
+        return i.PartNumber?.toLowerCase().includes(inputValue?.toLowerCase())
+    });
+    if (tempArr?.length <= 100) {
+        tempArr && tempArr.map(item => {
+            if (item.PartId === '0') return tempArr
+            if (tempBoolean && selectedParts.includes(item.PartId)) return false
+            finalArr.push({ label: item.PartNumber, value: item.PartId, RevisionNumber: item.RevisionNumber })
+            return null
+        })
+        return finalArr
+    } else {
+        const Arr = tempArr?.slice(0, 100)
+        Arr && Arr.map(item => {
+            if (item.PartId === '0') return tempArr
+            if (tempBoolean && selectedParts.includes(item.PartId)) return false
+            finalArr.push({ label: item.PartNumber, value: item.PartId, RevisionNumber: item.RevisionNumber })
+            return null
+        })
+        return finalArr
+
+    }
+}
