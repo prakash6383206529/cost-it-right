ac1dcfd464674fa391c16f4a358c4420f1ab7d48
Ashok Makhija <ashok.makhija@systematixindia.com>
2023-01-11 19:11:24 +0530

    CIR 3910 Costing brekup details , ag grid reset issue resolved

--- src/components/report/components/CostingDetailReport.js
+++ src/components/report/components/CostingDetailReport.js
@@ -382,7 +382,6 @@ function ReportListing(props) {
         else {
             newData = data
         }
-        newData.IsCustomerDataShow = reactLocalStorage.getObject('cbcCostingPermission')
         dispatch(getCostingReport(skip, take, isPagination, newData, isLastWeek, isCallApi, (res) => {
 
             if (res) {
