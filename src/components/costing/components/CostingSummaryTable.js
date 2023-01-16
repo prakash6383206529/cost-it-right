3c9c36563c164f0f6145a9b10515b09e89c6711c
Banti Prajapati <banti.prajapati@softude.com>
2023-01-16 19:35:45 +0530

    CIR-I3922 | costing summary table makes legends dynamic

--- src/components/costing/components/CostingSummaryTable.js
+++ src/components/costing/components/CostingSummaryTable.js
@@ -34,7 +34,7 @@ import { IdForMultiTechnology } from '../../../config/masterData'
 import ViewMultipleTechnology from './Drawers/ViewMultipleTechnology'
 import TooltipCustom from '../../common/Tooltip'
 import { Costratiograph } from '../../dashboard/CostRatioGraph'
-import { graphColor1, graphColor2, graphColor3, graphColor4, graphColor5, graphColor6 } from '../../dashboard/ChartsDashboard'
+import { colorArray } from '../../dashboard/ChartsDashboard'
 import { LOGISTICS } from '../../../config/masterData'
 
 const SEQUENCE_OF_MONTH = [9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -92,6 +92,7 @@ const CostingSummaryTable = (props) => {
   const [isLockedState, setIsLockedState] = useState(false)
   const [viewMultipleTechnologyDrawer, setViewMultipleTechnologyDrawer] = useState(false)
   const [multipleTechnologyData, setMultipleTechnologyData] = useState([])
+  const [pieChartLabel, setPieChartLabel] = useState([])
 
   const viewCostingData = useSelector((state) => state.costing.viewCostingDetailData)
 
@@ -167,6 +168,44 @@ const CostingSummaryTable = (props) => {
     checkForDecimalAndNull(tempObj.totalToolCost, initialConfiguration.NoOfDecimalForPrice),
     ]
     setPieChartDataArray(temp)
+    let labelArray = []
+    let labels = ['RM', 'BOP', 'CC', 'ST', 'O&P', 'P&F', 'TC']
+    temp && temp.map((item, index) => {
+      if (item !== 0) {
+        labelArray.push(labels[index])
+        console.log(labelArray);
+      }
+    })
+    let dataArray = []
+    labelArray && labelArray.map(item => {
+      switch (item) {
+        case 'RM':
+          dataArray.push(checkForDecimalAndNull(tempObj.netRM, initialConfiguration.NoOfDecimalForPrice))
+          break;
+        case 'BOP':
+          dataArray.push(checkForDecimalAndNull(tempObj.netBOP, initialConfiguration.NoOfDecimalForPrice))
+          break;
+        case 'CC':
+          dataArray.push(checkForDecimalAndNull(tempObj.nConvCost, initialConfiguration.NoOfDecimalForPrice))
+          break;
+        case 'ST':
+          dataArray.push(checkForDecimalAndNull(tempObj.nsTreamnt, initialConfiguration.NoOfDecimalForPrice))
+          break;
+        case 'O&P':
+          dataArray.push(checkForDecimalAndNull(tempObj.nOverheadProfit, initialConfiguration.NoOfDecimalForPrice))
+          break;
+        case 'P&F':
+          dataArray.push(checkForDecimalAndNull(tempObj.nPackagingAndFreight, initialConfiguration.NoOfDecimalForPrice))
+          break;
+        case 'TC':
+          dataArray.push(checkForDecimalAndNull(tempObj.totalToolCost, initialConfiguration.NoOfDecimalForPrice))
+          break;
+        default:
+          break;
+      }
+    })
+    setPieChartLabel(labelArray)
+    setPieChartDataArray(dataArray);
   }, [400])
   useEffect(() => {
     applyPermission(topAndLeftMenuData, selectedTechnology)
@@ -1011,31 +1050,14 @@ const CostingSummaryTable = (props) => {
         break;
     }
   }
-
+  console.log(pieChartDataArray, colorArray, pieChartLabel);
   const pieChartData = {
-    labels: ['RM', 'BOP', 'CC', 'ST', 'O&P', 'P&F', 'TC'],
+    labels: pieChartLabel,
     datasets: [
       {
         label: '',
         data: pieChartDataArray,
-        backgroundColor: [
-          graphColor1,
-          graphColor2,
-          graphColor3,
-          graphColor4,
-          graphColor5,
-          graphColor6,
-          graphColor4,
-        ],
-        borderColor: [
-          '#fff',
-          '#fff',
-          '#fff',
-          '#fff',
-          '#fff',
-          '#fff',
-          '#fff',
-        ],
+        backgroundColor: colorArray,
         borderWidth: 1,
       },
     ],
@@ -1048,15 +1070,8 @@ const CostingSummaryTable = (props) => {
         labels: {
           boxWidth: 15,
           borderWidth: 1,
-          borderColor: [
-            graphColor1,
-            graphColor2,
-            graphColor3,
-            graphColor4,
-            graphColor5,
-            graphColor6,
-            graphColor4,
-          ],
+          padding: 8,
+          color: '#000'
         }
       },
     },
