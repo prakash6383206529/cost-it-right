8a1432305389de0676f98862ed5c3704d2847f07
Ashok Makhija <ashok.makhija@systematixindia.com>
2023-01-31 17:47:29 +0530

    Spacing issues resolved

--- src/config/constants.js
+++ src/config/constants.js
@@ -2097,10 +2097,7 @@ export const statusOptionsMasters = [
   { label: "Draft", value: "1" },
   { label: "History", value: "5" },
   { label: "PendingForApproval", value: "2" },
-
   { label: "Rejected", value: "4" },
-
-
 ]
 
 
