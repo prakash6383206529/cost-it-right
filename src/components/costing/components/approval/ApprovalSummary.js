a5b5739453cc46c84b8a324e2d6dbb07562da6c3
shreya.mahashabde <shreya.mahashabde@systematixindia.com>
2023-02-09 19:07:25 +0530

    approval flow changes

--- src/components/costing/components/approval/ApprovalSummary.js
+++ src/components/costing/components/approval/ApprovalSummary.js
@@ -704,6 +704,7 @@ function ApprovalSummary(props) {
           reasonId={approvalDetails.ReasonId}
           IsPushDrawer={showPushDrawer}
           dataSend={[approvalDetails, partDetail]}
+          costingTypeId={costingTypeId}
         />
       )}
       {pushButton && (
