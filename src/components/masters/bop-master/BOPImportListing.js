850d2e13e6d7c99a89c8bda01f6a6719f5e1f28f
Banti Prajapati <banti.prajapati@softude.com>
2023-02-09 12:26:20 +0530

    CIR-I4180 | history drawer list column name change

--- src/components/masters/bop-master/BOPImportListing.js
+++ src/components/masters/bop-master/BOPImportListing.js
@@ -800,6 +800,7 @@ class BOPImportListing extends Component {
                         isSimulation={true}
                         //cellValue={cellValue}
                         rowData={this.state.selectedRowData}
+                        import={true}
                     />
                 }
 
