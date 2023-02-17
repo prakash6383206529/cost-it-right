51dbf5202c472dfffd53d3ee38b6199f0f9c82b1
Ashok Makhija <ashok.makhija@systematixindia.com>
2023-02-17 18:26:17 +0530

    CIR 4274 Forging calculator validations applied for total scrap recovery percentage

--- src/components/costing/components/WeightCalculatorDrawer/forging/HotForging.js
+++ src/components/costing/components/WeightCalculatorDrawer/forging/HotForging.js
@@ -403,6 +403,34 @@ function HotForging(props) {
       e.preventDefault();
     }
   };
+
+
+  const onForgingScrapRecoveryPercentChange = (e) => {
+
+    let machiningScrapRecoveryPercent = getValues('machiningScrapRecoveryPercent') ? getValues('machiningScrapRecoveryPercent') : 0
+    if (machiningScrapRecoveryPercent) {
+      if ((Number(e.target.value) + Number(machiningScrapRecoveryPercent)) > 100) {
+        Toaster.warning("Scrap recovery percentage cannot be greater than 100")
+        setTimeout(() => {
+          setValue('forgingScrapRecoveryPercent', '')
+        }, 100);
+      }
+    }
+  }
+
+  const onMachiningScrapRecoveryPercentChange = (e) => {
+
+    let forgingScrapRecoveryPercent = getValues('forgingScrapRecoveryPercent') ? getValues('forgingScrapRecoveryPercent') : 0
+    if (forgingScrapRecoveryPercent) {
+      if ((Number(e.target.value) + Number(forgingScrapRecoveryPercent)) > 100) {
+        Toaster.warning("Scrap recovery percentage cannot be greater than 100")
+        setTimeout(() => {
+          setValue('machiningScrapRecoveryPercent', '')
+        }, 100);
+      }
+    }
+  }
+
   const inputLengthTooltipMessage = <div>Input Length = (Forged Weight + Loss Weight / 0.7857 * Billet Diameter<sup>2</sup>) * Density / 1000000</div>
   const endBitLossTooltipMessage = <div>End Bit Loss = (0.7857 * Billet Diameter<sup>2</sup> * End Bit Length * (Density / 1000000) / No. of Part per Length)</div>
   return (
@@ -684,7 +712,7 @@ function HotForging(props) {
                   control={control}
                   register={register}
                   mandatory={false}
-                  handleChange={() => { }}
+                  handleChange={onForgingScrapRecoveryPercentChange}
                   rules={{
                     required: false,
                     validate: { postiveNumber, maxPercentValue }
@@ -709,7 +737,7 @@ function HotForging(props) {
                     validate: { postiveNumber, maxPercentValue }
                   }}
                   mandatory={false}
-                  handleChange={() => { }}
+                  handleChange={onMachiningScrapRecoveryPercentChange}
                   defaultValue={''}
                   className=""
                   customClassName={'withBorder'}
