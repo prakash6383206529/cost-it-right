8940ba6d7230529cccb0686329ec852d58e69642
Banti Prajapati <banti.prajapati@softude.com>
2023-02-23 14:23:41 +0530

    CIR-I4113 | process border issue fixed

--- src/components/costing/components/CostingHeadCosts/Part/ProcessCost.js
+++ src/components/costing/components/CostingHeadCosts/Part/ProcessCost.js
@@ -1187,7 +1187,7 @@ function ProcessCost(props) {
                       return (
                         <>
                           <tr key={index}>
-                            <td className={`text-overflow ${(item?.GroupName === '' || item?.GroupName === null) ? '' : 'process-name'}`}>
+                            <td className={`text-overflow ${(item?.GroupName === '' || item?.GroupName === null) ? '' : 'process-name no-border'}`}>
                               {
                                 (item?.GroupName === '' || item?.GroupName === null) ? '' :
                                   <div onClick={() => {
