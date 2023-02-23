17c4c57802c9322259045c9df482f7d87e7e5e0e
Banti Prajapati <banti.prajapati@softude.com>
2023-02-23 15:30:38 +0530

    CIR-I4116 | user lisitng grid fixed

--- src/components/user/UsersListing.js
+++ src/components/user/UsersListing.js
@@ -507,6 +507,7 @@ class UsersListing extends Component {
 	resetState = () => {
 		gridOptions.columnApi.resetColumnState();
 		gridOptions.api.setFilterModel(null);
+		window.screen.width >= 1920 && this.state.gridApi.sizeColumnsToFit()
 	}
 
 	onGridReady = (params) => {
