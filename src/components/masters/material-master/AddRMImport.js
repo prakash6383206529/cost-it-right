7208685ec1e73c72ed326d84db6b31ea8786a6ae
samrudhi chitnis <samrudhi.chitnis@systematixindia.com>
2023-03-06 15:39:28 +0530

    CIR-I3781|without selecting vendor machine is getting added

--- src/components/masters/material-master/AddRMImport.js
+++ src/components/masters/material-master/AddRMImport.js
@@ -120,7 +120,8 @@ class AddRMImport extends Component {
       levelDetails: {},
       noApprovalCycle: false,
       showForgingMachiningScrapCost: false,
-      showExtraCost: false
+      showExtraCost: false,
+      vendorFilterList: []
     }
   }
 
@@ -1240,9 +1241,9 @@ class AddRMImport extends Component {
       isOpenCategory, isOpenVendor, isOpenUOM, isEditFlag, isViewFlag, setDisable, costingTypeId, noApprovalCycle } = this.state;
 
     const filterList = async (inputValue) => {
-      const { vendorName } = this.state
+      const { vendorFilterList } = this.state
       const resultInput = inputValue.slice(0, searchCount)
-      if (inputValue?.length >= searchCount && vendorName !== resultInput) {
+      if (inputValue?.length >= searchCount && vendorFilterList !== resultInput) {
         this.setState({ inputLoader: true })
         let res
         if (costingTypeId === VBCTypeId && resultInput) {
@@ -1252,7 +1253,7 @@ class AddRMImport extends Component {
           res = await getVendorListByVendorType(costingTypeId, resultInput)
         }
         this.setState({ inputLoader: false })
-        this.setState({ vendorName: resultInput })
+        this.setState({ vendorFilterList: resultInput })
         let vendorDataAPI = res?.data?.SelectList
         if (inputValue) {
           return autoCompleteDropdown(inputValue, vendorDataAPI, false, [], true)
