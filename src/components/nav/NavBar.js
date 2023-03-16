f3aa093aaf363fc33f35b085c96608c244777b90
Banti Prajapati <banti.prajapati@softude.com>
2023-03-16 19:52:10 +0530

    CIR-I4344 | supplier contribution navbar hide for doughnut

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -829,7 +829,7 @@ class SideBar extends Component {
     const { isLoader, } = this.state;
     const isLoggedIn = isUserLoggedIn();
     return (
-      <nav>
+      <nav className={`${this.props.sidebarAndNavbarHide ? 'hide-navbar' : ''}`}>
         {isLoader && <Loader />}
         <div>
           <div className="flex-conatiner sign-social ">
@@ -927,8 +927,8 @@ class SideBar extends Component {
  */
 function mapStateToProps({ auth, comman }) {
   const { loading, userData, leftMenuData, menusData, moduleSelectList, menuData, topAndLeftMenuData } = auth
-  const { disabledClass } = comman;
-  return { loading, userData, leftMenuData, menusData, moduleSelectList, menuData, topAndLeftMenuData, disabledClass }
+  const { disabledClass, sidebarAndNavbarHide } = comman;
+  return { loading, userData, leftMenuData, menusData, moduleSelectList, menuData, topAndLeftMenuData, disabledClass, sidebarAndNavbarHide }
 }
 
 /**
