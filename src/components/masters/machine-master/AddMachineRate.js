75959afa6cd1425acb731e1db42cad7f9adbe4fd
samrudhi chitnis <samrudhi.chitnis@softude.com>
2023-03-16 19:33:02 +0530

    CIR-I4400|Delete process disabled functionality issue fixed

--- src/components/masters/machine-master/AddMachineRate.js
+++ src/components/masters/machine-master/AddMachineRate.js
@@ -847,7 +847,7 @@ class AddMachineRate extends Component {
   * @description DELETE ROW ENTRY FROM TABLE 
   */
   deleteItem = (index) => {
-    const { processGrid, UOM } = this.state;
+    const { processGrid, UOM, isProcessGroup } = this.state;
 
     let tempData = processGrid.filter((item, i) => {
       if (i === index) {
@@ -855,10 +855,13 @@ class AddMachineRate extends Component {
       }
       return true;
     });
-
+    if (isProcessGroup) {
+      this.setState({ lockUOMAndRate: tempData.length === 0 ? false : true })
+    } else {
+      this.setState({ lockUOMAndRate: isProcessGroup })
+    }
     this.setState({
       processGrid: tempData,
-      lockUOMAndRate: tempData.length === 0 ? false : true,
       UOM: tempData.length === 0 ? [] : !this.state.lockUOMAndRate ? [] : UOM,
       isEditIndex: false,
       processName: [],
