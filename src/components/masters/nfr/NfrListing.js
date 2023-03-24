91e09e0c4f4ff02dede81bdd90b22434095c7b80
Ashok Makhija <ashok.makhija@softude.com>
2023-03-24 15:40:54 +0530

    CIR T 1573 NFR Listiing api integration

--- src/components/masters/nfr/NfrListing.js
+++ src/components/masters/nfr/NfrListing.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { useState, useEffect, } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, } from 'reactstrap';
-import { APPROVED, CANCELLED, EMPTY_DATA, FILE_URL, RECEIVED, RFQ, SUBMITTED, UNDER_APPROVAL, UNDER_REVISION, } from '../../../config/constants'
+import { EMPTY_DATA, FILE_URL, RFQ, } from '../../../config/constants'
 import NoContentFound from '../../common/NoContentFound';
 import { MESSAGES } from '../../../config/message';
-import Toaster from '../../common/Toaster';
 import 'react-input-range/lib/css/index.css'
 import LoaderCustom from '../../common/LoaderCustom';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
@@ -17,6 +16,8 @@ import { checkPermission, searchNocontentFilter, userDetails } from '../../../he
 import DayTime from '../../common/DayTimeWrapper';
 import Attachament from '../../costing/components/Drawers/Attachament';
 import NfrPartsListing from './NfrPartsListing';
+import { getAllNfrList } from './actions/nfr';
+import { hyphenFormatter } from '../masterUtil';
 const gridOptions = {};
 
 
@@ -38,31 +39,22 @@ function NfrListing(props) {
     const [confirmPopup, setConfirmPopup] = useState(false);
     const [attachment, setAttachment] = useState(false);
     const [viewAttachment, setViewAttachment] = useState([])
-    const [deleteId, setDeleteId] = useState('');
+    const [nfrId, setNfrId] = useState('');
+    const [selectedPartData, setSelectedPartData] = useState([]);
     const { topAndLeftMenuData } = useSelector(state => state.auth);
 
     useEffect(() => {
         setloader(true)
         getDataList()
         applyPermission(topAndLeftMenuData)
-
-        let obj={
-        nfrId:5,
-             productCode:12345,
-           version:'A',
-         noOfParts:5,
-        simulatedOn:'25/05/2022',
-        approvedOn:'27/05/2022',
-       status:'Draft'
-        }
-
-        let temp=[]
-        for(let i=0;i<10;i++){
-            temp.push(obj)
-        }
-        setRowData(temp)
-
     }, [topAndLeftMenuData])
+
+
+    useEffect(() => {
+        getDataList()
+
+    }, [])
+
     /**
       * @method applyPermission
       * @description ACCORDING TO PERMISSION HIDE AND SHOW, ACTION'S
@@ -85,35 +77,13 @@ function NfrListing(props) {
     * @description HIDE DOMESTIC, IMPORT FORMS
     */
     const getDataList = () => {
-        // dispatch(getQuotationList(userDetails()?.DepartmentCode, (res) => {
-        //     let temp = []
-        //     res?.data?.DataList && res?.data?.DataList.map((item) => {
-        //         if (item.IsActive === false) {
-        //             item.Status = "Cancelled"
-        //         }
-        //         item.tooltipText = ''
-        //         switch (item.Status) {
-        //             case APPROVED:
-        //                 item.tooltipText = 'Total no. of parts for which costing has been approved from that quotation / Total no. of parts exist in that quotation'
-        //                 break;
-        //             case RECEIVED:
-        //                 item.tooltipText = 'Total no. of costing received / Total no. of expected costing in that quotation'
-        //                 break;
-        //             case UNDER_REVISION:
-        //                 item.tooltipText = 'Total no. of costing under revision / Total no. of expected costing in that quotation'
-        //                 break;
-        //             default:
-        //                 break;
-        //         }
-        //         temp.push(item)
-        //         return null
-        //     })
-        //     setRowData(temp)
-        //     setloader(false)
-        // }))
-        setTimeout(() => {
+        dispatch(getAllNfrList((res) => {
+            if (res?.data?.DataList?.length > 0) {
+                setRowData(res?.data?.DataList)
+            }
             setloader(false)
-        }, 1000);
+        }))
+        setloader(false)
     }
 
     const resetState = () => {
@@ -137,26 +107,17 @@ function NfrListing(props) {
             rowData: rowData,
             Id: Id
         }
+        console.log(rowData, "rowData")
+        setSelectedPartData(rowData)
+        setNfrId(rowData?.NfrNumber)
         setIsEdit(true)
         setAddRfqData(data)
         setAddRfq(true)
     }
 
-    const cancelItem = (id) => {
-        setConfirmPopup(true)
-        setDeleteId(id)
-    }
 
     const onPopupConfirm = () => {
-        // dispatch(cancelRfqQuotation(deleteId, (res) => {
-        //     if (res.status === 200) {
-        //         Toaster.success('Quotation has been cancelled successfully.')
-        //         setTimeout(() => {
-        //             getDataList()
-        //         }, 500);
-        //     }
-        //     setConfirmPopup(false)
-        // }))
+
     }
 
     const closePopUp = () => {
@@ -174,22 +135,12 @@ function NfrListing(props) {
 
         return (
             <>
-                { <button title='View' className="View mr-1" type={'button'} onClick={() => viewOrEditItemDetails(cellValue, rowData, true)} />}
-               
+                {<button title='View' className="View mr-1" type={'button'} onClick={() => viewOrEditItemDetails(cellValue, rowData, true)} />}
+
             </>
         )
     };
 
-
-    const formToggle = () => {
-        setAddRfq(true)
-
-        let data = {
-            isAddFlag: true,
-        }
-
-        setAddRfqData(data)
-    }
 
     const closeDrawer = () => {
         setAddRfqData({})
@@ -198,13 +149,6 @@ function NfrListing(props) {
         setIsEdit(false)
 
     }
-
-    const closeDrawerViewRfq = () => {
-        setViewRfq(false)
-        getDataList()
-
-    }
-
 
     const onGridReady = (params) => {
         setgridApi(params.api);
@@ -250,8 +194,8 @@ function NfrListing(props) {
         const cell = props?.valueFormatted ? props.valueFormatted : props?.value;
         const row = props?.valueFormatted ? props.valueFormatted : props?.data;
         let tempStatus = '-'
-            tempStatus = row?.status
-        
+        tempStatus = row?.Status
+
         return <div className={cell}>{tempStatus}</div>
     }
 
@@ -301,18 +245,11 @@ function NfrListing(props) {
 
     }
 
-
     const viewDetails = (UserId) => {
-
         setViewRfqData(UserId)
         setViewRfq(true)
-        // this.setState({
-        //     UserId: UserId,
-        //     isOpen: true,
-        // })
 
     }
-
 
     const defaultColDef = {
         resizable: true,
@@ -347,7 +284,6 @@ function NfrListing(props) {
                                         // SHOW FILTER BUTTON ONLY FOR RM MASTER NOT FOR SIMULATION AMD MASTER APPROVAL SUMMARY
                                         (!props.isMasterSummaryDrawer) &&
                                         <>
-                                       
 
                                             <button type="button" className="user-btn" title="Reset Grid" onClick={() => resetState()}>
                                                 <div className="refresh mr-0"></div>
@@ -384,14 +320,14 @@ function NfrListing(props) {
                                                 enableBrowserTooltips={true}
                                             >
                                                 {/* <AgGridColumn cellClass="has-checkbox" field="QuotationNumber" headerName='RFQ No.' cellRenderer={'linkableFormatter'} ></AgGridColumn> */}
-                                                <AgGridColumn field="nfrId"  headerName="NFR ID" width={150}></AgGridColumn>
-                                                <AgGridColumn field="productCode" headerName='Product Code' maxWidth={150}></AgGridColumn>
-                                                <AgGridColumn field="version" headerName='Version/Revision'></AgGridColumn>
-                                                <AgGridColumn field="noOfParts" headerName='No. of Parts'></AgGridColumn>
-                                                <AgGridColumn field="simulatedOn" headerName='Simulated On'></AgGridColumn>
-                                                <AgGridColumn field="approvedOn" headerName='Approved On'></AgGridColumn>
-                                                <AgGridColumn field="status" headerName="Status" cellClass="text-center" minWidth={170} cellRenderer="statusFormatter"></AgGridColumn>
-                                                {<AgGridColumn field="QuotationId" width={180} headerName="Action" type="rightAligned" floatingFilter={false} cellRenderer={'totalValueRenderer'}></AgGridColumn>}
+                                                <AgGridColumn field="NfrNumber" headerName="NFR ID" width={150} cellRenderer={hyphenFormatter}></AgGridColumn>
+                                                <AgGridColumn field="ProductCode" headerName='Product Code' maxWidth={150} cellRenderer={hyphenFormatter}></AgGridColumn>
+                                                <AgGridColumn field="NfrVersion" headerName='Version/Revision' cellRenderer={hyphenFormatter}></AgGridColumn>
+                                                <AgGridColumn field="NumberOfParts" headerName='No. of Parts' cellRenderer={hyphenFormatter}></AgGridColumn>
+                                                <AgGridColumn field="SimulatedOn" headerName='Simulated On' cellRenderer={dateFormater}></AgGridColumn>
+                                                <AgGridColumn field="ApprovedOn" headerName='Approved On' cellRenderer={dateFormater}></AgGridColumn>
+                                                <AgGridColumn field="Status" headerName="Status" cellClass="text-center" minWidth={170} cellRenderer="statusFormatter"></AgGridColumn>
+                                                {<AgGridColumn field="Status" width={180} headerName="Action" type="rightAligned" floatingFilter={false} cellRenderer={'totalValueRenderer'}></AgGridColumn>}
                                             </AgGridReact>
                                             <PaginationWrapper gridApi={gridApi} setPage={onPageSizeChanged} globalTake={10} />
                                         </div>
@@ -402,7 +338,6 @@ function NfrListing(props) {
                         </>))
                     }
 
-                 
 
                     {
                         confirmPopup && <PopupMsgWrapper isOpen={confirmPopup} closePopUp={closePopUp} confirmPopup={onPopupConfirm} message={`${MESSAGES.RFQ_DETAIL_CANCEL_ALERT}`} />
@@ -414,7 +349,7 @@ function NfrListing(props) {
             {addRfq &&
 
                 <NfrPartsListing
-                    data={addRfqData}
+                    data={selectedPartData}
                     //hideForm={hideForm}
                     AddAccessibilityRMANDGRADE={true}
                     EditAccessibilityRMANDGRADE={true}
@@ -422,6 +357,7 @@ function NfrListing(props) {
                     isOpen={addRfq}
                     anchor={"right"}
                     isEditFlag={isEdit}
+                    nfrId={nfrId}
                     closeDrawer={closeDrawer}
                 />
 
