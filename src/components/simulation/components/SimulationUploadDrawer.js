20536d1f3a1b82f63e507a4497f58dfd889c732c
samrudhi chitnis <samrudhi.chitnis@softude.com>
2023-03-24 19:35:38 +0530

    Console removed

--- src/components/simulation/components/SimulationUploadDrawer.js
+++ src/components/simulation/components/SimulationUploadDrawer.js
@@ -190,8 +190,6 @@ class SimulationUploadDrawer extends Component {
                                         correctRowCount = correctRowCount + 1
                                         let obj = {}
                                         val.map((el, i) => {
-                                            console.log('el: ', el);
-                                            console.log('fileHeads[i]: ', fileHeads[i]);
                                             if (fileHeads[i] === 'EffectiveDate' && typeof el === 'number') {
                                                 el = getJsDateFromExcel(el)
                                             }
@@ -211,7 +209,6 @@ class SimulationUploadDrawer extends Component {
                                             return null;
                                         })
                                         fileData.push(obj)
-                                        console.log('fileData: ', fileData);
                                         obj = {}
 
                                     }
