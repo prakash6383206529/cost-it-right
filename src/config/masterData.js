d4ab1a5927f600406946ed621deb205b347ef137
shreya mahashabde <shreya.mahashabde@softude.com>
2023-04-06 19:44:38 +0530

    CIR-I4508 | RFQ | Add RFQ | In the notes field validation missing

--- src/config/masterData.js
+++ src/config/masterData.js
@@ -3372,7 +3372,7 @@ export const LOGISTICS = 24
 
 export const STRINGMAXLENGTH = 50
 export const NUMBERMAXLENGTH = 6
-export const REMARKMAXLENGTH = 255
+export const REMARKMAXLENGTH = 512
 
 export const SIMULATION_LEFT_MENU_NOT_INCLUDED = ["Simulation Upload", "RM Import", "RM Domestic", "BOP Domestic", "BOP Import", "Process-Simulation", "Process", "Operation-Simulation", "Surface Treatment", "Overhead-Simulation", "Overhead", "Profits", "Profits-Simulation", "Freight-Simulation", "Combined Process", "Operations", "Exchange Rates", "Machine Rate"]
 
