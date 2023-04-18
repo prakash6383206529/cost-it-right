0f2b8a9d733298c56af96f9e7f265fa70b8fdd4d
Banti Prajapati <banti.prajapati@softude.com>
2023-04-18 15:17:25 +0530

    CIR-I4608 | pie chart issue fixed in costing summary table

--- src/components/costing/components/CostingSummaryTable.js
+++ src/components/costing/components/CostingSummaryTable.js
@@ -221,7 +221,7 @@ const CostingSummaryTable = (props) => {
         case 'TC':
           dataArray.push(checkForDecimalAndNull(tempObj.totalToolCost, initialConfiguration.NoOfDecimalForPrice))
           break;
-        case 'OTHER DIS':
+        case 'HUNDI/DIS':
           dataArray.push(checkForDecimalAndNull(tempObj.otherDiscountCost, initialConfiguration.NoOfDecimalForPrice))
           break;
         case 'ANY OTHER COST':
@@ -2019,7 +2019,7 @@ const CostingSummaryTable = (props) => {
                           <th>Net PO Price ({getConfigurationKey().BaseCurrency}){simulationDrawer && '(Old)'}</th>
                           {viewCostingData &&
                             viewCostingData?.map((data, index) => {
-                              return <td>
+                              return <td className={props?.isRfqCosting && data.status === APPROVED ? 'finalize-cost' : ''}>
                                 {data?.CostingHeading === VARIANCE && (isApproval ? viewCostingData?.length > 0 && viewCostingData[0]?.nPOPrice > viewCostingData[1]?.nPOPrice ? <span className='positive-sign'>+</span> : '' : '')}
                                 <span title={data?.nPOPrice}><span className='currency-symbol'>{getCurrencySymbol(getConfigurationKey().BaseCurrency)}</span>{checkForDecimalAndNull(data?.nPOPrice, initialConfiguration.NoOfDecimalForPrice)}</span>
                                 {
