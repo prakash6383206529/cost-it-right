e7f74d2e9cc7d3b2bbc2b24d7350491b2023e1aa
Banti <banti.prajapti@softude.com>
2023-04-25 16:52:09 +0530

    CIR-T1604 | overhead include UI issue fixed

--- src/components/costing/components/costingHeaderTabs/TabOverheadProfit.js
+++ src/components/costing/components/costingHeaderTabs/TabOverheadProfit.js
@@ -600,7 +600,7 @@ function TabOverheadProfit(props) {
                   </Col>
                 } */}
 
-                <Col md="12" className="py-4 overhead-profit-tab d-flex">
+                <Col md="12" className="py-3 overhead-profit-tab">
                   <label
                     className={`custom-checkbox mb-0 w-fit-content`}
                     onChange={onPressIncludeSurfaceTreatment}
