6dd52a22fa4f5b95d92ce908d2f5131ecf7a1f95
Ashok Makhija <ashok.makhija@softude.com>
2023-05-08 15:10:38 +0530

    CIR-I4750 crm heads made non mandatory in costing + add exchange rate api issue fixed in discount & other tab

--- src/components/costing/components/CostingHeadCosts/OverheadProfit/PaymentTerms.js
+++ src/components/costing/components/CostingHeadCosts/OverheadProfit/PaymentTerms.js
@@ -459,13 +459,13 @@ function PaymentTerms(props) {
                             Controller={Controller}
                             control={control}
                             register={register}
-                            mandatory={true}
+                            mandatory={false}
                             rules={{
                                 required: false,
                             }}
                             placeholder={'Select'}
                             options={CRMHeads}
-                            required={true}
+                            required={false}
                             handleChange={handleCrmHeadChange}
                             disabled={CostingViewMode}
                         />
--- src/components/costing/components/CostingHeadCosts/SurfaceTreatMent/SurfaceTreatmentCost.js
+++ src/components/costing/components/CostingHeadCosts/SurfaceTreatMent/SurfaceTreatmentCost.js
@@ -346,14 +346,14 @@ function SurfaceTreatmentCost(props) {
                                 Controller={Controller}
                                 control={control}
                                 register={register}
-                                mandatory={true}
+                                mandatory={false}
                                 rules={{
-                                  required: true,
+                                  required: false,
                                 }}
                                 placeholder={'Select'}
                                 defaultValue={item.SurfaceTreatmentCRMHead ? { label: item.SurfaceTreatmentCRMHead, value: index } : ''}
                                 options={CRMHeads}
-                                required={true}
+                                required={false}
                                 handleChange={(e) => { onCRMHeadChange(e, index) }}
                                 disabled={CostingViewMode}
                               />
