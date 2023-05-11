c33cb92409af455b535e033e30d6f5e9c9945243
Ashok Makhija <ashok.makhija@softude.com>
2023-05-11 12:31:06 +0530

    CIR T1620 Got given summary Excel doownload

--- src/config/constants.js
+++ src/config/constants.js
@@ -1840,6 +1840,7 @@ export const SALES_PROVISION_REPORT = 'Sales Provision Report'
 export const PURCHASE_PROVISION_REPORT = 'Purchase Provision Report'
 export const CUSTOMER_POAM_SUMMARY_REPORT = 'Customer Poam Summary Report'
 export const MASTER_MOVEMENT_REPORT = 'Master Movement Report'
+export const GOT_GIVEN_REPORT = "Got Given Report"
 
 
 //export const SIMULATION_HISTORY = 'Simulation History'
