12069d85e40cf47118fbef464268888dfcc879dc
shreya mahashabde <shreya.mahashabde@softude.com>
2023-05-17 16:38:53 +0530

    CIR-T1635 | BOP Simulation Without Costing

--- src/components/rfq/AddRfq.js
+++ src/components/rfq/AddRfq.js
@@ -62,7 +62,6 @@ function AddRfq(props) {
     const [gridApi, setGridApi] = useState(null);
     const [gridColumnApi, setGridColumnApi] = useState(null);
     const [partList, setPartList] = useState([])
-    console.log('partList: ', partList);
     const [vendorList, setVendorList] = useState([])
     const [updateButtonPartNoTable, setUpdateButtonPartNoTable] = useState(false)
     const [updateButtonVendorTable, setUpdateButtonVendorTable] = useState(false)
@@ -939,22 +938,24 @@ function AddRfq(props) {
         if (label === 'rmname') {
             if (rawMaterialNameSelectList?.length > 0) {
                 let opts = [...rawMaterialNameSelectList]
-                opts1 = [...rawMaterialNameSelectList]
-                opts1 = opts && opts?.map(item => {
+                opts && opts?.map(item => {
+                    if (item.Value === '0') return false
                     item.label = item.Text
                     item.value = item.Value
-                    return item
+                    opts1.push(item)
+                    return null
                 })
             }
         }
         if (label === 'rmgrade') {
             if (gradeSelectList?.length > 0) {
                 let opts = [...gradeSelectList]
-                opts1 = [...gradeSelectList]
-                opts1 = opts && opts?.map(item => {
+                opts && opts?.map(item => {
+                    if (item.Value === '0') return false
                     item.label = item.Text
                     item.value = item.Value
-                    return item
+                    opts1.push(item)
+                    return null
                 })
             }
         }
@@ -962,11 +963,12 @@ function AddRfq(props) {
         if (label === 'rmspecification') {
             if (rmSpecification?.length > 0) {
                 let opts = [...rmSpecification]
-                opts1 = [...rmSpecification]
-                opts1 = opts && opts?.map(item => {
+                opts && opts?.map(item => {
+                    if (item.Value === '0') return false
                     item.label = item.Text
                     item.value = item.Value
-                    return item
+                    opts1.push(item)
+                    return null
                 })
             }
         }
