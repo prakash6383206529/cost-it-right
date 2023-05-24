10fd1f4e6cbde717b99692138f6972bbce8a0a19
Banti <banti.prajapti@softude.com>
2023-05-24 12:43:21 +0530

    pipd report

--- src/components/report/components/PIPDReport/PipdReportListing.js
+++ src/components/report/components/PIPDReport/PipdReportListing.js
@@ -1,5 +1,5 @@
 import { AgGridColumn, AgGridReact } from "ag-grid-react"
-import React from "react"
+import React, { useEffect } from "react"
 import { EMPTY_DATA } from "../../../../config/constants"
 import { Col, Row } from "reactstrap"
 import DayTime from "../../../common/DayTimeWrapper"
@@ -20,6 +20,8 @@ function PipdReportListing(props) {
 
     const startDate = costReportFormData && costReportFormData.fromDate
     const endDate = costReportFormData && costReportFormData.toDate
+    const isPlant = costReportFormData && costReportFormData.isPlant
+    const isCompany = costReportFormData && costReportFormData.isCompany
 
     const defaultColDef = {
         resizable: true,
@@ -28,7 +30,192 @@ function PipdReportListing(props) {
 
     };
 
-    const PIPDData = [
+
+    const PIPDDataCompany = [
+
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "200",
+            "1/4/2022": "200",
+            "1/5/2023": "100",
+            "1/6/2023": "400",
+            "1/7/2023": "100",
+            "1/9/2023": "200",
+            "1/11/2023": "300",
+            "Total": "1500"
+        },
+        {
+            "Name": "4W Light",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "400",
+            "1/4/2022": "0",
+            "1/5/2023": "50",
+            "1/6/2023": "300",
+            "1/7/2023": "100",
+            "1/9/2023": "0",
+            "1/11/2023": "200",
+            "Total": "1050"
+        },
+        {
+            "Name": "",
+            "PriceType": "Net",
+            "1/3/2022": "200",
+            "1/4/2022": "-200",
+            "1/5/2023": "-50",
+            "1/6/2023": "-100",
+            "1/7/2023": "0",
+            "1/9/2023": "-200",
+            "1/11/2023": "-100",
+            "Total": "-450"
+        },
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "500",
+            "1/4/2022": "300",
+            "1/5/2023": "200",
+            "1/6/2023": "400",
+            "1/7/2023": "100",
+            "1/9/2023": "200",
+            "1/11/2023": "300",
+            "Total": "2000"
+        },
+        {
+            "Name": "2W Light",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "0",
+            "1/4/2022": "200",
+            "1/5/2023": "0",
+            "1/6/2023": "300",
+            "1/7/2023": "100",
+            "1/9/2023": "",
+            "1/11/2023": "200",
+            "Total": "800"
+        },
+        {
+            "Name": "",
+            "PriceType": "Net",
+            "1/3/2022": "-500",
+            "1/4/2022": "-100",
+            "1/5/2023": "-200",
+            "1/6/2023": "-100",
+            "1/7/2023": "0",
+            "1/9/2023": "-200",
+            "1/11/2023": "-100",
+            "Total": "-1200"
+        },
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "600",
+            "1/4/2022": "400",
+            "1/5/2023": "300",
+            "1/6/2023": "500",
+            "1/7/2023": "200",
+            "1/9/2023": "300",
+            "1/11/2023": "400",
+            "Total": "2700"
+        },
+        {
+            "Name": "Acoustics",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "0",
+            "1/4/2022": "200",
+            "1/5/2023": "0",
+            "1/6/2023": "300",
+            "1/7/2023": "100",
+            "1/9/2023": "0",
+            "1/11/2023": "200",
+            "Total": "800"
+        },
+        {
+            "Name": "",
+            "PriceType": "Net",
+            "1/3/2022": "-600",
+            "1/4/2022": "-200",
+            "1/5/2023": "-300",
+            "1/6/2023": "-200",
+            "1/7/2023": "-100",
+            "1/9/2023": "-300",
+            "1/11/2023": "-200",
+            "Total": "-1900"
+        },
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "500",
+            "1/4/2022": "500",
+            "1/5/2023": "500",
+            "1/6/2023": "500",
+            "1/7/2023": "500",
+            "1/9/2023": "500",
+            "1/11/2023": "500",
+            "Total": "3500"
+        },
+        {
+            "Name": "MWTL",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "400",
+            "1/4/2022": "400",
+            "1/5/2023": "400",
+            "1/6/2023": "400",
+            "1/7/2023": "400",
+            "1/9/2023": "400",
+            "1/11/2023": "400",
+            "Total": "2800"
+        },
+        {
+            "Name": "",
+            "PriceType": "Net",
+            "1/3/2022": "-100",
+            "1/4/2022": "-100",
+            "1/5/2023": "-100",
+            "1/6/2023": "-100",
+            "1/7/2023": "-100",
+            "1/9/2023": "-100",
+            "1/11/2023": "-100",
+            "Total": "-700"
+        },
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "500",
+            "1/4/2022": "500",
+            "1/5/2023": "500",
+            "1/6/2023": "200",
+            "1/7/2023": "100",
+            "1/9/2023": "500",
+            "1/11/2023": "500",
+            "Total": "2800"
+        },
+        {
+            "Name": "2W Switch",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "400",
+            "1/4/2022": "200",
+            "1/5/2023": "100",
+            "1/6/2023": "500",
+            "1/7/2023": "700",
+            "1/9/2023": "100",
+            "1/11/2023": "20",
+            "Total": "2020"
+        },
+        {
+            "Name": "",
+            "PriceType": "Net",
+            "1/3/2022": "-100",
+            "1/4/2022": "-300",
+            "1/5/2023": "-400",
+            "1/6/2023": "300",
+            "1/7/2023": "600",
+            "1/9/2023": "-400",
+            "1/11/2023": "-480",
+            "Total": "-780"
+        }
+    ]
+
+    const PIPDDataTechnology = [
         {
             "Name": '',
             "PriceType": 'Price Increase',
@@ -41,7 +228,7 @@ function PipdReportListing(props) {
             "10/10/2021": '1200',
         },
         {
-            "Name": '4W lighting',
+            "Name": 'Sheet Metal',
             "PriceType": 'Price Decrease',
             "1/3/2021": '500',
             "1/4/2021": '800',
@@ -74,7 +261,7 @@ function PipdReportListing(props) {
             "10/10/2021": '1200',
         },
         {
-            "Name": 'MKL',
+            "Name": 'Plastic',
             "PriceType": 'Price Decrease',
             "1/3/2021": '500',
             "1/4/2021": '800',
@@ -107,7 +294,40 @@ function PipdReportListing(props) {
             "10/10/2021": '1200',
         },
         {
-            "Name": 'ONKYO',
+            "Name": 'Wiring Harness',
+            "PriceType": 'Price Decrease',
+            "1/3/2021": '500',
+            "1/4/2021": '800',
+            "1/5/2021": '1000',
+            "1/7/2021": '1200',
+            "19/7/2021": '1300',
+            "10/8/2021": '1300',
+            "10/10/2021": '1200',
+        },
+        {
+            "Name": '',
+            "PriceType": 'Net Price',
+            "1/3/2021": '-500',
+            "1/4/2021": '-200',
+            "1/5/2021": '-100',
+            "1/7/2021": '0',
+            "19/7/2021": '100',
+            "10/8/2021": '100',
+            "10/10/2021": '0',
+        },
+        {
+            "Name": '',
+            "PriceType": 'Price Increase',
+            "1/3/2021": '1000',
+            "1/4/2021": '1000',
+            "1/5/2021": '1100',
+            "1/7/2021": '1200',
+            "19/7/2021": '1200',
+            "10/8/2021": '1200',
+            "10/10/2021": '1200',
+        },
+        {
+            "Name": 'Electronic',
             "PriceType": 'Price Decrease',
             "1/3/2021": '500',
             "1/4/2021": '800',
@@ -129,6 +349,165 @@ function PipdReportListing(props) {
             "10/10/2021": '0',
         },
     ]
+    const PIPDDataPLant = [
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "100",
+            "1/4/2022": "75",
+            "1/5/2023": "100",
+            "1/6/2023": "100",
+            "1/7/2023": "50",
+            "1/9/2023": "0",
+            "1/11/2023": "100",
+            "Total": ''
+        },
+        {
+            "Name": "1031",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "300",
+            "1/4/2022": "0",
+            "1/5/2023": "50",
+            "1/6/2023": "200",
+            "1/7/2023": "0",
+            "1/9/2023": "0",
+            "1/11/2023": "100",
+            "Total": ''
+        },
+        {
+            "Name": "",
+            "PriceType": "Net",
+            "1/3/2022": "200",
+            "1/4/2022": "-75",
+            "1/5/2023": "-50",
+            "1/6/2023": "100",
+            "1/7/2023": "-50",
+            "1/9/2023": "0",
+            "1/11/2023": "0",
+            "Total": ''
+        },
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "25",
+            "1/4/2022": "50",
+            "1/5/2023": "0",
+            "1/6/2023": "200",
+            "1/7/2023": "25",
+            "1/9/2023": "50",
+            "1/11/2023": "100",
+            "Total": ''
+        },
+        {
+            "Name": "",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "50",
+            "1/4/2022": "0",
+            "1/5/2023": "0",
+            "1/6/2023": "100",
+            "1/7/2023": "0",
+            "1/9/2023": "0",
+            "1/11/2023": "40",
+            "Total": ''
+        },
+        {
+            "Name": "1032",
+            "PriceType": "Net",
+            "1/3/2022": "25",
+            "1/4/2022": "-50",
+            "1/5/2023": "",
+            "1/6/2023": "-100",
+            "1/7/2023": "-25",
+            "1/9/2023": "-50",
+            "1/11/2023": "-60",
+            "Total": ''
+        },
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "25",
+            "1/4/2022": "25",
+            "1/5/2023": "0",
+            "1/6/2023": "50",
+            "1/7/2023": "25",
+            "1/9/2023": "100",
+            "1/11/2023": "50",
+            "Total": ''
+        },
+        {
+            "Name": "1035",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "25",
+            "1/4/2022": "0",
+            "1/5/2023": "0",
+            "1/6/2023": "0",
+            "1/7/2023": "100",
+            "1/9/2023": "0",
+            "1/11/2023": "10",
+            "Total": ''
+        },
+        {
+            "Name": "",
+            "PriceType": "Net",
+            "1/3/2022": "0",
+            "1/4/2022": "-25",
+            "1/5/2023": "0",
+            "1/6/2023": "-50",
+            "1/7/2023": "75",
+            "1/9/2023": "-100",
+            "1/11/2023": "-40",
+            "Total": ''
+        },
+        {
+            "Name": "",
+            "PriceType": "Price Increase",
+            "1/3/2022": "50",
+            "1/4/2022": "50",
+            "1/5/2023": "0",
+            "1/6/2023": "50",
+            "1/7/2023": "0",
+            "1/9/2023": "50",
+            "1/11/2023": "50",
+            "Total": ''
+        },
+        {
+            "Name": "1036",
+            "PriceType": "Price Decrease",
+            "1/3/2022": "25",
+            "1/4/2022": "0",
+            "1/5/2023": "0",
+            "1/6/2023": "0",
+            "1/7/2023": "0",
+            "1/9/2023": "0",
+            "1/11/2023": "50",
+            "Total": ''
+        },
+        {
+            "Name": "",
+            "PriceType": "Net",
+            "1/3/2022": "-25",
+            "1/4/2022": "-50",
+            "1/5/2023": "0",
+            "1/6/2023": "-50",
+            "1/7/2023": "0",
+            "1/9/2023": "-50",
+            "1/11/2023": "0",
+            "Total": ''
+        }
+    ]
+
+
+    useEffect(() => {
+        console.log(isPlant, "plant", isCompany, "com")
+        if (isPlant) {
+            setGridData(PIPDDataTechnology)
+        } else if (isCompany) {
+            setGridData(PIPDDataPLant)
+        }
+        else {
+            setGridData(PIPDDataCompany)
+        }
+    }, [isPlant, isCompany])
     const onGridReady = (params) => {
         params.api.paginationGoToPage(0);
         setGridApi(params.api)
@@ -160,6 +539,7 @@ function PipdReportListing(props) {
         effectiveDateRenderer: effectiveDateFormatter,
         BoldCellRenderer: BoldCellRenderer
     };
+    console.log(gridData, "gridData")
     return (
         <>
             <div className={"container-fluid"}>
@@ -195,9 +575,9 @@ function PipdReportListing(props) {
                                             domLayout='autoHeight'
                                             // suppressRowTransform={true}
                                             floatingFilter={true}
-                                            rowData={PIPDData}
+                                            rowData={gridData}
                                             pagination={true}
-                                            paginationPageSize={10}
+                                            paginationPageSize={12}
                                             onGridReady={onGridReady}
                                             gridOptions={gridOptions}
 
@@ -213,15 +593,15 @@ function PipdReportListing(props) {
                                             <AgGridColumn field="Name" width="150" headerName="Name" cellClass={"colorWhite"} floatingFilter={true}></AgGridColumn>
                                             <AgGridColumn field="PriceType" headerName="In Cr" floatingFilter={true}></AgGridColumn>
                                             <AgGridColumn headerName="Effective Date" headerClass="justify-content-center" marryChildren={true}>
-                                                <AgGridColumn width="150" field="1/3/2021" headerName="1/3/2021" />
-                                                <AgGridColumn width="150" field="1/4/2021" headerName="1/4/2021" />
-                                                <AgGridColumn width="150" field="1/5/2021" headerName="1/5/2021" />
-                                                <AgGridColumn width="150" field="1/7/2021" headerName="1/7/2021" />
-                                                <AgGridColumn width="150" field="19/7/2021" headerName="19/7/2021" />
-                                                <AgGridColumn width="150" field="10/8/2021" headerName="10/8/2021" />
-                                                <AgGridColumn width="150" field="10/10/2021" headerName="10/10/2021" />
-
+                                                <AgGridColumn width="150" field="1/3/2022" headerName="1/3/2022" />
+                                                <AgGridColumn width="150" field="1/4/2022" headerName="1/4/2022" />
+                                                <AgGridColumn width="150" field="1/5/2023" headerName="1/5/2023" />
+                                                <AgGridColumn width="150" field="1/6/2023" headerName="1/6/2023" />
+                                                <AgGridColumn width="150" field="1/7/2023" headerName="1/7/2023" />
+                                                <AgGridColumn width="150" field="1/9/2023" headerName="1/9/2023" />
+                                                <AgGridColumn width="150" field="1/11/2023" headerName="1/11/2023" />
                                             </AgGridColumn>
+                                            <AgGridColumn width="150" field="Total" headerName="Total" />
 
                                         </AgGridReact>
                                         <PaginationWrapper gridApi={gridApi} setPage={onPageSizeChanged} />
