df07d1fd5ee7d5b415f79fdefbc1ae7a2cdfdc86
Ashok Makhija <ashok.makhija@softude.com>
2023-05-24 10:59:19 +0530

    CIR I 4818 Rm calculators negetive calculation issue resolved

--- src/components/costing/components/WeightCalculatorDrawer/forging/HotForging.js
+++ src/components/costing/components/WeightCalculatorDrawer/forging/HotForging.js
@@ -149,8 +149,12 @@ function HotForging(props) {
     const BilletLength = checkForNull(getValues('BilletLength'))
     const InputLength = dataSend.InputLength
     const NoOfPartsPerLength = dataSend.NoOfPartsPerLength
-    const EndBitLength = BilletLength - (InputLength * NoOfPartsPerLength)
+    let EndBitLength = BilletLength - (InputLength * NoOfPartsPerLength)
     let obj = dataSend
+    if (EndBitLength < 0) {
+      Toaster.warning('End bit length cannot be negetive')
+      EndBitLength = 0
+    }
     obj.EndBitLength = EndBitLength
     setDataSend(obj)
     setValue('EndBitLength', checkForDecimalAndNull(EndBitLength, getConfigurationKey().NoOfDecimalForPrice))
@@ -686,7 +690,11 @@ function HotForging(props) {
                   handleChange={() => { }}
                   rules={{
                     required: false,
-                    validate: { postiveNumber, maxPercentValue }
+                    pattern: {
+                      value: /^\d{0,6}(\.\d{0,4})?$/i,
+                      message: 'Maximum length for integer is 6 and for decimal is 4',
+                    },
+                    validate: { maxPercentValue }
                   }}
                   defaultValue={''}
                   className=""
@@ -705,7 +713,11 @@ function HotForging(props) {
                   register={register}
                   rules={{
                     required: false,
-                    validate: { postiveNumber, maxPercentValue }
+                    pattern: {
+                      value: /^\d{0,6}(\.\d{0,4})?$/i,
+                      message: 'Maximum length for integer is 6 and for decimal is 4',
+                    },
+                    validate: { maxPercentValue }
                   }}
                   mandatory={false}
                   handleChange={() => { }}
