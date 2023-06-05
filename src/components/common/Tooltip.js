fa625fb58f08cc6d07a19f803def1a2801230a39
Banti <banti.prajapti@softude.com>
2023-06-05 15:42:24 +0530

    CIR-I4837 | add formula in tooltip

--- src/components/common/Tooltip.js
+++ src/components/common/Tooltip.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { Tooltip } from 'reactstrap';
 
 const TooltipCustom = (props) => {
-    const { tooltipText, customClass, tooltipClass, id, disabledIcon, placement } = props;
+    const { tooltipText, customClass, tooltipClass, id, disabledIcon, placement, width } = props;
+    console.log('width: ', width);
     const [tooltipOpen, setTooltipOpen] = useState(false);
     const toggle = () => setTooltipOpen(!tooltipOpen);
+
+    const tooltipStyle = {
+        minWidth: width ? width : 'auto'
+    };
 
     return (
         <>
             {!disabledIcon && <i className={`fa fa-info-circle tooltip_custom_right tooltip-icon mb-n3 ${customClass}`} id={id}></i>}
-            <Tooltip className={tooltipClass} placement={placement ? placement : "top"} isOpen={tooltipOpen} target={id} toggle={toggle}>{tooltipText}</Tooltip>
+            <Tooltip style={tooltipStyle} className={tooltipClass} placement={placement ? placement : "top"} isOpen={tooltipOpen} target={id} toggle={toggle}>{tooltipText}</Tooltip>
         </>
     );
 }
