92fdc171c9c61b172c61cfc6d7f549406b4d2eee
Banti <banti.prajapti@softude.com>
2023-06-05 15:42:24 +0530

    CIR-I4837 | add formula in tooltip

--- src/components/layout/FormInputs.js
+++ src/components/layout/FormInputs.js
@@ -365,7 +365,7 @@ export function renderTextInputField(field) {
           ""
         )}
       </label>
-      <div className={inputbox}>
+      <div className={inputbox} id={field.id}>
         <input
           maxLength={field.maxLength}
           {...others}
@@ -584,15 +584,16 @@ export function renderText(field) {
           ""
         )}{" "}
       </label>
-      <input
-        maxLength={field.maxLength}
-        {...input}
-        {...others}
-
-        className={InputClassName}
-        autoComplete={'off'}
-      />
-
+      <div id={field.id}>
+        <input
+          id={field.id}
+          maxLength={field.maxLength}
+          {...input}
+          {...others}
+          className={InputClassName}
+          autoComplete={'off'}
+        />
+      </div>
       <div className="text-help mb-2">{touched ? error : ""}</div>
     </div>
   );
