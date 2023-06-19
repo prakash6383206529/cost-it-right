7fe5fb9a143864452481c94cb58f25456cf5f90c
shreya mahashabde <shreya.mahashabde@softude.com>
2023-06-19 15:11:33 +0530

    CIR-I5009 | All type of files is getting accepted while attaching the attachment in whole application

--- src/components/masters/overhead-profit-master/AddProfit.js
+++ src/components/masters/overhead-profit-master/AddProfit.js
@@ -628,7 +628,7 @@ class AddProfit extends Component {
   */
   setDisableFalseFunction = () => {
     const loop = Number(this.dropzone.current.files.length) - Number(this.state.files.length)
-    if (Number(loop) === 1) {
+    if (Number(loop) === 1 || Number(this.dropzone.current.files.length) === Number(this.state.files.length)) {
       this.setState({ setDisable: false, attachmentLoader: false })
     }
   }
