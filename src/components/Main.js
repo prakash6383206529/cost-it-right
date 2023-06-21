940aeb69e53c7a0b16679ef0fefbb53b47e34e40
Banti <banti.prajapti@softude.com>
2023-06-21 18:19:04 +0530

    issu fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -59,10 +59,6 @@ import RfqListing from './rfq/RfqListing'
 import CostMovementReport from './report/components/CostMovementReport/CostMovementReport'
 import CostRatioReport from './report/components/CostRatioReport/CostRatioReport'
 import SupplierContributionReport from './report/components/SupplierContribution'
-<<<<<<< HEAD
-=======
-import BudgetListing from './masters/budget-master/BudgetListing'
->>>>>>> c3895df2f6 (CIR-T1596 | NFR approval flow UI create)
 import NfrTabs from './masters/nfr'
 import SaleProvisionReport from './report/components/SaleProvisionReport/SaleProvisionReport'
 import PurchaseProvisionReport from './report/components/PurchaseProvisionReport/PurchaseProvisionReport'
