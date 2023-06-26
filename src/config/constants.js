b4d5e205d47f944fa8a9fd4dc9a94ecdcc7f949a
mahak.rawat <mahak.rawat@softude.com>
2023-06-26 11:14:38 +0530

    VErsion updated and env  updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -2601,4 +2601,4 @@ export const RAW_MATERIAL_VENDOR_TYPE = Number(reactLocalStorage.getObject('vend
 export const VBC_VENDOR_TYPE = Number(reactLocalStorage.getObject('vendortype')[VENDOR_TYPE_VBC])
 
 //VERSION 
-export const VERSION = "V2.1.180";
+export const VERSION = "V2.1.185";
