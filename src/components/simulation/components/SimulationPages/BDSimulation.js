def358170a9194ae46f420af367d61b73bce9498
samrudhi chitnis <samrudhi.chitnis@softude.com>
2023-06-27 19:21:55 +0530

    CIR-I5079|CIR-I5135|Last revision and impacted data reset button issue resolved

--- src/components/simulation/components/SimulationPages/BDSimulation.js
+++ src/components/simulation/components/SimulationPages/BDSimulation.js
@@ -50,7 +50,7 @@ function BDSimulation(props) {
     const [noData, setNoData] = useState(false);
     const [showTooltip, setShowTooltip] = useState(false)
     const [basicRateviewTooltip, setBasicRateViewTooltip] = useState(false)
-
+    const [textFilterSearch, setTextFilterSearch] = useState('')
     const { register, control, setValue, formState: { errors }, } = useForm({
         mode: 'onChange',
         reValidateMode: 'onChange',
@@ -322,6 +322,7 @@ function BDSimulation(props) {
 
     const onFilterTextBoxChanged = (e) => {
         gridApi.setQuickFilter(e.target.value);
+        setTextFilterSearch(e?.target?.value)
     }
 
     const cellChange = (props) => {
@@ -329,6 +330,7 @@ function BDSimulation(props) {
     }
     const resetState = () => {
         gridApi?.setQuickFilter('');
+        setTextFilterSearch('')
         gridOptions?.columnApi?.resetColumnState();
         gridOptions?.api?.setFilterModel(null);
         if (!isImpactedMaster) {
@@ -396,7 +398,7 @@ function BDSimulation(props) {
                                 <div className="ag-grid-wrapper height-width-wrapper">
                                     <div className="ag-grid-header d-flex align-items-center justify-content-between">
                                         <div className='d-flex align-items-center'>
-                                            <input type="text" className="form-control table-search" id="filter-text-box" placeholder="Search " autoComplete={'off'} onChange={(e) => onFilterTextBoxChanged(e)} />
+                                            <input type="text" className="form-control table-search" id="filter-text-box" value={textFilterSearch} placeholder="Search " autoComplete={'off'} onChange={(e) => onFilterTextBoxChanged(e)} />
                                             <button type="button" className="user-btn float-right mr-2" title="Reset Grid" onClick={() => resetState()}>
                                                 <div className="refresh mr-0"></div>
                                             </button>
