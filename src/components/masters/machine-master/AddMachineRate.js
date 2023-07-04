c240bd79e915faa2a3b0a4883731630c0764de8f
Ashok Makhija <ashok.makhija@softude.com>
2023-07-04 18:45:26 +0530

    CIR-I-5127 Add machine master issue resolved

--- src/components/masters/machine-master/AddMachineRate.js
+++ src/components/masters/machine-master/AddMachineRate.js
@@ -777,7 +777,7 @@ class AddMachineRate extends Component {
         processName: [],
         UOM: isProcessGroup ? UOM : [],
         lockUOMAndRate: isProcessGroup
-      }, () => this.props.change('MachineRate', isProcessGroup ? MachineRate : 0));
+      }, () => this.props.change('MachineRate', isProcessGroup ? MachineRate : ''));
       this.setState({ DropdownChange: false, errorObj: { processName: false, processUOM: false, machineRate: false } })
     }, 200);
   }
@@ -836,7 +836,7 @@ class AddMachineRate extends Component {
       processGridEditIndex: '',
       isEditIndex: false,
 
-    }, () => this.props.change('MachineRate', isProcessGroup ? MachineRate : 0));
+    }, () => this.props.change('MachineRate', isProcessGroup ? MachineRate : ''));
   };
 
   /**
@@ -896,7 +896,7 @@ class AddMachineRate extends Component {
       UOM: tempData.length === 0 ? [] : !this.state.lockUOMAndRate ? [] : UOM,
       isEditIndex: false,
       processName: [],
-    }, () => this.props.change('MachineRate', tempData.length === 0 ? 0 : this.props.fieldsObj.MachineRate))
+    }, () => this.props.change('MachineRate', tempData.length === 0 ? '' : this.props.fieldsObj.MachineRate))
     this.setState({ DropdownChange: false })
   }
 
