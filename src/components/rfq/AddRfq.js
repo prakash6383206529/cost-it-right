ef5b2c90df926026e75be4372fa93c8895ad9f5a
Banti <banti.prajapti@softude.com>
2023-07-05 19:14:39 +0530

    CIR-I5000 | tooltip hover issue fixed

--- src/components/rfq/AddRfq.js
+++ src/components/rfq/AddRfq.js
@@ -787,8 +787,12 @@ function AddRfq(props) {
         }
         setPartName('')
         setState(false)
+        setShowTooltip(false)
         setTimeout(() => {
             setState(true)
+            setTimeout(() => {
+                setShowTooltip(true)
+            }, 250);
         }, 500);
         reactLocalStorage.setObject('PartData', [])
 
