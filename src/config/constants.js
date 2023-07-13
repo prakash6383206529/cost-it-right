95cdde9021f9b56f4b1bd86c40a771d9c5537c61
shreya mahashabde <shreya.mahashabde@softude.com>
2023-07-13 19:00:59 +0530

    version changed and comment removed

--- src/config/constants.js
+++ src/config/constants.js
@@ -2638,4 +2638,4 @@ export const RAW_MATERIAL_VENDOR_TYPE = Number(reactLocalStorage.getObject('vend
 export const VBC_VENDOR_TYPE = Number(reactLocalStorage.getObject('vendortype')[VENDOR_TYPE_VBC])
 
 //VERSION 
-export const VERSION = "V2.1.206";
+export const VERSION = "V2.1.207";
