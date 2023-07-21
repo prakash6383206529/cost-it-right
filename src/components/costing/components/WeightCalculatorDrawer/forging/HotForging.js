c758a88cced0c75be48db004eaa1ecfeaed8ebd8
Banti <banti.prajapti@softude.com>
2023-07-21 14:33:34 +0530

    CIR-I5208 | spelling correction

--- src/components/costing/components/WeightCalculatorDrawer/forging/HotForging.js
+++ src/components/costing/components/WeightCalculatorDrawer/forging/HotForging.js
@@ -154,7 +154,7 @@ function HotForging(props) {
     let EndBitLength = BilletLength - (InputLength * NoOfPartsPerLength)
     let obj = dataSend
     if (EndBitLength < 0) {
-      Toaster.warning('End bit length cannot be negetive')
+      Toaster.warning('End bit length cannot be Negative')
       EndBitLength = 0
     }
     obj.EndBitLength = EndBitLength
