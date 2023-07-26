4fde48735023172b9fa8492dcf640ad3d5904c4e
samrudhi chitnis <samrudhi.chitnis@softude.com>
2023-07-26 18:22:00 +0530

    CIR-I5363|Attachment issue fixed for RM master

--- src/components/masters/material-master/AddRMImport.js
+++ src/components/masters/material-master/AddRMImport.js
@@ -123,7 +123,8 @@ class AddRMImport extends Component {
       vendorFilterList: [],
       isCallCalculation: false,
       isDropDownChanged: false,
-      CostingTypePermission: false
+      CostingTypePermission: false,
+      isEditBuffer: false
     }
   }
 
@@ -213,7 +214,10 @@ class AddRMImport extends Component {
   componentDidUpdate(prevProps, prevState) {
     const { initialConfiguration } = this.props
     if (!this.state.isViewFlag && !this.state.isCallCalculation) {
-      if (this.props.fieldsObj !== prevProps.fieldsObj) {
+      if (this.props.fieldsObj !== prevProps.fieldsObj && !this.state.isEditFlag) {
+        this.handleNetCost()
+      }
+      if (this.props.fieldsObj !== prevProps.fieldsObj && this.state.isEditFlag && this.state.isEditBuffer) {
         this.handleNetCost()
       }
       if ((prevState?.costingTypeId !== this.state.costingTypeId) && initialConfiguration.IsMasterApprovalAppliedConfigure && CheckApprovalApplicableMaster(RM_MASTER_ID) === true) {
@@ -560,6 +564,9 @@ class AddRMImport extends Component {
                   this.commonFunction()
                 }
               }, 500)
+              setTimeout(() => {
+                this.setState({ isEditBuffer: true })
+              }, 500);
             })
             // ********** ADD ATTACHMENTS FROM API INTO THE DROPZONE'S PERSONAL DATA STORE **********
             let files = Data.FileList && Data.FileList.map((item) => {
@@ -736,7 +743,7 @@ class AddRMImport extends Component {
   }
 
   closeApprovalDrawer = (e = '', type) => {
-    this.setState({ approveDrawer: false, setDisable: false })
+    this.setState({ approveDrawer: false, setDisable: false, isEditBuffer: true })
     if (type === 'submit') {
       this.clearForm('submit')
       this.cancel('submit')
@@ -1043,7 +1050,7 @@ class AddRMImport extends Component {
       return false
     }
 
-    this.setState({ isVendorNameNotSelected: false })
+    this.setState({ isVendorNameNotSelected: false, isEditBuffer: false })
 
     let plantArray = []
     if (costingTypeId === VBCTypeId) {
