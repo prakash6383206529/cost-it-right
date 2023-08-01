f99900d910f509b089c349ae3cedc52e599071d8
shreya mahashabde <shreya.mahashabde@softude.com>
2023-08-01 19:51:22 +0530

    constant file

--- src/config/constants.js
+++ src/config/constants.js
@@ -2647,6 +2647,7 @@ export const LABOUR_VENDOR_TYPE = Number(reactLocalStorage.getObject('vendortype
 export const RAW_MATERIAL_VENDOR_TYPE = Number(reactLocalStorage.getObject('vendortype')[VENDOR_TYPE_RAW_MATERIAL])
 export const VBC_VENDOR_TYPE = Number(reactLocalStorage.getObject('vendortype')[VENDOR_TYPE_VBC])
 
+
 //VERSION 
 
 export const VERSION = "V2.1.222";
