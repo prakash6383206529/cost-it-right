824bc2d147ebdd7f235ae6a1cff2c38c3ab72e63
shreya mahashabde <shreya.mahashabde@softude.com>
2023-08-16 16:05:07 +0530

    CIR-T1636 | Different boxes to be given for attaching Feasibility, Capacity, and Timeline files. 1 attachment per box in Costing in Discount and Other tab on RFQ Portal III

--- src/components/costing/components/CostingSummaryTable.js
+++ src/components/costing/components/CostingSummaryTable.js
@@ -2836,6 +2836,7 @@ const CostingSummaryTable = (props) => {
             index={viewAtttachments}
             closeDrawer={closeAttachmentDrawer}
             anchor={'right'}
+            isRfqCosting={props?.isRfqCosting}
           />
         )
       }
