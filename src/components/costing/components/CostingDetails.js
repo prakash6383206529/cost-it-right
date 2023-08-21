3eee0170df99a5b4b82fe9983623b14366555590
shreya mahashabde <shreya.mahashabde@softude.com>
2023-08-21 15:27:32 +0530

    CIR-I5506 | Costing select part type(BOP) | Costing summary page, Edit the costing | Getting object reference error

--- src/components/costing/components/CostingDetails.js
+++ src/components/costing/components/CostingDetails.js
@@ -380,7 +380,7 @@ function CostingDetails(props) {
           if (response.data.Result) {
             dispatch(getPartInfo(newValue.value, (res) => {
               let Data = res.data.Data
-
+              reactLocalStorage.setObject('costingArray', [])
               setValue('PartName', Data?.PartName ? Data.PartName : '')
               setValue('Description', Data?.Description ? Data.Description : '')
               setValue('ECNNumber', Data?.ECNNumber ? Data.ECNNumber : '')
@@ -518,7 +518,7 @@ function CostingDetails(props) {
       //   }
       // })))
 
-      reactLocalStorage.setObject('costingArray', [])
+      // reactLocalStorage.setObject('costingArray', [])
       reactLocalStorage.setObject('surfaceCostingArray', [])
       setIsOpenVendorSOBDetails(true)
 
