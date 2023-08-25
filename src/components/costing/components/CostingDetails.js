e396b868ad0328ef6f6d136b8c2281c1ec27c751
mahak.rawat <mahak.rawat@softude.com>
2023-08-25 17:49:45 +0530

    Costing route fixed

--- src/components/costing/components/CostingDetails.js
+++ src/components/costing/components/CostingDetails.js
@@ -395,6 +395,7 @@ function CostingDetails(props) {
             dispatch(getPartInfo(newValue.value, (res) => {
               let Data = res.data.Data
               reactLocalStorage.setObject('costingArray', [])
+              reactLocalStorage.setObject('surfaceCostingArray', [])
               setValue('PartName', Data?.PartName ? Data.PartName : '')
               setValue('Description', Data?.Description ? Data.Description : '')
               setValue('ECNNumber', Data?.ECNNumber ? Data.ECNNumber : '')
@@ -533,7 +534,7 @@ function CostingDetails(props) {
       // })))
 
       // reactLocalStorage.setObject('costingArray', [])
-      reactLocalStorage.setObject('surfaceCostingArray', [])
+      // reactLocalStorage.setObject('surfaceCostingArray', [])
       setIsOpenVendorSOBDetails(true)
 
     } else {
