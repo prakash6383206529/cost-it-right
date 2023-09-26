eacc3b0ff9e3860fecc1a840c3f4ea562cb80d9c
shreya mahashabde <shreya.mahashabde@softude.com>
2023-09-26 14:28:58 +0530

    CIR-I5615 | revert

--- src/components/costing/components/CostingHeadCosts/AdditionalOtherCost/AddConditionCosting.js
+++ src/components/costing/components/CostingHeadCosts/AdditionalOtherCost/AddConditionCosting.js
@@ -270,13 +270,6 @@ function AddConditionCosting(props) {
         }
     }
 
-    const basicPriceTitle = () => {
-        const { initialConfiguration } = this.props
-        if (initialConfiguration?.IsBasicRateAndCostingConditionVisible) {
-            return `Basic Price (${initialConfiguration?.BaseCurrency}) =  Basic Rate (${initialConfiguration?.BaseCurrency}) + Freight Cost (${initialConfiguration?.BaseCurrency}) + Shearing Cost (${initialConfiguration?.BaseCurrency})`
-        }
-    }
-
     return (
 
         <div>
@@ -302,7 +295,6 @@ function AddConditionCosting(props) {
                                 <Row>
 
                                     <Col md="3" className='pr-1'>
-                                        <TooltipCustom id="rm-basic-price" tooltipText={basicPriceTitle()} />
                                         <SearchableSelectHookForm
                                             label={`Condition`}
                                             name={'Condition'}
