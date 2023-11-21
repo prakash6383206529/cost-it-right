b9f67baab6024534a642c71caf4054778bbee710
samrudhi chitnis <samrudhi.chitnis@softude.com>
2023-11-21 17:33:46 +0530

    version updated to V2.1.265.7

--- src/config/constants.js
+++ src/config/constants.js
@@ -2729,4 +2729,4 @@ export const RAW_MATERIAL_VENDOR_TYPE = Number(reactLocalStorage.getObject('vend
 export const VBC_VENDOR_TYPE = Number(reactLocalStorage.getObject('vendortype')[VENDOR_TYPE_VBC])
 
 //VERSION 
-export const VERSION = "V2.1.265.6";
+export const VERSION = "V2.1.265.7";
