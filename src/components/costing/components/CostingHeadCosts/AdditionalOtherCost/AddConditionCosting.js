2fd83097db74fa8c4904b72b5c8e2ae069d762c0
samrudhi chitnis <samrudhi.chitnis@softude.com>
2024-01-03 12:58:54 +0530

    CIR-I6290|Condition costing issue fixed

--- src/components/costing/components/CostingHeadCosts/AdditionalOtherCost/AddConditionCosting.js
+++ src/components/costing/components/CostingHeadCosts/AdditionalOtherCost/AddConditionCosting.js
@@ -14,6 +14,7 @@ import { trim } from 'lodash'
 
 function AddConditionCosting(props) {
     const { currency, currencyValue, basicRateCurrency, basicRateBase, isFromImport, isFromMaster, EntryType } = props
+    console.log('EntryType: ', EntryType);
     const [tableData, setTableData] = useState(props?.tableData)
     // const [tableData, setTableData] = useState([])
     const [disableTotalCost, setDisableTotalCost] = useState(true)
@@ -70,7 +71,7 @@ function AddConditionCosting(props) {
             }
         }
 
-        if (isFromMaster || hasCostingConditionEntryTypeId || props?.costingConditionEntryType) {
+        if (isFromMaster || hasCostingConditionEntryTypeId || props?.costingConditionEntryType !== undefined) {
             const entryTypeId =
                 EntryType !== undefined
                     ? EntryType
