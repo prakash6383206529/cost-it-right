88053a48934c0e6a8d4adad23cef11d30d37f3a7
samrudhi chitnis <samrudhi.chitnis@softude.com>
2024-03-06 17:53:12 +0530

    Version V2.1.322

--- src/config/constants.js
+++ src/config/constants.js
@@ -2896,7 +2896,7 @@ export const VBC_VENDOR_TYPE = Number(reactLocalStorage.getObject('vendortype')[
 export const PartTypeIDFromAPI = 2
 
 //VERSION 
-export const VERSION = "V2.1.321";
+export const VERSION = "V2.1.322";
 
 
 
