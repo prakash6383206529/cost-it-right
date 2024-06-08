547b9c2a130e50b5eb5ff08c7c6b2637f6bfc546
mahak.rawat <mahak.rawat@softude.com>
2024-06-08 17:19:05 +0530

    Corrected the routing from Add RFQ

--- src/components/common/PopupMsgWrapper.js
+++ src/components/common/PopupMsgWrapper.js
@@ -11,8 +11,10 @@ function PopupMsgWrapper(props) {
   function confirmHandler(e) {
     props.confirmPopup(e)
     setTimeout(() => {
+      console.log('props.redirectPath: ', props.redirectPath);
       document.querySelector('body').removeAttribute('style')
-      if (props.redirectPath !== '') {
+      if (props.redirectPath !== '' && props?.redirectPath !== null && props?.redirectPath !== undefined) {
+        console.log("Props");
         history.push(SUPPLIER_MANAGEMENT, { vendorId: props.vendorId, plantId: props.plantId });
       }
     }, 200);
