b03a46000da62d4172d859527fa4561ea441c9af
Mansi Agrawal <mansi.agrawal@softude.com>
2024-09-04 20:10:17 +0530

    RFO/ Costing - Cost Comparative | Investment details | Hundi/ Discount section: Remove Hundi keword.

--- src/helper/core.js
+++ src/helper/core.js
@@ -2,16 +2,16 @@ import { useTranslation } from 'react-i18next';
 
 export const useLabels = () => {
     // const { t } = useTranslation('MasterLabels');
-    const labels = ['MasterLabels', 'Costing'];
+    const labels = ['MasterLabels', 'CostingLabels'];
     const { i18n } = useTranslation(labels);
    // Create separate translation functions for each namespace
    const tMasterLabels = i18n.getFixedT(null, 'MasterLabels');
-   const tCosting = i18n.getFixedT(null, 'Costing');
+   const tCosting = i18n.getFixedT(null, 'CostingLabels');
   
     return {
         technologyLabel: tMasterLabels('commonFields.technology', { defaultValue: 'Technology' }),
         partTypeLabel: tMasterLabels('commonFields.partType', { defaultValue: 'Part Type' }),
         RMCategoryLabel:tMasterLabels('RMCategoryLabel', { defaultValue: 'Category' }),
-        hundiDiscount : tCosting('labels.discount', { defaultValue: '123/Discount' }),
+        hundiDiscount : tCosting('discount', { defaultValue: 'Hundi/Discount' }),
     };
 };
