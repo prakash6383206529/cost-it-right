e2858a05ba5a298d8af44f04fb4b137470817b27
mahak.rawat <mahak.rawat@softude.com>
2024-10-01 10:53:48 +0530

    Version update

--- src/config/constants.js
+++ src/config/constants.js
@@ -3257,7 +3257,7 @@ export const RAWMATERIALCOST = 'Raw Material Other Cost'
 export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 
 //VERSION 
-export const VERSION = "V3.1.10";
+export const VERSION = "V3.1.11";
 
 
 
