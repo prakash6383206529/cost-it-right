997c4b54a08f83d11c1ac66fee6ccc987e375687
Mansi Agrawal <mansi.agrawal@softude.com>
2024-10-08 16:34:08 +0530

    CIR-I7348||(Base-163& 173) - Masters - Part | Manage assembly | Bulk upload | An error is coming - Please select the same masters file.

--- src/helper/core.js
+++ src/helper/core.js
@@ -5,6 +5,16 @@ import { getConfigurationKey } from './auth';
 import PrimaryLogo from '../assests/images/logo/company-logo.svg';
 import SecondaryLogo from '../assests/images/logo/CIRlogo.svg';
 
+// Company Logo 
+export const CompanyLogo = (props) => {
+  return <img src={showLogoFromDataBase ? getConfigurationKey().LogoURL : PrimaryLogo} alt={showLogoFromDataBase ? getConfigurationKey().ClientName ?? "LOGO" : 'Softude'} height={props.height ?? ''} />
+}
+// CIR Logo 
+export const CirLogo = (props) => {
+  return <img className="logo-second" src={SecondaryLogo} height={props.height ?? ''} alt="Cost It Right" />
+}
+
+//
 export const useLabels = () => {
     // const { t } = useTranslation('MasterLabels');
     const labels = ['MasterLabels', 'CostingLabels'];
@@ -35,12 +45,10 @@ export const useLabels = () => {
 export const useWithLocalization = (dataArray, ns) => {
     const { t } = useTranslation(ns);
     const labels = useLabels();
-
     return dataArray.map(item => ({
         ...item,
         label: t(item.label, { defaultValue: item.defaultValue })
             // .replace(/Technology/g, labels.technologyLabel) //       THIS CODE FOR THE TECHNOLOGY LABEL WILL USE FURTHER IN FUTURE .
-
             .replace(/Vendor/g, labels.vendorLabel)
             // .replace(/Category/g, labels.RMCategoryLabel)
         // Add more .replace() calls for other labels as needed
@@ -52,16 +60,28 @@ export const withLocalization = (dataArray, t, ns) => {
         label: t(item.label, { defaultValue: item.defaultValue, ns: ns })
     }));
 };
-export const CompanyLogo = (props) => {
-    return <img src={showLogoFromDataBase ? getConfigurationKey().LogoURL : PrimaryLogo} alt={showLogoFromDataBase ? getConfigurationKey().ClientName ?? "LOGO" : 'Softude'} height={props.height ?? ''} />
-}
-
-export const CirLogo = (props) => {
-    return <img className="logo-second" src={SecondaryLogo} height={props.height ?? ''} alt="Cost It Right" />
-}
 
 
-  export const localizeLabels = (headers, labelMap) => {
+  // export const localizeLabels = (headers, labelMap) => {
+  //   return headers.map(header => {
+  //     let newLabel = header.label;
+      
+  //     Object.entries(labelMap).forEach(([originalTerm, localizedTerm]) => {
+  //       const regex = new RegExp(originalTerm, 'g');
+  //       newLabel = newLabel.replace(regex, localizedTerm);
+  //     });
+      
+  //     return { ...header, label: newLabel };
+  //   });
+  // };
+  export const localizeHeadersWithLabels = (headers, t) => {
+    const labelMap = {
+      'Vendor': t('VendorLabel', { defaultValue: 'Vendor' }),
+      'Technology': t('TechnologyLabel', { defaultValue: 'Technology' }),
+      'Category': t('RMCategoryLabel', { defaultValue: 'Category' }),
+      // Add more labels as needed
+    };
+  
     return headers.map(header => {
       let newLabel = header.label;
       
