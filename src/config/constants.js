69eba9e3124597b6a01b2cbce4cf2bbc91f688cf
aditi.bhawasar <aditi.bhawasar@softude.com>
2024-10-24 13:09:07 +0530

    version updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -18,8 +18,7 @@ export const config = () => {
 }
 
 
-// const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
-const BASE_URL = `http://10.10.1.100:4001/api/v1`;
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 export const FILE_URL = `${process.env.REACT_APP_FILE_URL}`;
 
 /** Export API */
@@ -3273,7 +3272,7 @@ export const RAWMATERIALCOST = 'Raw Material Other Cost'
 export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 
 //VERSION 
-export const VERSION = "V3.1.18";
+export const VERSION = "V3.1.19";
 
 
 
