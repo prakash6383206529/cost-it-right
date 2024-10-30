74b23141edd8a57c77518647c63c0358dbe9fd5c
Mansi Agrawal <mansi.agrawal@softude.com>
2024-10-30 17:00:14 +0530

    CP7-I1||LABOUR MASTER | CBC<Machine Type<Labour Type

--- src/components/masters/labour-master/AddLabour.js
+++ src/components/masters/labour-master/AddLabour.js
@@ -210,7 +210,7 @@ class AddLabour extends Component {
           if (item.Value === '0') return false
           if (this.findLabourtype(item.Value, this.state.gridTable)) return false;
 
-          if (costingTypeId === CBCTypeId ) {
+          if (costingTypeId === CBCTypeId && this.props.initialConfiguration?.IsShowProductInLabour) {
             if (item.Text === 'Skilled') {
               temp.push({ label: item.Text, value: item.Value })
             }
