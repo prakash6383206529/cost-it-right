1c5aebcecb3780503aa885598b2bed251d2ff07c
shreya mahashabde <shreya.mahashabde@softude.com>
2024-11-06 13:42:01 +0530

    V3.1.34

--- src/config/constants.js
+++ src/config/constants.js
@@ -3278,7 +3278,7 @@ export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 export const IsSelectSinglePlant = true
 
 //VERSION 
-export const VERSION = "V3.1.33";
+export const VERSION = "V3.1.34";
 
 
 
