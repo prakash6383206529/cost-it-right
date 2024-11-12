732e74ab7d04a8a1e071f65c454218add6ad95ec
Aditi Bhawsar <aditi.bhawsar@softude.com>
2024-11-12 19:31:13 +0530

    update url

--- src/config/constants.js
+++ src/config/constants.js
@@ -18,8 +18,7 @@ export const config = () => {
 }
 
 
-// const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
-const BASE_URL = `http://172.16.0.18:81/api/v1`;
+const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 export const FILE_URL = `${process.env.REACT_APP_FILE_URL}`;
 
 /** Export API */
