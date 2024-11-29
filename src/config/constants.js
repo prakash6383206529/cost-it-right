ecff41270d8a9e97ae8d61e8539944e7e625d93a
umesh.rathore <umesh.rathore@softude.com>
2024-11-29 12:57:53 +0530

    VErsion changed to V3.1.23.1

--- src/config/constants.js
+++ src/config/constants.js
@@ -3271,7 +3271,7 @@ export const RAWMATERIALCOST = 'Raw Material Other Cost'
 export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 
 //VERSION 
-export const VERSION = "V3.1.23";
+export const VERSION = "V3.1.23.1";
 
 
 
