050a63987774387a043b521e36a5d0471d5624cf
Samrudhi Chitnis <samrudhi.chitnis@softude.com>
2024-11-29 12:59:50 +0530

    V3.1.43

--- src/config/constants.js
+++ src/config/constants.js
@@ -3280,7 +3280,7 @@ export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 export const IsSelectSinglePlant = true
 
 //VERSION 
-export const VERSION = "V3.1.42";
+export const VERSION = "V3.1.43";
 
 
 
