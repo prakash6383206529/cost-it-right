b98264451392e198cee08870887cdb65892d1b3a
Mansi Agrawal <mansi.agrawal@softude.com>
2024-12-02 16:03:43 +0530

    vendor and source vendor Rm issue fixed

--- src/components/masters/material-master/AddRMDetails.js
+++ src/components/masters/material-master/AddRMDetails.js
@@ -513,8 +513,9 @@ function AddRMDetails(props) {
     // };
 
 
-    const vendorFilterList = (inputValue) => DropDownFilterList(inputValue, VBC_VENDOR_TYPE, 'vendorFilter', getVendorNameByVendorSelectList, setState, state);
-    const sourceVendorFilterList = (inputValue) => DropDownFilterList(inputValue, VBC_VENDOR_TYPE, 'sourceVendorFilter', getVendorNameByVendorSelectList, setState, state);
+
+    const vendorFilterList = (inputValue) => DropDownFilterList(inputValue, RAW_MATERIAL_VENDOR_TYPE, 'vendorFilter', getVendorNameByVendorSelectList, setState, state);
+    const sourceVendorFilterList = (inputValue) => DropDownFilterList(inputValue, RAW_MATERIAL_VENDOR_TYPE, 'sourceVendorFilter', getVendorNameByVendorSelectList, setState, state);
     const sourceLocationFilterList = (inputValue) => DropDownFilterList(inputValue, '', 'sourceLocationFilter', (filterType, resultInput) => getCityByCountry(0, 0, resultInput), setState, state);
 
 
