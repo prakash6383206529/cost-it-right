2fa67d16ddda218e3a67d0be8479bbfda990b8ae
Bhola Vishwakarma <bhola.vishwakarma@softude.com>
2024-12-10 14:56:36 +0530

    CC6-I67

--- src/components/masters/operation/AddMoreOperation.js
+++ src/components/masters/operation/AddMoreOperation.js
@@ -1612,12 +1612,16 @@ function AddMoreOperation(props) {
 
 
                                             <Col md="3">
-                                                <TooltipCustom id={"wireRate"} disabledIcon={true} width={"350px"} tooltipText={`${getLabel('wireCost')} = ${getLabel('wireRate')} * Consumption`} />
-
+                                                <TooltipCustom
+                                                    id={"wireCostTooltip"}
+                                                    disabledIcon={true}
+                                                    width={"350px"}
+                                                    tooltipText={`${getLabel('wireCost')} = ${getLabel('wireRate')} * Consumption`}
+                                                />
                                                 <NumberFieldHookForm
                                                     label={getLabel('wireCost')}
                                                     name={'wireCost'}
-                                                    id={"wireRate"}
+                                                    id={"wireCostTooltip"}
                                                     Controller={Controller}
                                                     control={control}
                                                     register={register}
