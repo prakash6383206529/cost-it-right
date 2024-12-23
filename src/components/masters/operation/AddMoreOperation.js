623b1f1e4adee7af27ef8a353d81f3a797a37453
Bhola Vishwakarma <bhola.vishwakarma@softude.com>
2024-12-23 12:29:20 +0530

    CC6-I70 | (Pre demoV3) - Additional masters - Operation - Add more detail | Error message must pop-up while putting value

--- src/components/masters/operation/AddMoreOperation.js
+++ src/components/masters/operation/AddMoreOperation.js
@@ -1290,7 +1290,13 @@ function AddMoreOperation(props) {
                                                 required: false,
                                                 validate: {
                                                     acceptAllExceptSingleSpecialCharacter,
-                                                    maxLength25,
+                                                    maxLength25: (value) => {
+                                                        if (value && value.length > 25) {
+                                                            Toaster.warning('Description cannot exceed 25 characters');
+                                                            return 'Max length must be 25';
+                                                        }
+                                                        return true;
+                                                    },
                                                     checkWhiteSpaces,
                                                 }
                                             }}
