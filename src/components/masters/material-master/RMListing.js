cbf543c4c6bb40cb676ec1e603751fa2ddecaf92
Bhola Vishwakarma <bhola.vishwakarma@softude.com>
2024-12-30 12:42:29 +0530

    CC6-I182 | manage RM| Manage material - 2 Toaster message getting appear when user click on delete icon

--- src/components/masters/material-master/RMListing.js
+++ src/components/masters/material-master/RMListing.js
@@ -143,9 +143,7 @@ const RMListing = (props) => {
   const confirmDelete = (ID) => {
     dispatch(
       deleteMaterialTypeAPI(ID, (res) => {
-        if (res.status === 417 && res.data.Result === false) {
-          Toaster.error(res.data.Message);
-        } else if (res && res.data && res.data.Result === true) {
+        if (res && res?.data && res?.data?.Result === true) {
           Toaster.success(MESSAGES.DELETE_MATERIAL_SUCCESS);
           setState((prevState) => ({ ...prevState, dataCount: 0 }));
           getListData();
