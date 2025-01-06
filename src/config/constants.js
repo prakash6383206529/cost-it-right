2a7d4acc3105d4b21de6fc7212963c50039919e6
Samrudhi Chitnis <samrudhi.chitnis@softude.com>
2025-01-06 18:04:01 +0530

    V3.1.31.7

--- src/config/constants.js
+++ src/config/constants.js
@@ -18,7 +18,8 @@ export const config = () => {
 }
 
 
-const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+// const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
+const BASE_URL = `http://10.10.1.100:10152/api/v1`;
 // const BASE_URL = `http://10.62.1.11:8081/api/v1`;
 export const FILE_URL = `${process.env.REACT_APP_FILE_URL}`;
 
@@ -3291,5 +3292,5 @@ export const RAWMATERIALCOST = 'Raw Material Other Cost'
 export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 
 //VERSION 
-export const VERSION = "V3.1.31.6";
+export const VERSION = "V3.1.31.7";
 
