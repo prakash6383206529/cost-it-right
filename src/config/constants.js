c371bc52f9129a5731d4aff2a770fd83e232f837
mahak.rawat <mahak.rawat@softude.com>
2025-01-20 16:53:02 +0530

    Version Changed to V3.1.65.2

--- src/config/constants.js
+++ src/config/constants.js
@@ -3286,7 +3286,7 @@ export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 export const IsSelectSinglePlant = true
 
 //VERSION 
-export const VERSION = "V3.1.65.1";
+export const VERSION = "V3.1.65.2";
 
 
 
