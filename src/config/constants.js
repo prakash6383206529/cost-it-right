01fc92d1cbb4fcdf51d7811a9fb0724a76e01733
Aditi Bhawsar <aditi.bhawsar@softude.com>
2025-02-04 15:46:34 +0530

    version updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -3295,7 +3295,7 @@ export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 export const IsSelectSinglePlant = true
 
 //VERSION 
-export const VERSION = "V3.1.69";
+export const VERSION = "V3.1.70";
 
 
 
