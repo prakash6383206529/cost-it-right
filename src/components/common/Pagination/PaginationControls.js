eb0bc6118af80963aef6e45b6b129b0872970e2b
Bhola Vishwakarma <bhola.vishwakarma@softude.com>
2025-02-24 19:10:52 +0530

    CC6-I264 | (Pre DemoV3) -Common Issue for all listing | Users | Pagination - Pagination Navigation Arrows Not Functioning Correctly through out the application.

--- src/components/common/Pagination/PaginationControls.js
+++ src/components/common/Pagination/PaginationControls.js
@@ -108,7 +108,7 @@ const PaginationControls = ({ totalRecordCount, getDataList, floatingFilterData,
     return (
         <div className="d-flex pagination-button-container">
             <p>
-                <Button id="commonPagination_previous" variant="previous-btn" onClick={onBtPrevious} />
+                <Button id="commonPagination_previous" variant="previous-btn" onClick={onBtPrevious} disabled={pageNo === 1} />
             </p>
             {pageSize?.pageSize10 && (
                 <p className="next-page-pg custom-left-arrow">
@@ -126,7 +126,7 @@ const PaginationControls = ({ totalRecordCount, getDataList, floatingFilterData,
                 </p>
             )}
             <p>
-                <Button id="commonPagination_next" variant="next-btn" onClick={onBtNext} />
+                <Button id="commonPagination_next" variant="next-btn" onClick={onBtNext} disabled={pageNo >= Math.ceil(totalRecordCount / pageSizeValue)} />
             </p>
         </div>
     );
