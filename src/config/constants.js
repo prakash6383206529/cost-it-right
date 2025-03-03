dba5cd68e46842f825a6d319576acb89e5144955
Banti Prajapati <banti.prajapati@softude.com>
2025-03-03 15:07:59 +0530

    version update

--- src/config/constants.js
+++ src/config/constants.js
@@ -3358,7 +3358,7 @@ export const COSTINGCONDITIONCOST = 'Costing Condition Cost'
 export const IsSelectSinglePlant = true
 
 //VERSION 
-export const VERSION = "V4.1.1";
+export const VERSION = "V4.1.2";
 
 
 
