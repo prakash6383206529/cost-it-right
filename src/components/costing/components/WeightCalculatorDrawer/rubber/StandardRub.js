67aa360b6c4105b395d1078a3e686ec00592eb90
Shaylendra Patel <shaylendra.patel@softude.com>
2025-03-22 17:20:51 +0530

    CJ2-I55 | hide warning toaster if error is showing and vice versa

--- src/components/costing/components/WeightCalculatorDrawer/rubber/StandardRub.js
+++ src/components/costing/components/WeightCalculatorDrawer/rubber/StandardRub.js
@@ -357,7 +357,9 @@ function StandardRub(props) {
             ...(isVolumeAutoCalculate && (!(tableData.length > 0) || lastRow?.Length === 0) ? ["Length"] : []),
         ];
         const isValid = await trigger(validationFields);
-        if(!isValid || (!isVolumeAutoCalculate && obj.Volume === 0) || (isVolumeAutoCalculate && (obj.InnerDiameter === 0 || obj.OuterDiameter === 0 || obj.Length === 0 || obj.CuttingAllowance === 0))   ){
+        if(!isValid){
+            return false;
+        }else if((!isVolumeAutoCalculate && obj.Volume === 0) || (isVolumeAutoCalculate && (obj.InnerDiameter === 0 || obj.OuterDiameter === 0 || obj.Length === 0 || obj.CuttingAllowance === 0))   ){
             Toaster.warning("Please fill all the mandatory fields first.")
             return false;
         }
