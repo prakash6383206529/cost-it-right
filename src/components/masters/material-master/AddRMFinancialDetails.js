ee6f52c20d81a8cac54a53c9544bdcb4a68d827a
Bhola Vishwakarma <bhola.vishwakarma@softude.com>
2025-03-24 19:05:15 +0530

    remove console message

--- src/components/masters/material-master/AddRMFinancialDetails.js
+++ src/components/masters/material-master/AddRMFinancialDetails.js
@@ -183,7 +183,6 @@ function AddRMFinancialDetails(props) {
                     if ((IsFetchExchangeRateVendorWiseForZBCRawMaterial() || IsFetchExchangeRateVendorWiseForParts()) && (!rawMaterailDetails?.Vendor && !rawMaterailDetailsRefFinancial.current?.customer)) {
                         return false;
                     }
-                    console.log("rawMaterailDetailsRefFinancial.current?.customer?.value", rawMaterailDetailsRefFinancial.current?.customer?.value)
                     if ((rawMaterailDetails?.Vendor?.value || rawMaterailDetailsRefFinancial.current?.customer?.value) && states.isImport) {
 
                         const { costingHeadTypeId, vendorId, clientId } = getExchangeRateParams({ fromCurrency: state.currency?.label, toCurrency: Data?.Currency, defaultCostingTypeId: costingTypeId, vendorId: rawMaterailDetails?.Vendor?.value, clientValue: rawMaterailDetailsRefFinancial.current?.customer?.value, master: RAWMATERIAL, plantCurrency: getValues("plantCurrency") });
@@ -512,7 +511,6 @@ function AddRMFinancialDetails(props) {
 
         let NetLandedCostConversion
         let NetLandedCostLocalConversion
-        console.log("CurrencyExchangeRate", CurrencyExchangeRate)
         NetLandedCostLocalConversion = checkForDecimalAndNull(NetLandedCost * checkForNull(CurrencyExchangeRate?.plantCurrencyRate) ?? 1, getConfigurationKey().NoOfDecimalForPrice)
 
         if (states.isImport) {
