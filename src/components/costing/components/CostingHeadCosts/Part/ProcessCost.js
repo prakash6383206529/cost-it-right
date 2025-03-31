3dfb83919dda760e8a6184456375d8550ca04070
Mansi Agrawal <mansi.agrawal@softude.com>
2025-03-31 16:29:19 +0530

    (Hero-QA) - Costing | Process/ Operation | In the applicability dropdown, Select must be display.||CJ2-I64

--- src/components/costing/components/CostingHeadCosts/Part/ProcessCost.js
+++ src/components/costing/components/CostingHeadCosts/Part/ProcessCost.js
@@ -575,7 +575,7 @@ function ProcessCost(props) {
         // el.CostingConditionNumber = processApplicabilitySelect?.find(type => type?.value === el?.Applicability?.value)?.label || null
         setValue(`${ProcessGridFields}.${index}.ProcessCost`, checkForDecimalAndNull(el?.ProcessCost, initialConfiguration?.NoOfDecimalForPrice))
         setValue(`${ProcessGridFields}.${index}.Quantity`, checkForDecimalAndNull(el.Quantity, getConfigurationKey().NoOfDecimalForInputOutput))
-        setValue(`${ProcessGridFields}.${index}.Applicability`, { label: el?.CostingConditionNumber, value: el?.CostingConditionMasterAndTypeLinkingId })
+        setValue(`${ProcessGridFields}.${index}.Applicability`, el?.CostingConditionNumber ? { label: el?.CostingConditionNumber, value: el?.CostingConditionMasterAndTypeLinkingId } : null)
         setValue(`${ProcessGridFields}.${index}.ProcessCRMHead`, { label: el?.ProcessCRMHead, value: el?.index })
         return null
       })
@@ -846,7 +846,7 @@ function ProcessCost(props) {
         setValue(`${ProcessGridFields}.${i}.ProcessCost`, checkForDecimalAndNull(el.ProcessCost, initialConfiguration?.NoOfDecimalForPrice))
         setValue(`${ProcessGridFields}.${i}.Quantity`, checkForDecimalAndNull(el.Quantity, getConfigurationKey().NoOfDecimalForInputOutput))
         setValue(`${ProcessGridFields}.${i}.remarkPopUp`, el.Remark)
-        setValue(`${ProcessGridFields}.${i}.Applicability`, { label: el?.CostingConditionNumber, value: el?.CostingConditionMasterAndTypeLinkingId })
+        setValue(`${ProcessGridFields}.${i}.Applicability`, el?.CostingConditionNumber ? { label: el?.CostingConditionNumber, value: el?.CostingConditionMasterAndTypeLinkingId } : null)
         setValue(`${ProcessGridFields}.${i}.ProcessCRMHead`, { label: el?.ProcessCRMHead, value: el?.index })
 
         return null
@@ -1117,10 +1117,10 @@ function ProcessCost(props) {
         ...tempData,
         CostingConditionMasterAndTypeLinkingId: e?.value,
         CostingConditionNumber: e?.label,
-        Applicability: {
+        Applicability: e?.value ? {
           value: e.value,
           label: e.label
-        },
+        } : null,
        // ...netCosts
       }
     }
@@ -1134,10 +1134,10 @@ function ProcessCost(props) {
           ...childProcess,
           CostingConditionMasterAndTypeLinkingId: e?.value,
           CostingConditionNumber: e?.label,
-          Applicability: {  // Store complete applicability object for child processes
+          Applicability: e?.value ? {  // Store complete applicability object for child processes
             value: e?.value,
             label: e?.label
-          },
+          } : null,
          // ...childNetCosts
         };
       });
@@ -1192,7 +1192,7 @@ function ProcessCost(props) {
           ({ processCost, ProcessCostWithOutInterestAndDepreciation } = findProcessCost(tempData.UOM, tempData.MHR, productionPerHour, tempData?.MHRWithOutInterestAndDepreciation))
 
       }
-      const parentApplicability = processTempData.Applicability?.label;
+      const parentApplicability = processTempData.Applicability?.label || null;
 
       //const netCosts = calculateNetCosts(processCost, parentApplicability, "Process", ProcessCostWithOutInterestAndDepreciation, tempData?.IsDetailed, tempData?.UOMType);
       tempData = {
@@ -1862,7 +1862,7 @@ function ProcessCost(props) {
                                 defaultValue={item?.CostingConditionMasterAndTypeLinkingId ? {
                                   label: item?.CostingConditionNumber,
                                   value: item?.CostingConditionMasterAndTypeLinkingId
-                                } : ''}
+                                } : null}
                                 options={processApplicabilitySelect}
                                 required={false}
                                 handleChange={(e) => { onHandleChangeApplicability(e, index) }}
