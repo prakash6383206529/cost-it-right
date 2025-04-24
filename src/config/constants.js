f3c7ee2ba99cfbe3b7c0b3b6b5af67195ff10629
mahak.rawat <mahak.rawat@softude.com>
2025-04-24 14:32:38 +0530

    Version updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -3409,7 +3409,7 @@ export const HANGEROVERHEAD = "Hanger Overhead"
 
 export const IsSelectSinglePlant = true
 //VERSION 
-export const VERSION = "V4.2.25";
+export const VERSION = "V4.2.26";
 
 
 
