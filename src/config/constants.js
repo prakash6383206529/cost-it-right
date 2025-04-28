4b15171eb9bcdd679a0c196526e88e8680766c33
Aditi Bhawsar <aditi.bhawsar@softude.com>
2025-04-28 20:07:23 +0530

    version updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -3410,7 +3410,7 @@ export const HANGEROVERHEAD = "Hanger Overhead"
 
 export const IsSelectSinglePlant = true
 //VERSION 
-export const VERSION = "V4.2.35";
+export const VERSION = "V4.2.36";
 
 
 
