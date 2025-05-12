85cbfac06e2fd011849c225172e871f6dc03b623
Bhola Vishwakarma <bhola.vishwakarma@softude.com>
2025-05-12 13:04:13 +0530

    C-A8-I81 | ( FST ) - Masters | Add / View/ Edit-  Validation getting appears for non mandatory field and after enter value also validation is not getting remove.

--- src/components/masters/fuel-master/AddPower.js
+++ src/components/masters/fuel-master/AddPower.js
@@ -2017,7 +2017,7 @@ class AddPower extends Component {
                           <Col md="3">
                             <Field
                               label="Exchange Rate Source"
-                              name="ExchangeSource"
+                              name="exchangeSource"
                               placeholder="Select"
                               options={this.renderListing("ExchangeSource")}
                               handleChangeDescription={this.handleExchangeRateSource}
