872928406c986f224c8c487d9a0e04e59df1fe3e
Mansi Agrawal <mansi.agrawal@softude.com>
2025-05-15 17:36:17 +0530

    version updated

--- src/config/constants.js
+++ src/config/constants.js
@@ -3380,7 +3380,7 @@ export const HANGEROVERHEAD = "Hanger Overhead"
 
 export const IsSelectSinglePlant = true
 //VERSION 
-export const VERSION = "V4.2.44";
+export const VERSION = "V4.2.45";
 
 
 
