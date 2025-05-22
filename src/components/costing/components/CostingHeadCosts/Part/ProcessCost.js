5874f5ccf38241efa4ee0a0bb55eebba61da58af
praveen.choudhary <praveen.choudhary@softude.com>
2025-05-22 16:08:05 +0530

    CJ2-I252 remark validation added at one missing place

--- src/components/costing/components/CostingHeadCosts/Part/ProcessCost.js
+++ src/components/costing/components/CostingHeadCosts/Part/ProcessCost.js
@@ -1916,6 +1916,7 @@ function ProcessCost(props) {
                                     //errors={errors && errors.remarkPopUp && errors.remarkPopUp[index] !== undefined ? errors.remarkPopUp[index] : ''}                        
                                     disabled={(CostingViewMode || IsLocked) ? true : false}
                                     hidden={false}
+                                    validateWithRemarkValidation={true}
                                   />
                                   <Row>
                                     <Col md="12" className='remark-btn-container'>
