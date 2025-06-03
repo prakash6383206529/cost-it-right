4ec234208848d5e3192abd15c9dc1fb69ccaec51
Shaylendra Patel <shaylendra.patel@softude.com>
2025-06-03 19:51:17 +0530

    version update to V4.2.51

--- src/config/constants.js
+++ src/config/constants.js
@@ -3442,7 +3442,7 @@ export const HANGEROVERHEAD = "Hanger Overhead"
 
 export const IsSelectSinglePlant = true
 //VERSION 
-export const VERSION = "V4.2.50";
+export const VERSION = "V4.2.51";
 
 
 
