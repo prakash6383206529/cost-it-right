3036a7f59f9449a74db2cf67376687254da1ebde
Bhola Vishwakarma <bhola.vishwakarma@softude.com>
2025-06-09 11:44:37 +0530

    Revert "C-A8-I193 | ( FST ) -  Additional Master | Budget - Value getting added in sum column along with error message with 0 value."
    This reverts commit 6bcb33715b746cef1b6b48cd3564ef9f603b2944.

--- src/components/masters/part-master/IndivisualPartListing.js
+++ src/components/masters/part-master/IndivisualPartListing.js
@@ -472,7 +472,7 @@ const IndivisualPartListing = (props) => {
           Toaster.success(MESSAGES.PART_ACTIVE_SUCCESSFULLY)
 
         }
-        getTableListData(skipRecord, globalTakes, state.floatingFilterData, true)
+        getTableListData(currentRowIndex, defaultPageSize, state.floatingFilterData, true)
         setState((prevState) => ({ ...prevState, dataCount: 0 }))
       }
     }))
