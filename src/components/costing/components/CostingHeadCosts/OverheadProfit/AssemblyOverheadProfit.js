2ed5cbe57bbed9b50810a0626e719618b45d9d8e
Samrudhi Chitnis <samrudhi.chitnis@softude.com>
2025-06-11 14:23:48 +0530

    CJ2-I480

--- src/components/costing/components/CostingHeadCosts/OverheadProfit/AssemblyOverheadProfit.js
+++ src/components/costing/components/CostingHeadCosts/OverheadProfit/AssemblyOverheadProfit.js
@@ -200,7 +200,6 @@ function AssemblyOverheadProfit(props) {
         <td>{item?.CostingPartDetails && item?.CostingPartDetails?.OverheadCost !== null ? checkForDecimalAndNull(item?.CostingPartDetails?.OverheadCost, initialConfiguration?.NoOfDecimalForPrice) : 0}</td>
         <td>{item?.CostingPartDetails && item?.CostingPartDetails?.ProfitCost !== null ? checkForDecimalAndNull(item?.CostingPartDetails?.ProfitCost, initialConfiguration?.NoOfDecimalForPrice) : 0}</td>
         <td>{item?.CostingPartDetails && item?.CostingPartDetails?.RejectionCost !== null ? checkForDecimalAndNull(item?.CostingPartDetails?.RejectionCost, initialConfiguration?.NoOfDecimalForPrice) : 0}</td>
-        <td>{item?.CostingPartDetails && item?.CostingPartDetails?.ICCCost !== null ? checkForDecimalAndNull(item?.CostingPartDetails?.ICCCost, initialConfiguration?.NoOfDecimalForPrice) : 0}</td>
       </tr>
       {item.IsOpen && <tr>
         <td colSpan={8} className="cr-innerwrap-td overhead-profit-container">
