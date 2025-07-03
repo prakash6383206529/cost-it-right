a6a2d6f707e8accbd2a80a9124912d5551892e00
Praveen Choudhary <praveen.choudhary@softude.com>
2025-07-03 20:00:30 +0530

    code formatting

--- src/helper/BulkDelete.js
+++ src/helper/BulkDelete.js
@@ -8,14 +8,14 @@ import { useDispatch, useSelector } from 'react-redux'
 
 function BulkDelete(props) {
 	const dispatch = useDispatch()
-  const [showPopup, setShowPopup] = useState(false)
-  const [popupMessage, setPopupMessage] = useState("")
+	const [showPopup, setShowPopup] = useState(false)
+	const [popupMessage, setPopupMessage] = useState("")
 
-  const getAssociatedConfig = (type, notEligibleList = [], eligibleToDelete = []) => {
+	const getAssociatedConfig = (type, notEligibleList = [], eligibleToDelete = []) => {
 		const mastersList = []
 		const eligibleToDeleteIds = []
-    switch (type) {
-      case 'RM':
+		switch (type) {
+			case 'RM':
 				if (_.size(notEligibleList)) {
 					_.forEach(notEligibleList, item => {
 						mastersList.push(_.get(item, 'RawMaterialName', ''))
@@ -27,79 +27,79 @@ function BulkDelete(props) {
 					})
 				}
 				return {
-          associatedKeyName: ["IsRMAssociated", "IsRFQRawMaterial"],
+					associatedKeyName: ["IsRMAssociated", "IsRFQRawMaterial"],
 					associatedSuccessMessage: "Raw Materials Deleted Successfully",
 					associatedType: "master",
 					associatedMasterType: "rm",
 					eligibleToDeleteIdsList: eligibleToDeleteIds,
-          associatedMessage: _.size(notEligibleList) === 0
-            ? "Are you sure you want to delete this raw material detail?"
-            : `Are you sure you want to delete this raw material detail? The following raw materials cannot be deleted because they are associated with costings: ${mastersList.join(", ")}`,
-        };
-      default:
-        return {
+					associatedMessage: _.size(notEligibleList) === 0
+						? "Are you sure you want to delete this raw material detail?"
+						: `Are you sure you want to delete this raw material detail? The following raw materials cannot be deleted because they are associated with costings: ${mastersList.join(", ")}`,
+				};
+			default:
+				return {
 					associatedKeyName: [],
 					associatedSuccessMessage: '',
 					associatedType: '',
 					associatedMasterType: '',
 					eligibleToDeleteIdsList: [],
-          associatedMessage: "Are you sure you want to delete this item?",
-        }
-    }
-  }
+					associatedMessage: "Are you sure you want to delete this item?",
+				}
+		}
+	}
 
-  const { associatedKeyName } = getAssociatedConfig(props?.type)
-	
+	const { associatedKeyName } = getAssociatedConfig(props?.type)
+
 	// If any key (Example IsRMAssociated or IsRFQRawMaterial) is true, the item is not eligible for deletion and should be filtered out.
-  const notEligibleToDelete = _.filter(props?.bulkDeleteData, item => _.some(associatedKeyName, key => item?.[key] === true))
-	
+	const notEligibleToDelete = _.filter(props?.bulkDeleteData, item => _.some(associatedKeyName, key => item?.[key] === true))
+
 	// EligibleToDeleteIds
 	const eligibleToDelete = _.filter(props?.bulkDeleteData, item => _.every(associatedKeyName, key => item?.[key] === false || item?.[key] === null))
 
-  const openPopup = () => {
-    const { associatedMessage } = getAssociatedConfig(props?.type, notEligibleToDelete, eligibleToDelete)
-    setPopupMessage(associatedMessage)
-    setShowPopup(true)
-  }
+	const openPopup = () => {
+		const { associatedMessage } = getAssociatedConfig(props?.type, notEligibleToDelete, eligibleToDelete)
+		setPopupMessage(associatedMessage)
+		setShowPopup(true)
+	}
 
-  const closePopUp = () => {
-    setShowPopup(false)
-  }
+	const closePopUp = () => {
+		setShowPopup(false)
+	}
 
-  const onPopupConfirm = () => {
-    setShowPopup(false)
-    deleteItem()
-  }
+	const onPopupConfirm = () => {
+		setShowPopup(false)
+		deleteItem()
+	}
 
 	const deleteItem = () => {
 		const { associatedSuccessMessage, associatedType, associatedMasterType, eligibleToDeleteIdsList } = getAssociatedConfig(props?.type, notEligibleToDelete, eligibleToDelete)
 		const payload = {
-  		Type: associatedType,
+			Type: associatedType,
 			MasterType: associatedMasterType,
 			GuidList: eligibleToDeleteIdsList,
 			MasterIdList: []
 		}
-				
+
 		dispatch(bulkDelete(payload, (res) => {
 			Toaster.success(associatedSuccessMessage)
-	  }))
+		}))
 		setShowPopup(false)
-  }
+	}
 
-  return (
-    <>
-      <Button
-        title="Delete"
-        className="mr-1"
-        icon="delete-primary"
-        id={`bulk_delete`}
-        onClick={openPopup}
-        buttonName={props?.dataCount === 0 || props?.deletePermission === false ? "" : `(${props?.dataCount})`}
-        disabled={props?.dataCount === 0 || props?.deletePermission === false}
-      />
-      {showPopup && (<PopupMsgWrapper isOpen={showPopup} closePopUp={closePopUp} confirmPopup={onPopupConfirm} message={popupMessage}/>)}
-    </>
-  );
+	return (
+		<>
+			<Button
+				title="Delete"
+				className="mr-1"
+				icon="delete-primary"
+				id={`bulk_delete`}
+				onClick={openPopup}
+				buttonName={props?.dataCount === 0 || props?.deletePermission === false ? "" : `(${props?.dataCount})`}
+				disabled={props?.dataCount === 0 || props?.deletePermission === false}
+			/>
+			{showPopup && (<PopupMsgWrapper isOpen={showPopup} closePopUp={closePopUp} confirmPopup={onPopupConfirm} message={popupMessage} />)}
+		</>
+	);
 }
 
 export default BulkDelete;
